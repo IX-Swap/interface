64ea5a44686dfd7b6302397cf40d34e8ce506200
Christopher Regner <dev@chrisregner.com>
2020-03-17 15:42:18 +0800

    Add Identity Page

--- src/context/config.js
+++ src/context/config.js
@@ -1,7 +1,7 @@
 
 export const apiUrl = (() => {
   if (process.env.NODE_ENV === 'development') {
-    return 'http://localhost:3456'
+    return 'https://api.mozork.com'
   }
   return 'https://api.investax.io'
 })()
--- src/context/httpRequests.js
+++ src/context/httpRequests.js
@@ -49,3 +49,20 @@ export async function deleteRequest (uri, payload) {
     body: JSON.stringify(payload)
   })
 }
+
+export async function putRequest (uri, payload) {
+  const bearerToken = localStorage.getItem('id_token')
+  return await fetch(apiUrl + uri, {
+    method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${bearerToken}`
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(payload)
+  })
+}
