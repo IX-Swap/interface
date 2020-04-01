081afeff7fb74044a7d01e5bc8dc047dfa24b4dc
Chad Lynch <chyad@hotmail.com>
2020-04-01 18:33:13 +0800

    updates

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -17,7 +17,7 @@ export default function Error () {
       <div className={classes.logotype}>
         <img className={classes.logotypeIcon} src={logo} alt='logo' />
         <Typography variant='h3' color='white' className={classes.logotypeText}>
-          Material Admin
+          InvestaX Digital Securities
         </Typography>
       </div>
       <Paper classes={{ root: classes.paperRoot }}>
