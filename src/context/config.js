0f19341f218c3a8deac86789b84811b5a38635f2
Chad Lynch <chyad@hotmail.com>
2020-04-20 14:20:18 +0800

    updated api url

--- src/context/config.js
+++ src/context/config.js
@@ -1,7 +1,6 @@
 export const apiUrl = (() => {
   if (process.env.NODE_ENV === 'development') {
-    return 'http://localhost:3456'
+    return 'https://api.mozork.com'
   }
-  return 'https://api.mozork.com'
-  // return 'https://api.investax.io'
+  return 'https://api.investax.io'
 })()
