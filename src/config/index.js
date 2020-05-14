09681fac806d97661225746ff454f90153b252f5
Maikko Aleman <maikko.aleman@gmail.com>
2020-05-14 10:47:05 +0800

    renamed env var from API_URL to REACT_APP_API_URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.API_URL || 'http://localhost:3050';
+export const API_URL = process.env.REACT_APP_API_URL || '';
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { ThemeProvider } from '@material-ui/styles'
-import { CssBaseline } from '@material-ui/core'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
 
-import Themes from './themes'
-import App from './components/App'
-import * as serviceWorker from './serviceWorker'
-import { UserProvider } from './context/UserContext'
+import Themes from './themes';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { UserProvider } from './context/UserContext';
 
 ReactDOM.render(
   <ThemeProvider theme={Themes.default}>
@@ -16,9 +16,9 @@ ReactDOM.render(
     </UserProvider>
   </ThemeProvider>,
   document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
