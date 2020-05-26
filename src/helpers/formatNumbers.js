5b9b54bf91aff1ac55516b7daece20d3460d6b28
Maikko Aleman <maikko.aleman@gmail.com>
2020-05-26 22:10:53 +0800

    commitments list

---
+++ src/helpers/formatNumbers.js
@@ -0,0 +1,7 @@
+// Adds decimal places and commas
+export const formatMoney = (value) =>
+  value && value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+// Adds commas
+export const formatNumber = (value) =>
+  value && value.toString().replace(/\d(?=(\d{3})+\.)/g, '$&,');
---
+++ src/pages/invest/pages/dso-board/CommitmentListItem.js
@@ -0,0 +1,36 @@
+// @flow
+import React from 'react';
+import { TableRow, TableCell } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { formatMoney, formatNumber } from 'helpers/formatNumbers';
+import { Commitment } from './modules/types';
+
+const useStyles = makeStyles(() => ({
+  logo: {
+    height: '42px',
+    width: '42px',
+    borderRadius: '42px',
+    backgroundColor: '#eaeaea',
+  },
+}));
+
+const CommitmentListItem = ({ commitment }: { commitment: Commitment }) => {
+  const classes = useStyles();
+  const { dso, pricePerUnit, totalAmount, numberOfUnits, status } = commitment;
+
+  return (
+    <TableRow>
+      <TableCell>
+        <div className={classes.logo} />
+      </TableCell>
+      <TableCell>{dso.tokenSymbol}</TableCell>
+      <TableCell>{formatMoney(pricePerUnit)}</TableCell>
+      <TableCell>asdas</TableCell>
+      <TableCell>{formatMoney(totalAmount)}</TableCell>
+      <TableCell>{formatNumber(numberOfUnits)}</TableCell>
+      <TableCell>{status}</TableCell>
+    </TableRow>
+  );
+};
+
+export default CommitmentListItem;
