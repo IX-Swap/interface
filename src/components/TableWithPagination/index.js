ad63b730961e1ae72351a116662156baa81e9dd4
adin234 <adinbautista@gmail.com>
2020-05-30 18:16:58 +0800

    create a generic pagination component

---
+++ src/components/TableWithPagination/index.js
@@ -0,0 +1,207 @@
+// @flow
+import * as React from 'react';
+import {
+  TableContainer,
+  TableBody,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableFooter,
+  TablePagination,
+  LinearProgress,
+} from '@material-ui/core';
+import { get } from 'lodash';
+import init from './modules';
+import type { Module, ModuleActions, ModuleMeta } from './modules';
+
+const { useRef, useEffect } = React;
+
+type TableColumn<T> = {
+  key: $Keys<T>,
+  label: string,
+  render: (val: any) => string,
+  align?: ?string,
+};
+
+type BaseRequirements<T> = {
+  id: string,
+  columns: Array<TableColumn<T>>,
+  endpoint: string,
+  children?: (...props: any) => Node | React.Element<any>,
+};
+
+type ItemsProps = {
+  items: Array<any>,
+  columns: Array<TableColumn<any>>,
+  children?: (...props: any) => Node | React.Element<any>,
+};
+
+type TableWithPaginationProps<T> = {
+  requirements: Module,
+  columns: Array<TableColumn<T>>,
+  children?: (...props: any) => Node | React.Element<any>,
+};
+
+const initializeRequirements = (id: string, endpoint: string) =>
+  init(id, endpoint);
+
+const usePaginationLogic = (actions: ModuleActions, meta: ModuleMeta) => {
+  const { PAGINATION_STATUS, useDispatch, useState } = meta;
+  const { setPage, setRowsPerPage, getter, clearApiStatus } = actions;
+  const dispatch = useDispatch();
+  const state = useState();
+  const { status, page, total, limit, items, statusCode, error } = state;
+  const mountedRef = useRef(true);
+
+  const handleChangePage = (_, newPage: number) => {
+    setPage(dispatch, { page: newPage });
+  };
+
+  const handleChangeRowsPerPage = (newRows: number) => {
+    setRowsPerPage(dispatch, { rows: newRows });
+    setPage(dispatch, { page: 0 });
+  };
+
+  useEffect(() => {
+    if (status === PAGINATION_STATUS.INIT) {
+      getter(dispatch, {
+        skip: page * limit,
+        limit,
+        ref: mountedRef,
+      });
+      clearApiStatus(dispatch);
+    }
+  }, [
+    page,
+    limit,
+    status,
+    dispatch,
+    getter,
+    clearApiStatus,
+    PAGINATION_STATUS,
+  ]);
+
+  useEffect(
+    () => () => {
+      mountedRef.current = false;
+    },
+    []
+  );
+
+  return {
+    dispatch,
+    items,
+    status,
+    total,
+    limit,
+    page,
+    statusCode,
+    error,
+    handleChangePage,
+    handleChangeRowsPerPage,
+    setPage,
+    PAGINATION_STATUS,
+  };
+};
+
+const Items = ({ items, columns, children }: ItemsProps) => (
+  <TableBody>
+    {items.length ? (
+      items.map((row) => (
+        <TableRow hover key={row._id}>
+          {columns.map((e) => (
+            <TableCell align="left" key={e.key}>
+              {e.key &&
+                (e.render ? e.render(get(row, e.key)) : get(row, e.key))}
+              {!e.key && children && children(row)}
+            </TableCell>
+          ))}
+        </TableRow>
+      ))
+    ) : (
+      <TableRow>
+        <TableCell align="center" colSpan={5}>
+          No Data
+        </TableCell>
+      </TableRow>
+    )}
+  </TableBody>
+);
+
+const TableWithPagination = ({
+  columns,
+  requirements,
+  children,
+}: TableWithPaginationProps<any>) => {
+  const { actions, meta } = requirements;
+  const {
+    items,
+    total,
+    limit,
+    page,
+    PAGINATION_STATUS,
+    handleChangeRowsPerPage,
+    handleChangePage,
+    status,
+  } = usePaginationLogic(actions, meta);
+
+  return (
+    <>
+      {[PAGINATION_STATUS.GETTING].includes(status) ? <LinearProgress /> : null}
+      <TableContainer component={Paper}>
+        <Table aria-label="accounts table">
+          <TableHead>
+            <TableRow>
+              {columns.map((e) => (
+                <TableCell key={e.key} align="left">
+                  <b>{e.label}</b>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <Items items={items} columns={columns}>
+            {children}
+          </Items>
+          {total && (
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25]}
+                  colSpan={6}
+                  count={total}
+                  rowsPerPage={limit}
+                  page={page}
+                  onChangeRowsPerPage={(
+                    evt: SyntheticInputEvent<HTMLElement>
+                  ) => handleChangeRowsPerPage(parseInt(evt.target.value))}
+                  onChangePage={handleChangePage}
+                />
+              </TableRow>
+            </TableFooter>
+          )}
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+const MainComponent = ({
+  id,
+  endpoint,
+  columns,
+  children,
+}: BaseRequirements<any>) => {
+  const Reqts = initializeRequirements(id, endpoint);
+
+  return (
+    <Reqts.meta.Provider>
+      <TableWithPagination columns={columns} requirements={Reqts}>
+        {children}
+      </TableWithPagination>
+    </Reqts.meta.Provider>
+  );
+};
+
+export default MainComponent;
--- src/pages/authorizer/banks/index.js
+++ src/pages/authorizer/banks/index.js
@@ -1,13 +1,151 @@
-import React from 'react';
-import BanksList from './list';
-import AuthorizerBankModule from './modules';
+// @flow
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useCallback, useState } from 'react';
+import { Typography, Select, MenuItem } from '@material-ui/core';
+import TableWithPagination from 'components/TableWithPagination';
+import type { Bank } from 'pages/accounts/bank/modules/types';
+import { makeStyles } from '@material-ui/core/styles';
+import { snackbarService } from 'uno-material-ui';
+import DialogAuthorizeConfirmation from './confirm';
+import { columns } from './data';
+import Actions from './modules/actions';
 
-const { AuthorizerBanksListProvider } = AuthorizerBankModule;
+const { toggleBankStatus } = Actions;
+
+const useStyles = makeStyles({
+  formControl: {
+    minWidth: 120,
+  },
+});
+
+const useBanksListLogic = () => {
+  const [bank, setBank] = useState<Bank | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
+  const [seed, setSeed] = useState(0);
+  const [newStatus, setNewStatus] = useState<string>('');
+  const handleSelectChange = (mBank: Bank, status: string) => {
+    setBank(mBank);
+    setNewStatus(status);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirm = async (mBank: Bank, status: string) => {
+    const confirm = await toggleBankStatus(mBank, status);
+    let message = 'Failed to update bank status!';
+    let type = 'error';
+
+    if (confirm) {
+      message = 'Successfully updated bank status!';
+      type = 'success';
+      setOpen(false);
+    }
+
+    setSeed(seed + 1);
+    snackbarService.showSnackbar(message, type);
+  };
+
+  return {
+    seed,
+    bank,
+    open,
+    newStatus,
+    handleSelectChange,
+    handleClose,
+    handleConfirm,
+  };
+};
+
+const RowStatusComponent = ({
+  bank,
+  handleSelectChange,
+}: {
+  bank: Bank,
+  handleSelectChange: (bank: Bank, status: string) => void,
+}) => {
+  const classes = useStyles();
+  switch (bank.status) {
+    case 'Approved':
+      return <Typography color="primary">Approved</Typography>;
+    case 'Rejected':
+      return <Typography color="error">Rejected</Typography>;
+    default:
+      return (
+        <Select
+          className={classes.formControl}
+          value={bank.status}
+          onClick={(evt) => {
+            evt.stopPropagation();
+            evt.preventDefault();
+            evt.nativeEvent.stopPropagation();
+            evt.nativeEvent.stopImmediatePropagation();
+          }}
+          onChange={(evt: SyntheticInputEvent<HTMLElement>) =>
+            handleSelectChange(bank, evt.target.value)
+          }
+          inputProps={{
+            name: 'status',
+          }}
+        >
+          <MenuItem value="Approved">Approved</MenuItem>
+          <MenuItem value="Rejected">Rejected</MenuItem>
+        </Select>
+      );
+  }
+};
+
+const MemoizedTable = React.memo(({ handleSelectChange }: any) => {
+  const mColumns = columns;
+  return (
+    <TableWithPagination
+      id="authorizerBanksList"
+      endpoint="/accounts/banks/list/"
+      columns={mColumns}
+    >
+      {(mBank: Bank) => (
+        <RowStatusComponent
+          bank={mBank}
+          handleSelectChange={handleSelectChange}
+        />
+      )}
+    </TableWithPagination>
+  );
+});
+MemoizedTable.displayName = 'MemoizedTable';
 
 export default function Banks() {
+  const {
+    open,
+    newStatus,
+    handleClose,
+    bank,
+    handleConfirm,
+    handleSelectChange,
+    seed,
+  } = useBanksListLogic();
+
+  const mHandleSelectChange = useCallback(
+    (mBank: Bank, status: string) => {
+      handleSelectChange(mBank, status);
+    },
+    [seed]
+  );
+
   return (
-    <AuthorizerBanksListProvider>
-      <BanksList />
-    </AuthorizerBanksListProvider>
+    <>
+      {bank && (
+        <DialogAuthorizeConfirmation
+          open={open}
+          newStatus={newStatus}
+          handleClose={handleClose}
+          bank={bank}
+          handleConfirm={handleConfirm}
+        />
+      )}
+      <MemoizedTable handleSelectChange={mHandleSelectChange} />
+    </>
   );
 }
