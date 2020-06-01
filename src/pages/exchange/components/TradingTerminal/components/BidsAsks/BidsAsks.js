416daf7205c4a847725d6a5caf8661b064e92bdd
Maikko Aleman <maikko.aleman@gmail.com>
2020-06-01 08:34:05 +0800

    commented out unused vars

--- src/pages/exchange/components/TradingTerminal/components/BidsAsks/BidsAsks.js
+++ src/pages/exchange/components/TradingTerminal/components/BidsAsks/BidsAsks.js
@@ -12,175 +12,171 @@ import localStore from 'services/storageHelper';
 
 // Modules
 import PostOrderActions from './modules/actions';
-import Modules from './modules';
+import { usePostOrderDispatch } from './modules';
 
 // Monitoring Module
-import MonitoringModule from '../Monitoring/modules';
+// import MonitoringModule from '../Monitoring/modules';
 
 // Styles
 import useStyles from '../styles';
 
-const { MonitoringState } = MonitoringModule;
-const { PostOrderState, usePostOrderDispatch } = Modules;
+// const { MonitoringState } = MonitoringModule;
 const BidsAsksHistory = (props) => {
-    const { id } = props;
-    const classes = useStyles();
-    const bearerToken = localStore.getAccessToken();
-    const _userId = localStore.getUserId();
-    const socket = io(`${API_URL}?token=${bearerToken}`);
+  const { id } = props;
+  const classes = useStyles();
+  const bearerToken = localStore.getAccessToken();
+  const _userId = localStore.getUserId();
+  const socket = io(`${API_URL}?token=${bearerToken}`);
 
-    // eslint-disable-next-line
+  // eslint-disable-next-line
     const [collection, setCollection] = useState(false); 
-    const { SUBSCRIBE_API } = ENDPOINT_URL;
-    const { BIDS_ASKS } = SUBSCRIBE_API;
+  const { SUBSCRIBE_API } = ENDPOINT_URL;
+  const { BIDS_ASKS } = SUBSCRIBE_API;
 
-    // Subscribe to the bids/asks
-    // TODO: Better way to implement this locally/globally
-    useEffect(() => {
-        socket.emit(BIDS_ASKS.emit, id);
-        socket.on(`${BIDS_ASKS.on}/${_userId}`, (data) => {
-            setCollection(data);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    // State for the RESET FORM fields
-    const [form, setFields] = useState({
-        price: 0,
-        amount: 0,
-        total: 0,
+  // Subscribe to the bids/asks
+  // TODO: Better way to implement this locally/globally
+  useEffect(() => {
+    socket.emit(BIDS_ASKS.emit, id);
+    socket.on(`${BIDS_ASKS.on}/${_userId}`, (data) => {
+      setCollection(data);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    // Handle change/update for the fields
-    const updateField = (e) => {
-        const { name } = e.target;
-        const { value } = e.target;
-        setFields({
-            ...form,
-            [name]: value,
-        });
-    };
+  // State for the RESET FORM fields
+  const [form, setFields] = useState({
+    price: 0,
+    amount: 0,
+    total: 0,
+  });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
 
-    const sellButtonClassName = classNames(
-        classes.formButton,
-        classes.sellButton
-    );
+  // Handle change/update for the fields
+  const updateField = (e) => {
+    const { name } = e.target;
+    const { value } = e.target;
+    setFields({
+      ...form,
+      [name]: value,
+    });
+  };
 
-    const dispatch = usePostOrderDispatch();
-    const orderState = PostOrderState();
-    const payloadState = MonitoringState();
-    
-    const _handleBidAsk = evt => {
-        PostOrderActions.postOrder(dispatch, {
-            pair: id,
-            side: 'BID', 
-            type: 'LIMIT', 
-            price: 100, 
-            amount: 10
-        });
-    }
-    const fields = [
-        {
-            id: 'price',
-            name: 'price',
-            label: 'Price:',
-            value: form.price,
-            onChange: updateField,
-            placeholder: 'Price...',
-            type: 'number',
-        },
-        {
-            id: 'amount',
-            name: 'amount',
-            label: 'Amount:',
-            value: form.amount,
-            onChange: updateField,
-            placeholder: 'Amount...',
-            type: 'number',
-        },
-        {
-            id: 'total',
-            name: 'total',
-            label: 'Total:',
-            value: form.total,
-            onChange: updateField,
-            placeholder: 'Total...',
-            type: 'number',
-        },
-    ];
+  const sellButtonClassName = classNames(
+    classes.formButton,
+    classes.sellButton
+  );
 
-    return (
-        <Paper className={classes.bidsAsksContainer}>
-            <form className={classes.formContainer}>
-                <div className={classes.formHeader}>
-                    <Typography className={classes.formTitle} variant="h3">
-                        Buy {(collection.length && collection[1].symbol)}
-                    </Typography>
-                    <div>
-                        {(collection.length && collection[1].available || 0)}
-                    </div>
-                </div>
-                {fields.map(field => 
-                    <div className={classes.inputContainer}>
-                        <label>{field.label}</label>
-                        <input
-                            className={classes.inputField}
-                            key={field.id}
-                            id={`${field.id}-buy`}
-                            value={field.value}
-                            onChange={field.onChange}
-                            placeholder={field.placeholder}
-                            type={field.type}
-                            name={field.name}
-                        />
-                    </div>
-                )}
-                <Button 
-                    className={classes.formButton}
-                    variant="contained" 
-                    color="primary" 
-                    disableElevation
-                    onClick={_handleBidAsk}
-                >
-                    BID IXPS
-                </Button>
-            </form>
-            <form className={classes.formContainer}>
-                <div className={classes.formHeader}>
-                    <Typography className={classes.formTitle} variant="h3">
-                        Buy {(collection.length && collection[0].symbol)}
-                    </Typography>
-                    <div>
-                        {(collection.length && collection[0].available || 0)}
-                    </div>
-                </div>
-                {fields.map(field => 
-                    <div className={classes.inputContainer}>
-                        <label>{field.label}</label>
-                        <input
-                            className={classes.inputField}
-                            key={field.id}
-                            id={`${field.id}-sell`}
-                            value={field.value}
-                            onChange={field.onChange}
-                            placeholder={field.placeholder}
-                            type={field.type}
-                            name={field.name}
-                        />
-                    </div>
-                )}
-                <Button 
-                    className={sellButtonClassName}
-                    variant="contained" 
-                    color="primary" 
-                    disableElevation
-                >
-                    ASK IXPS
-                </Button>
-            </form>
-        </Paper>
-    );
+  const dispatch = usePostOrderDispatch();
+  // const orderState = PostOrderState();
+  // const payloadState = MonitoringState();
+
+  const _handleBidAsk = (evt) => {
+    PostOrderActions.postOrder(dispatch, {
+      pair: id,
+      side: 'BID',
+      type: 'LIMIT',
+      price: 100,
+      amount: 10,
+    });
+  };
+
+  const fields = [
+    {
+      id: 'price',
+      name: 'price',
+      label: 'Price:',
+      value: form.price,
+      onChange: updateField,
+      placeholder: 'Price...',
+      type: 'number',
+    },
+    {
+      id: 'amount',
+      name: 'amount',
+      label: 'Amount:',
+      value: form.amount,
+      onChange: updateField,
+      placeholder: 'Amount...',
+      type: 'number',
+    },
+    {
+      id: 'total',
+      name: 'total',
+      label: 'Total:',
+      value: form.total,
+      onChange: updateField,
+      placeholder: 'Total...',
+      type: 'number',
+    },
+  ];
+
+  return (
+    <Paper className={classes.bidsAsksContainer}>
+      <form className={classes.formContainer}>
+        <div className={classes.formHeader}>
+          <Typography className={classes.formTitle} variant="h3">
+            Buy {collection.length && collection[1].symbol}
+          </Typography>
+          <div>{(collection.length && collection[1].available) || 0}</div>
+        </div>
+        {fields.map((field) => (
+          <div className={classes.inputContainer}>
+            <label>{field.label}</label>
+            <input
+              className={classes.inputField}
+              key={field.id}
+              id={`${field.id}-buy`}
+              value={field.value}
+              onChange={field.onChange}
+              placeholder={field.placeholder}
+              type={field.type}
+              name={field.name}
+            />
+          </div>
+        ))}
+        <Button
+          className={classes.formButton}
+          variant="contained"
+          color="primary"
+          disableElevation
+          onClick={_handleBidAsk}
+        >
+          BID IXPS
+        </Button>
+      </form>
+      <form className={classes.formContainer}>
+        <div className={classes.formHeader}>
+          <Typography className={classes.formTitle} variant="h3">
+            Buy {collection.length && collection[0].symbol}
+          </Typography>
+          <div>{(collection.length && collection[0].available) || 0}</div>
+        </div>
+        {fields.map((field) => (
+          <div className={classes.inputContainer}>
+            <label>{field.label}</label>
+            <input
+              className={classes.inputField}
+              key={field.id}
+              id={`${field.id}-sell`}
+              value={field.value}
+              onChange={field.onChange}
+              placeholder={field.placeholder}
+              type={field.type}
+              name={field.name}
+            />
+          </div>
+        ))}
+        <Button
+          className={sellButtonClassName}
+          variant="contained"
+          color="primary"
+          disableElevation
+        >
+          ASK IXPS
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default BidsAsksHistory;
--- src/pages/exchange/components/TradingTerminal/components/Monitoring/Monitoring.js
+++ src/pages/exchange/components/TradingTerminal/components/Monitoring/Monitoring.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import { withRouter } from 'react-router-dom'
-import classNames from 'classnames'
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 import moment from 'moment';
 
 // Material Components
@@ -18,154 +18,141 @@ import MonitoringActions from './modules/actions';
 import Modules from './modules';
 
 // Styles
-import useStyles from '../styles'
+import useStyles from '../styles';
 
-const { MonitoringState, useMonitoringDispatch } = Modules;
+const {
+  // MonitoringState,
+  useMonitoringDispatch,
+} = Modules;
 function Monitoring(props) {
-    const dispatch = useMonitoringDispatch();
-    const state = MonitoringState();
-    const classes = useStyles();
-    const [ fav, setFav ] = useState(false);
-    const [ search, setSearch ] = useState(false);
-    const { title, type, data = [] } = props;
-    const maxValue = data.length && Math.max(...data.map(d =>d.price));
-    const minValue = data.length && Math.min(...data.map(d =>d.price));
+  const dispatch = useMonitoringDispatch();
+  // const state = MonitoringState();
+  const classes = useStyles();
+  const [fav, setFav] = useState(false);
+  const [search, setSearch] = useState(false);
+  const { title, type, data = [] } = props;
+  const maxValue = data.length && Math.max(...data.map((d) => d.price));
+  const minValue = data.length && Math.min(...data.map((d) => d.price));
 
-    const isAsksBids = props.type === 'asks' ||  props.type === 'bids';
+  const isAsksBids = props.type === 'asks' || props.type === 'bids';
 
-    let filteredData = search ? search : data;
-    
-    const _onSearch = evt => {
-        const target = evt.target;
-        const value = target.value;
+  const filteredData = search || data;
 
-        if (value.length > 3) {
-            const filterData = data.filter(d => {
-                return (
-                    d.name.toLowerCase().search(value.toLowerCase()) !== -1
-                );
-            });
+  const _onSearch = (evt) => {
+    const { target } = evt;
+    const { value } = target;
 
-            setSearch(filterData);
-        }
-    };
+    if (value.length > 3) {
+      const filterData = data.filter(
+        (d) => d.name.toLowerCase().search(value.toLowerCase()) !== -1
+      );
 
-    let _handleStorePayload = () => {};
-
-    if (isAsksBids) {
-        // SET PAYLOAD DATA FOR ORDERS
-        _handleStorePayload = data => {
-            MonitoringActions.setBidAndAsk(dispatch, data);
-        }
+      setSearch(filterData);
     }
+  };
 
-    return (
-        <React.Fragment>
-            {title && (
-                <Typography 
-                    className={classes.monitoringTitle} 
-                    variant="h1"
-                >
-                    {title}
-                </Typography>
-            )}
-            <section className={classes.monitoring}>
-                {type === 'bids' && (
-                    <section className={classes.bidsHeader}>
-                        <Typography 
-                            className={classes.maxBidsTitle} 
-                            variant="h3"
-                        >
-                            {maxValue}<ArrowUpwardRoundedIcon fontSize='small' />
-                        </Typography>
-                        <Typography 
-                            className={classes.minBidsTitle} 
-                            variant="h3"
-                        >
-                            {minValue}
-                        </Typography>
-                    </section>
+  let _handleStorePayload = () => {};
+
+  if (isAsksBids) {
+    // SET PAYLOAD DATA FOR ORDERS
+    _handleStorePayload = (data) => {
+      MonitoringActions.setBidAndAsk(dispatch, data);
+    };
+  }
+
+  return (
+    <>
+      {title && (
+        <Typography className={classes.monitoringTitle} variant="h1">
+          {title}
+        </Typography>
+      )}
+      <section className={classes.monitoring}>
+        {type === 'bids' && (
+          <section className={classes.bidsHeader}>
+            <Typography className={classes.maxBidsTitle} variant="h3">
+              {maxValue}
+              <ArrowUpwardRoundedIcon fontSize="small" />
+            </Typography>
+            <Typography className={classes.minBidsTitle} variant="h3">
+              {minValue}
+            </Typography>
+          </section>
+        )}
+        {type === 'asks' && (
+          <ul className={classes.monitoringHeader}>
+            <li>Price(SGD) {maxValue}</li>
+            <li>Amount(IXPS)</li>
+            <li>Total(SGD)</li>
+          </ul>
+        )}
+        {type === 'marketList' && (
+          <>
+            <div className={classes.actionContainer}>
+              <button onClick={() => setFav(!fav)}>
+                {fav ? (
+                  <StarIcon fontSize="small" />
+                ) : (
+                  <StarBorderIcon fontSize="small" />
                 )}
-                {type === 'asks' && (
-                    <ul className={classes.monitoringHeader}>
-                        <li>
-                        Price(SGD) {maxValue}
-                        </li>
-                        <li>
-                        Amount(IXPS) 
-                        </li>
-                        <li>
-                        Total(SGD) 
-                        </li>
-                    </ul>
-                )}
+              </button>
+              <button className={classes.actionBtn}>SGD</button>
+              <button className={classes.actionBtn}>USD</button>
+            </div>
+            <div className={classes.searchContainer}>
+              <input
+                onChange={_onSearch}
+                className={classes.searchInput}
+                type="search"
+                placeholder="search..."
+              />
+            </div>
+            <ul className={classes.marketHeader}>
+              <li className={classes.marketHeaderItem}>
+                Pair <ArrowDropUpIcon fontSize="small" color="disabled" />
+              </li>
+              <li className={classes.marketHeaderItem}>Price</li>
+            </ul>
+          </>
+        )}
+        <ul className={classes.monitoringList}>
+          {filteredData.map((d, i) => {
+            const activeStyle =
+              type === 'bids' ? classes.positiveCell : classes.negativeCell;
+            const priceStyle = classNames(
+              classes.defaultListItemStyle,
+              activeStyle
+            );
+
+            return (
+              <li
+                key={i}
+                className={classes.monitoringListItem}
+                onClick={() => _handleStorePayload(d)}
+              >
                 {type === 'marketList' && (
-                    <React.Fragment>
-                        <div className={classes.actionContainer}>
-                            <button onClick={() => setFav(!fav)}>
-                                {fav ?
-                                    <StarIcon fontSize='small' />
-                                    :
-                                    <StarBorderIcon fontSize='small' />
-                                }
-                            </button>
-                            <button className={classes.actionBtn}>SGD</button>
-                            <button className={classes.actionBtn}>USD</button>
-                        </div>
-                        <div className={classes.searchContainer}>
-                            <input  
-                                onChange={_onSearch}
-                                className={classes.searchInput} 
-                                type="search" 
-                                placeholder="search..." 
-                            />
-                        </div>
-                        <ul className={classes.marketHeader}>
-                            <li className={classes.marketHeaderItem}>
-                                Pair <ArrowDropUpIcon fontSize='small' color='disabled' />
-                            </li>
-                            <li className={classes.marketHeaderItem}>
-                                Price
-                            </li>
-                        </ul>
-                    </React.Fragment>
-                    
+                  <p className={classes.defaultListItemStyle} data-value={i}>
+                    <StarBorderIcon fontSize="small" /> {d.name}
+                  </p>
                 )}
-                <ul className={classes.monitoringList}>
-                    {filteredData.map((d, i ) => {
-                        const activeStyle = type === 'bids' ? classes.positiveCell : classes.negativeCell;
-                        const priceStyle = classNames(
-                            classes.defaultListItemStyle, 
-                            activeStyle
-                        );
-
-                        return (
-                            <li 
-                                key={i}
-                                className={classes.monitoringListItem}
-                                onClick={() => _handleStorePayload(d)}
-                            >   
-                                {type === 'marketList' && ( 
-                                    <p className={classes.defaultListItemStyle} data-value={i}>
-                                        <StarBorderIcon fontSize='small' /> {d.name}
-                                    </p>
-                                )}
-                                {type !== 'marketList' && (
-                                    <p className={priceStyle}>{d.price}</p>
-                                )}
-                                <p className={classes.defaultListItemStyle}>{d.amount || 0}</p>
-                                {type === 'tradeHistory' ?
-                                    <p className={classes.defaultListItemStyle}>{moment(d.createdAt).format(TIME_FORMAT)}</p>
-                                    :
-                                    <p className={classes.defaultListItemStyle}>{d.total}</p>
-                                }
-                            </li>   
-                        );
-                    })}
-                </ul>
-            </section>
-        </React.Fragment>
-    )
+                {type !== 'marketList' && (
+                  <p className={priceStyle}>{d.price}</p>
+                )}
+                <p className={classes.defaultListItemStyle}>{d.amount || 0}</p>
+                {type === 'tradeHistory' ? (
+                  <p className={classes.defaultListItemStyle}>
+                    {moment(d.createdAt).format(TIME_FORMAT)}
+                  </p>
+                ) : (
+                  <p className={classes.defaultListItemStyle}>{d.total}</p>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
 }
 
-export default withRouter(Monitoring)
+export default withRouter(Monitoring);
