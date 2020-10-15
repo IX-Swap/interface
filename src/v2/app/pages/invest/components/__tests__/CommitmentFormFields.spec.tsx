e42e5f09a7b6ea7860a09cff7f6dbaa46de21e78
varundevpro <28918411+VarunDevPro@users.noreply.github.com>
2020-10-15 16:12:12 +0530

    Remove redundant mock

--- src/v2/app/pages/invest/components/__tests__/CommitmentFormFields.spec.tsx
+++ src/v2/app/pages/invest/components/__tests__/CommitmentFormFields.spec.tsx
@@ -6,17 +6,10 @@ import {
   CommitmentFormFieldsProps
 } from 'v2/app/pages/invest/components/CommitmentFormFields'
 import { asset } from '__fixtures__/authorizer'
-import * as useCommitmentFormHook from 'v2/app/pages/invest/components/CommitmentForm'
-import { generateCreateTypedFormResult } from '__fixtures__/createTypedForm'
 import { Form } from 'v2/components/form/Form'
 
 describe('CommitmentFormFields', () => {
   const props: CommitmentFormFieldsProps = { symbol: asset.symbol }
-  beforeEach(() => {
-    jest
-      .spyOn(useCommitmentFormHook, 'useCommitmentForm')
-      .mockReturnValue(generateCreateTypedFormResult())
-  })
   afterEach(async () => {
     await cleanup()
     jest.clearAllMocks()
