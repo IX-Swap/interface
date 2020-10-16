2cf037a75dea7b86463c1f749b425611f25bab16
varundevpro <28918411+VarunDevPro@users.noreply.github.com>
2020-10-16 18:08:41 +0530

    Add tests to issuance pages

---
+++ src/__fixtures__/issuance.ts
@@ -0,0 +1,124 @@
+import { DSORequestArgs, DSOFormValues } from 'v2/types/dso'
+import { corporate, asset } from './authorizer'
+
+export const createDSOArgs: DSORequestArgs = {
+  businessModel: 'business model',
+  capitalStructure: 'capital structure',
+  corporate: corporate._id,
+  currency: asset._id,
+  distributionFrequency: 'distribution frequency',
+  dividendYield: 1,
+  documents: [],
+  equityMultiple: 'equity multiple',
+  fundraisingMilestone: 'fundraising milestone',
+  grossIRR: 0,
+  interestRate: 1,
+  introduction: 'introduction',
+  investmentPeriod: 1,
+  investmentStructure: 'investment structure',
+  issuerName: 'issuer name',
+  launchDate: '01-01-2000',
+  leverage: 'leverage',
+  logo: '5f4f7d87f3e2c40bbab8a3f1',
+  minimumInvestment: 100,
+  pricePerUnit: 1,
+  subscriptionDocument: 'subscription document',
+  team: [],
+  tokenName: 'token name',
+  tokenSymbol: 'token symbol',
+  totalFundraisingAmount: 100000,
+  useOfProceeds: 'use of proceeds',
+  policyBuilder: undefined,
+  status: 'Approved'
+}
+
+export const formvalues: DSOFormValues = {
+  team: [
+    {
+      name: 'Team Ultimate',
+      position: 'Maintainer',
+      about:
+        '{"blocks":[{"key":"e12jc","text":"Foo","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}'
+    }
+  ],
+  documents: [
+    {
+      document: {
+        _id: '5f898b52aa141c6d0d358ce5',
+        user: '5f7c1398fbc4b5491a1634ce',
+        title: '',
+        type: '',
+        originalFileName: 'a.jpg',
+        createdAt: '2020-10-16T12:00:18.823Z',
+        updatedAt: '2020-10-16T12:00:18.823Z'
+      },
+      label: '',
+      title: '',
+      type: ''
+    }
+  ],
+  status: 'Submitted',
+  logo: '5f898b69aa141c6d0d358ce6',
+  useOfProceeds:
+    '{"blocks":[{"key":"5mjvv","text":"As usual","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+  totalFundraisingAmount: 100000,
+  tokenSymbol: '$#',
+  tokenName: 'IXWTKN',
+  subscriptionDocument: '5f898b02aa141c6d0d358ce4',
+  pricePerUnit: 10,
+  minimumInvestment: 200,
+  leverage: '1',
+  launchDate: '2020-10-16T18:30:00.000Z',
+  issuerName: 'IXIssuer',
+  investmentStructure: '1',
+  investmentPeriod: 2,
+  introduction:
+    '{"blocks":[{"key":"3r09q","text":"Introducing IXW token","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+  interestRate: 1,
+  grossIRR: 1,
+  fundraisingMilestone:
+    '{"blocks":[{"key":"2ofbo","text":"next month","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+  equityMultiple: '1',
+  dividendYield: 1,
+  distributionFrequency: 'Monthly',
+  currency: '5f732c5b8a568b50914d8373',
+  corporate: '5f898a64aa141c6d0d358ce1',
+  capitalStructure: 'Good',
+  businessModel:
+    '{"blocks":[{"key":"fjodt","text":"lorem ipsum","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}'
+}
+
+export const requestargs: DSORequestArgs = {
+  team: [
+    {
+      name: 'Team Ultimate',
+      position: 'Maintainer',
+      about: '<p>Foo</p>\n'
+    }
+  ],
+  documents: ['5f898b52aa141c6d0d358ce5'],
+  logo: '5f898b69aa141c6d0d358ce6',
+  useOfProceeds: '<p>As usual</p>\n',
+  totalFundraisingAmount: 100000,
+  subscriptionDocument: '5f898b02aa141c6d0d358ce4',
+  pricePerUnit: 10,
+  minimumInvestment: 200,
+  leverage: '1',
+  launchDate: '2020-10-16T18:30:00.000Z',
+  investmentStructure: '1',
+  investmentPeriod: 2,
+  introduction: '<p>Introducing IXW token</p>\n',
+  interestRate: 1,
+  grossIRR: 1,
+  fundraisingMilestone: '<p>next month</p>\n',
+  equityMultiple: '1',
+  dividendYield: 1,
+  distributionFrequency: 'Monthly',
+  currency: '5f732c5b8a568b50914d8373',
+  corporate: '5f898a64aa141c6d0d358ce1',
+  capitalStructure: 'Good',
+  businessModel: '<p>lorem ipsum</p>\n',
+  issuerName: 'IXIssuer',
+  tokenName: 'IXWTKN',
+  tokenSymbol: '$#'
+}
--- src/v2/app/pages/issuance/pages/EditDSO.tsx
+++ src/v2/app/pages/issuance/pages/EditDSO.tsx
@@ -1,26 +1,13 @@
 import React from 'react'
-import { useDSOById } from 'v2/app/pages/invest/hooks/useDSOById'
-import { DSOForm } from 'v2/app/components/DSO/DSOForm'
-import { useUpdateDSO } from 'v2/app/pages/issuance/hooks/useUpdateDSO'
-import { DSOFormValues } from 'v2/types/dso'
-import { transformDSOFormValuesToRequestArgs } from 'v2/app/pages/issuance/utils'
 import { Grid } from '@material-ui/core'
 import { VSpacer } from 'v2/components/VSpacer'
 import { useIssuanceRouter } from 'v2/app/pages/issuance/router'
+import { DSO } from 'v2/app/pages/issuance/components/DSO'
 
 export const EditDSO = () => {
   const {
     params: { dsoId }
   } = useIssuanceRouter()
-  const { isLoading, data } = useDSOById(dsoId)
-  const [updateDSO] = useUpdateDSO(dsoId)
-  const handleSubmit = async (values: DSOFormValues) => {
-    await updateDSO(transformDSOFormValuesToRequestArgs(values, true))
-  }
-
-  if (isLoading || data === undefined) {
-    return null
-  }
 
   return (
     <Grid container>
@@ -28,12 +15,7 @@ export const EditDSO = () => {
         <VSpacer size='medium' />
       </Grid>
       <Grid item>
-        <DSOForm
-          data={data}
-          onSubmit={handleSubmit}
-          submitButtonLabel='Save'
-          isEditing
-        />
+        <DSO dsoId={dsoId} isEditing />
       </Grid>
     </Grid>
   )
