e12bb5486b33493b0b78fea2fbf4ec95fb31ebf9
varundevpro <28918411+VarunDevPro@users.noreply.github.com>
2020-10-20 10:30:37 +0530

    Fix notifications filter

--- src/v2/app/pages/notifications/components/NotificationsFilter.tsx
+++ src/v2/app/pages/notifications/components/NotificationsFilter.tsx
@@ -7,21 +7,38 @@ import { VSpacer } from 'v2/components/VSpacer'
 export const NotificationsFilter = () => {
   const { filter, handleClick } = useNotificationsFilter()
 
+  const features = [
+    'logins',
+    'bank-accounts',
+    'deposits',
+    'digital-security-withdrawals',
+    'withdrawals',
+    'corporates',
+    'individuals',
+    'commitments',
+    'offerings',
+    'deployments'
+  ]
+
   return (
     <Grid container direction='column'>
       <VSpacer size='small' />
       <Typography variant='h6'>Filters</Typography>
       <VSpacer size='small' />
-      {Object.entries(AppFeature).map(([key, value]) => (
-        <Grid item key={key}>
-          <FormControlLabel
-            label={key}
-            checked={filter.includes(value)}
-            onClick={() => handleClick(value)}
-            control={<Checkbox size='medium' />}
-          />
-        </Grid>
-      ))}
+      {Object.entries(AppFeature).map(([key, value]) => {
+        if (!features.includes(value)) return null
+
+        return (
+          <Grid item key={key}>
+            <FormControlLabel
+              label={key}
+              checked={filter.includes(value)}
+              onClick={() => handleClick(value)}
+              control={<Checkbox size='medium' />}
+            />
+          </Grid>
+        )
+      })}
     </Grid>
   )
 }
