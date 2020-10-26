bbfe80018995a459e0b9244af7a6e0ea5f23fab4
Alex Solovev <alex@investax.io>
2020-10-26 14:59:00 +0800

    Remove form debug code
    Signed-off-by: Alex Solovev <alex@investax.io>

--- src/v2/components/form/Form.tsx
+++ src/v2/components/form/Form.tsx
@@ -2,21 +2,12 @@ import React, { PropsWithChildren } from 'react'
 import { SubmitHandler, useForm, FormProvider } from 'react-hook-form'
 import { ObjectSchema, Shape, object } from 'yup'
 import { yupResolver } from '@hookform/resolvers'
-import { DeepMap, FieldError } from '@hookform/error-message/dist/types'
 import { useUnmountCallback } from 'v2/hooks/useUnmountCallback'
 
 export interface FormProps<T extends {}> {
   defaultValues?: Partial<T>
   onSubmit?: SubmitHandler<T>
   validationSchema?: ObjectSchema<Shape<object | undefined, T>>
-}
-
-export type FormErrorsMap = DeepMap<Record<string, any>, FieldError>
-
-export const formErrorsToMessagesArray = (errors: FormErrorsMap) => {
-  return Object.values(errors)
-    .map((error: FieldError) => error.message)
-    .filter(e => e !== undefined) as string[]
 }
 
 export const Form = <T,>(props: PropsWithChildren<FormProps<T>>) => {
@@ -45,9 +36,6 @@ export const Form = <T,>(props: PropsWithChildren<FormProps<T>>) => {
       >
         {children}
       </form>
-      <pre style={{ fontSize: 13, maxWidth: 1000, overflow: 'auto' }}>
-        {JSON.stringify(form.errors, null, 2)}
-      </pre>
     </FormProvider>
   )
 }
