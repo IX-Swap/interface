cb207578c2fedb185452f726c74a30982cc8226e
VarunDevPro <28918411+VarunDevPro@users.noreply.github.com>
2020-10-27 08:24:24 +0530

    Add tests to new components

---
+++ src/v2/app/pages/identity/components/__tests__/IdentityDialog.spec.tsx
@@ -0,0 +1,39 @@
+/**  * @jest-environment jsdom-sixteen  */
+import React from 'react'
+import { render, cleanup } from 'test-utils'
+import { fireEvent, waitFor } from '@testing-library/react'
+import {
+  IdentityDialog,
+  IdentityDialogProps
+} from 'v2/app/pages/identity/components/IdentityDialog'
+
+describe('IdentityDialog', () => {
+  const props: IdentityDialogProps = {
+    closeFn: jest.fn(),
+    isOpen: true
+  }
+  afterEach(async () => {
+    await cleanup()
+    jest.clearAllMocks()
+  })
+
+  it('renders without error', () => {
+    render(<IdentityDialog {...props} />)
+  })
+
+  it('renders nothing if isOpen is false', async () => {
+    const { container } = render(<IdentityDialog {...props} isOpen={false} />)
+    expect(container).toBeEmptyDOMElement()
+  })
+
+  it('invokes closeFn on (Skip for now) button click', async () => {
+    const { getByRole } = render(<IdentityDialog {...props} />)
+
+    const button = getByRole('button')
+    fireEvent.click(button)
+    await waitFor(() => {
+      expect(button.textContent).toEqual('Skip for now')
+      expect(props.closeFn).toHaveBeenCalledTimes(1)
+    })
+  })
+})
