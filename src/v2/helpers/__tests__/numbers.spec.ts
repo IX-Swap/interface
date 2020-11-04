a7729e4fa521249b93d3f9c98c0a45d1c2b72b5f
VarunDevPro <28918411+VarunDevPro@users.noreply.github.com>
2020-11-04 16:39:45 +0530

    Fix Money formatting for Nullable Types

--- src/v2/helpers/__tests__/numbers.spec.ts
+++ src/v2/helpers/__tests__/numbers.spec.ts
@@ -9,9 +9,9 @@ describe('formatAmount', () => {
   it('returns formatted amount', () => {
     expect(formatAmount(123)).toEqual('123.00')
   })
-  it('defaults amount to 0', () => {
-    expect(formatAmount(undefined as any)).toEqual('0.00')
-    expect(formatAmount(null as any)).toEqual('0.00')
+  it('defaults amount to empty string', () => {
+    expect(formatAmount(undefined as any)).toEqual('')
+    expect(formatAmount(null as any)).toEqual('')
   })
 })
 
@@ -24,13 +24,13 @@ describe('formatMoney', () => {
     expect(formatMoney(123)).toEqual('SGD 123.00')
   })
 
-  it('defaults amount to "0"', () => {
-    expect(formatMoney(null, '$')).toEqual('$ 0.00')
-    expect(formatMoney(undefined, '$')).toEqual('$ 0.00')
+  it('defaults amount to empty string', () => {
+    expect(formatMoney(null, '$')).toEqual('')
+    expect(formatMoney(undefined as any, '$')).toEqual('')
   })
 
   it('returns formatted money with symbol at end if right is true', () => {
-    expect(formatMoney(null, '$', true)).toEqual('0.00 $')
+    expect(formatMoney(null, '$', true)).toEqual('')
     expect(formatMoney(123, undefined, true)).toEqual('123.00 SGD')
   })
 })
