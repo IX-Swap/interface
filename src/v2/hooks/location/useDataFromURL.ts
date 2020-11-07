fb5682538f31afda42ab9c7ee984076860bd07a1
Alex Solovev <alex@investax.io>
2020-11-08 01:31:57 +0800

    Fix history duplication
    Signed-off-by: Alex Solovev <alex@investax.io>

--- src/v2/hooks/location/useDataFromURL.ts
+++ src/v2/hooks/location/useDataFromURL.ts
@@ -1,81 +1,81 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useLocation } from 'react-router-dom'
-import { AppFeature } from 'v2/types/app'
 import { urlParams } from 'v2/config/appURL'
 import {
   getCurrentLocationData,
   stripColonFromURLParam
 } from 'v2/hooks/location/utils'
+import { useEffect } from 'react'
 
 export const useDataFromURL = () => {
   const { pathname } = useLocation()
-  const { service, feature, params } = getCurrentLocationData(pathname)
-  const state: any = {}
 
-  if (params.length > 0) {
-    if (service === 'authorizer') {
-      const [itemId, action] = params
-      // debugger
-      state[stripColonFromURLParam(urlParams.itemId)] = itemId
-    }
-
-    switch (feature) {
-      case 'bank-accounts': {
-        const [bankId, action] = params
-        state[stripColonFromURLParam(urlParams.bankId)] = bankId
-        break
+  useEffect(() => {
+    const { service, feature, params } = getCurrentLocationData(pathname)
+    const state: any = {}
+    if (params.length > 0) {
+      if (service === 'authorizer') {
+        const [itemId, action] = params
+        state[stripColonFromURLParam(urlParams.itemId)] = itemId
       }
 
-      case 'digital-security': {
-        const [balanceId, action] = params
-        state[stripColonFromURLParam(urlParams.balanceId)] = balanceId
-        break
-      }
-
-      case 'individuals': {
-        const [identityId, action] = params
-        state[stripColonFromURLParam(urlParams.identityId)] = identityId
-        break
-      }
-
-      case 'corporates': {
-        const [identityId, action] = params
-        state[stripColonFromURLParam(urlParams.identityId)] = identityId
-        break
-      }
-
-      case 'commitments': {
-        const [commitmentId, action] = params
-        state[stripColonFromURLParam(urlParams.commitmentId)] = commitmentId
-        break
-      }
-
-      case 'cash-deposits':
-      case 'cash-withdrawals':
-      case 'digital-security-withdrawals': {
-        const [balanceId, action] = params
-        // debugger
-        state[stripColonFromURLParam(urlParams.balanceId)] = balanceId
-        break
-      }
-
-      case 'offerings': {
-        if (service === 'invest') {
-          const [issuerId, dsoId, action] = params
-          state[stripColonFromURLParam(urlParams.issuerId)] = issuerId
-          state[stripColonFromURLParam(urlParams.dsoId)] = dsoId
-        } else {
-          const [dsoId, action] = params
-          state[stripColonFromURLParam(urlParams.dsoId)] = dsoId
+      switch (feature) {
+        case 'bank-accounts': {
+          const [bankId, action] = params
+          state[stripColonFromURLParam(urlParams.bankId)] = bankId
+          break
         }
 
-        break
+        case 'digital-security': {
+          const [balanceId, action] = params
+          state[stripColonFromURLParam(urlParams.balanceId)] = balanceId
+          break
+        }
+
+        case 'individuals': {
+          const [identityId, action] = params
+          state[stripColonFromURLParam(urlParams.identityId)] = identityId
+          break
+        }
+
+        case 'corporates': {
+          const [identityId, action] = params
+          state[stripColonFromURLParam(urlParams.identityId)] = identityId
+          break
+        }
+
+        case 'commitments': {
+          const [commitmentId, action] = params
+          state[stripColonFromURLParam(urlParams.commitmentId)] = commitmentId
+          break
+        }
+
+        case 'cash-deposits':
+        case 'cash-withdrawals':
+        case 'digital-security-withdrawals': {
+          const [balanceId, action] = params
+          state[stripColonFromURLParam(urlParams.balanceId)] = balanceId
+          break
+        }
+
+        case 'offerings': {
+          if (service === 'invest') {
+            const [issuerId, dsoId, action] = params
+            state[stripColonFromURLParam(urlParams.issuerId)] = issuerId
+            state[stripColonFromURLParam(urlParams.dsoId)] = dsoId
+          } else {
+            const [dsoId, action] = params
+            state[stripColonFromURLParam(urlParams.dsoId)] = dsoId
+          }
+
+          break
+        }
+
+        default:
+          return state
       }
-
-      default:
-        return state
     }
-  }
 
-  window.history.pushState(state, 'state')
+    window.history.replaceState(state, 'state')
+  }, [pathname])
 }
