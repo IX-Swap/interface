c81097cb59d45c37b0e3eb8ddee282ac4b768c47
Alex Solovev <alex@investax.io>
2020-11-24 20:00:32 +0800

    Add validation for grouped values in identity forms
    Signed-off-by: Alex Solovev <alex@investax.io>

--- src/v2/validation/validators.ts
+++ src/v2/validation/validators.ts
@@ -1,4 +1,9 @@
 import { isAddress } from '@ethersproject/address'
+import {
+  CorporateDeclarations,
+  DeclarationValue,
+  IndividualDeclarations
+} from 'v2/app/pages/identity/const/declarations'
 
 const passwordPatterns = [/[A-Z]/, /[a-z]/, /[0-9]/, /[^A-Za-z0-9]/]
 
@@ -15,4 +20,48 @@ export const addressValidator = (value: string | null | undefined) => {
   }
 
   return false
+}
+
+export const individualAccreditedInvestorValidator = (
+  value: Partial<IndividualDeclarations> | null | undefined
+) => {
+  if (value === null || value === undefined) {
+    return false
+  }
+
+  const {
+    NetPersonalAssets,
+    IndividualIncome,
+    IndividualFinancialAsset,
+    JointlyHeldAccount
+  } = value
+
+  return (
+    NetPersonalAssets === DeclarationValue.Yes ||
+    IndividualFinancialAsset === DeclarationValue.Yes ||
+    IndividualIncome === DeclarationValue.Yes ||
+    JointlyHeldAccount === DeclarationValue.Yes
+  )
+}
+
+export const corporateAccreditedInvestorValidator = (
+  value: Partial<CorporateDeclarations> | null | undefined
+) => {
+  if (value === null || value === undefined) {
+    return false
+  }
+
+  const {
+    NetAssets,
+    ShareholderIsAccreditedInvestor,
+    AllPartnersAreAccreditedInvestors,
+    AllBeneficiariesAreAccreditedInvestors
+  } = value
+
+  return (
+    NetAssets === DeclarationValue.Yes ||
+    ShareholderIsAccreditedInvestor === DeclarationValue.Yes ||
+    AllPartnersAreAccreditedInvestors === DeclarationValue.Yes ||
+    AllBeneficiariesAreAccreditedInvestors === DeclarationValue.Yes
+  )
 }
