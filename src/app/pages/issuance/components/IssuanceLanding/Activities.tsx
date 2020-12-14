77d8012fbf860c3666133208f3a40057e57fdff8
Alex Solovev <alex@investax.io>
2020-12-14 23:19:11 +0800

    Use real dso activities data
    Signed-off-by: Alex Solovev <alex@investax.io>

--- src/app/pages/issuance/components/IssuanceLanding/Activities.tsx
+++ src/app/pages/issuance/components/IssuanceLanding/Activities.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { VSpacer } from 'components/VSpacer'
 import { ChartWrapper } from 'app/pages/issuance/components/IssuanceLanding/ChartWrapper'
 import { TableView } from 'components/TableWithPagination/TableView'
-import { Commitment } from 'types/commitment'
 import columns from './columns'
 import { useAuth } from 'hooks/auth/useAuth'
 import { getIdFromObj } from 'helpers/strings'
 import { investQueryKeys } from 'config/queryKeys'
 import { useIssuanceRouter } from '../../router'
+import { DSOActivity } from 'types/dso'
 
 export const Activities = () => {
   // TODO: fix switching dso bug
@@ -19,7 +19,7 @@ export const Activities = () => {
   return (
     <ChartWrapper title='Activities'>
       <VSpacer size='small' />
-      <TableView<Commitment>
+      <TableView<DSOActivity>
         uri={url}
         name={investQueryKeys.getCommitmentsByUserId(userId)}
         columns={columns}
--- src/app/pages/issuance/components/IssuanceLanding/columns.tsx
+++ src/app/pages/issuance/components/IssuanceLanding/columns.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { TableColumn } from 'types/util'
 import format from 'date-fns/format'
-import { Commitment } from 'types/commitment'
 import { DSOLogo } from 'app/components/DSO/components/DSOLogo'
 import { Grid, Typography } from '@material-ui/core'
+import { DSOActivity } from 'types/dso'
 
-export const renderActivityDate = (date: string, row: Commitment) =>
+export const renderActivityDate = (date: string, row: DSOActivity) =>
   format(new Date(date), 'MM/dd/yyyy p')
 
-export const renderCommitmentAvatar = (a: string, row: Commitment) => (
+export const renderCommitmentAvatar = (a: string, row: DSOActivity) => (
   <Grid container alignItems='center' spacing={2}>
     <Grid item>
-      <DSOLogo dsoId={row.dso._id} size={40} />
+      <DSOLogo dsoId={row.identity.individual.photo ?? ''} size={40} />
     </Grid>
     <Grid item>
       <Typography variant='h5' style={{ fontSize: '0.875rem' }}>
@@ -23,11 +23,11 @@ export const renderCommitmentAvatar = (a: string, row: Commitment) => (
   </Grid>
 )
 
-export const renderActivity = () => (
-  <Typography>{`Added a new commitment`}</Typography>
+export const renderActivity = (_: any, row: DSOActivity) => (
+  <Typography>{row.invariant}</Typography>
 )
 
-export const columns: Array<TableColumn<Commitment>> = [
+export const columns: Array<TableColumn<DSOActivity>> = [
   {
     key: 'createdAt',
     label: 'Date',
