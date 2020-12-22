71d7da534ce9c8a7d5c1ee043d217b5d5bf5a297
Sumen del Rosario <sumen.delrosario@gmail.com>
2020-12-22 21:14:34 +0800

    add column items for MyDSOsTable

--- src/app/pages/issuance/components/MyDSOsTable.tsx
+++ src/app/pages/issuance/components/MyDSOsTable.tsx
@@ -5,16 +5,23 @@ import { getIdFromObj } from 'helpers/strings'
 import { issuanceURL } from 'config/apiURL'
 import { dsoQueryKeys } from 'config/queryKeys'
 import { columns } from './columns'
+import { Actions } from 'app/pages/invest/components/DSOTable/Actions'
+import { VSpacer } from 'components/VSpacer'
 
 export const MyDSOsTable = () => {
   const { user } = useAuth()
   const userId = getIdFromObj(user)
 
   return (
-    <TableView
-      name={dsoQueryKeys.getDSOsByUserId(userId)}
-      uri={issuanceURL.dso.getByUserId(userId)}
-      columns={columns}
-    />
+    <>
+      <VSpacer size='medium' />
+      <TableView
+        name={dsoQueryKeys.getDSOsByUserId(userId)}
+        uri={issuanceURL.dso.getByUserId(userId)}
+        columns={columns}
+        hasActions
+        actions={Actions}
+      />
+    </>
   )
 }
---
+++ src/app/pages/issuance/components/columns.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { DigitalSecurityOffering } from 'types/dso'
+import { TableColumn } from 'types/util'
+import { DSOLogo } from 'app/components/DSO/components/DSOLogo'
+import { Grid } from '@material-ui/core'
+import { renderPriceWithCurrency } from 'app/pages/invest/components/DSOTable/columns'
+
+export const renderDSOLogo = (dsoId: string) => (
+  <Grid container justify='center' alignItems='center'>
+    <DSOLogo dsoId={dsoId} size={70} />
+  </Grid>
+)
+export const columns: Array<TableColumn<DigitalSecurityOffering>> = [
+  {
+    key: '_id',
+    label: '',
+    render: renderDSOLogo,
+    align: 'center'
+  },
+  {
+    key: 'tokenName',
+    label: 'Name'
+  },
+  {
+    key: 'pricePerUnit',
+    label: 'Price',
+    render: renderPriceWithCurrency,
+    headAlign: 'right',
+    align: 'right'
+  },
+  {
+    key: 'minimumInvestment',
+    label: 'Minimum',
+    render: renderPriceWithCurrency,
+    headAlign: 'right',
+    align: 'right'
+  },
+  {
+    key: 'capitalStructure',
+    label: 'Capital Structure'
+  },
+  {
+    key: 'distributionFrequency',
+    label: 'Distribution Frequency'
+  },
+  {
+    key: 'status',
+    label: 'Status'
+  }
+]
