12b675fab30fc6774f6da265c58d7206c54f319d
varundevpro <28918411+VarunDevPro@users.noreply.github.com>
2020-12-22 19:33:13 +0530

    Added Create DSO button, Amount Raised Percentage & Removed unused files

--- src/app/pages/issuance/components/IssuanceLanding/AmountRaisedChart.tsx
+++ src/app/pages/issuance/components/IssuanceLanding/AmountRaisedChart.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Chart } from 'react-google-charts'
 import { DigitalSecurityOffering } from 'types/dso'
+import { getDSOStats } from 'app/components/DSO/utils'
+import { Box, Typography } from '@material-ui/core'
 
 export interface AmountRaisedChartProps {
   dso: DigitalSecurityOffering
@@ -8,29 +10,42 @@ export interface AmountRaisedChartProps {
 
 export const AmountRaisedChart = ({ dso }: AmountRaisedChartProps) => {
   // TODO: Replace with `getDSOStats` helper
-  const percentRaised =
-    (dso.insight.raisedTotal * 100) / (dso.totalFundraisingAmount ?? 0)
+  const { percentRaised } = getDSOStats(dso)
 
-  const raised = Math.floor(Math.min(100, percentRaised))
+  const raised = Math.floor(percentRaised)
   const toRaise = 100 - raised
 
   return (
-    <Chart
-      chartType='PieChart'
-      loader={<div>Loading Chart</div>}
-      data={[
-        ['Investor', 'Amount'],
-        ['', raised],
-        ['', toRaise]
-      ]}
-      options={{
-        pieHole: 0.75,
-        pieStartAngle: -45,
-        colors: ['green', 'lightgrey'],
-        legend: 'none',
-        width: 72,
-        height: 72
-      }}
-    />
+    <Box style={{ position: 'relative' }}>
+      <Chart
+        chartType='PieChart'
+        loader={<div>Loading Chart</div>}
+        data={[
+          ['Investor', 'Amount'],
+          ['', raised],
+          ['', toRaise]
+        ]}
+        options={{
+          pieHole: 0.75,
+          pieStartAngle: 0,
+          colors: ['rgb(245,189,37)', 'lightgrey'],
+          legend: 'none',
+          width: 72,
+          height: 72
+        }}
+      />
+      <Typography
+        style={{
+          display: 'inline-block',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          fontSize: 9
+        }}
+      >
+        {raised}%
+      </Typography>
+    </Box>
   )
 }
--- src/hooks/location/useDataFromURL.ts
+++ src/hooks/location/useDataFromURL.ts
@@ -86,6 +86,6 @@ export const useDataFromURL = () => {
       }
     }
 
-    replace({ ...location, state })
+    replace({ state })
   }, [pathname]) // eslint-disable-line
 }
