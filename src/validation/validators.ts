2ccc318cf6e073882436e5ccd3114cce19b06e88
Alex Solovev <alex@investax.io>
2021-01-15 16:02:50 +0800

    Improved password validation messages
    Signed-off-by: Alex Solovev <alex@investax.io>

--- src/validation/validators.ts
+++ src/validation/validators.ts
@@ -5,15 +5,42 @@ import {
   DeclarationValue,
   IndividualDeclarations
 } from 'app/pages/identity/const/declarations'
+import { hasValue } from 'helpers/forms'
 
 const passwordPatterns = [/[A-Z]/, /[a-z]/, /[0-9]/, /[^A-Za-z0-9]/]
 
-export const passwordValidator = (value: string | null | undefined) => {
-  if (value !== null && value !== undefined) {
-    return passwordPatterns.every(pattern => pattern.test(value))
-  }
+export const passwordValidator = {
+  uppercaseLettersTest: (value: string | null | undefined) => {
+    if (hasValue<string>(value)) {
+      return passwordPatterns[0].test(value)
+    }
 
-  return false
+    return false
+  },
+
+  lowercaseLettersTest: (value: string | null | undefined) => {
+    if (hasValue<string>(value)) {
+      return passwordPatterns[1].test(value)
+    }
+
+    return false
+  },
+
+  numbersTest: (value: string | null | undefined) => {
+    if (hasValue<string>(value)) {
+      return passwordPatterns[2].test(value)
+    }
+
+    return false
+  },
+
+  specialCharactersTest: (value: string | null | undefined) => {
+    if (hasValue<string>(value)) {
+      return passwordPatterns[3].test(value)
+    }
+
+    return false
+  }
 }
 
 export const addressValidator = (value: string | null | undefined) => {
