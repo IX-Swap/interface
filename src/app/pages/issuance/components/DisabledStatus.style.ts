969fcef7bbd6fd51ec4291bac517b68a79e3b260
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-01-18 17:43:43 +0800

    Disabled Status component created

---
+++ src/app/pages/issuance/components/DisabledStatus.style.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useStyles = makeStyles(theme => ({
+  disabledStatus: {
+    height: 27,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '0 28px',
+    border: `1px solid ${theme.palette.text.secondary}`,
+    borderRadius: 4
+  }
+}))
