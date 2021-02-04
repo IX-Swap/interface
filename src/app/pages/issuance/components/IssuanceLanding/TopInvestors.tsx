80529bbde87092c39431b03fb28fe296ef4406b9
Alex Solovev <alex@investax.io>
2021-02-04 17:32:31 +0800

    Fixed google-charts colors for dark theme
    Signed-off-by: Alex Solovev <alex@investax.io>

--- src/app/pages/issuance/components/IssuanceLanding/TopInvestors.tsx
+++ src/app/pages/issuance/components/IssuanceLanding/TopInvestors.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Chart } from 'react-google-charts'
 import { ChartWrapper } from 'app/pages/issuance/components/IssuanceLanding/ChartWrapper'
 import { useTopInvestors } from 'app/pages/issuance/hooks/useTopInvestors'
+import { useTheme } from '@material-ui/core/styles'
 
 export const TopInvestors = () => {
   const { data, isLoading } = useTopInvestors()
+  const theme = useTheme()
 
   if (isLoading) {
     return null
@@ -28,8 +30,12 @@ export const TopInvestors = () => {
           pieHole: 0.5,
           pieStartAngle: -45,
           colors: hasData ? undefined : ['lightgrey'],
+          backgroundColor: 'transparent',
           legend: {
-            position: 'bottom'
+            position: 'bottom',
+            textStyle: {
+              color: theme.palette.getContrastText(theme.palette.backgrounds.main as any)
+            }
           },
           enableInteractivity: hasData
         }}
