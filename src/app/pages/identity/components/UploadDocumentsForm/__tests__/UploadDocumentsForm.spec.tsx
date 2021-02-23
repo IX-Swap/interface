8309843a9e4ae66542a8e490ba09c6d3819dd35e
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-02-23 16:05:26 +0800

    Corporate Upload Documents created

--- src/app/pages/identity/components/UploadDocumentsForm/__tests__/UploadDocumentsForm.spec.tsx
+++ src/app/pages/identity/components/UploadDocumentsForm/__tests__/UploadDocumentsForm.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, cleanup } from 'test-utils'
 import { UploadDocumentsForm } from 'app/pages/identity/components/UploadDocumentsForm/UploadDocumentsForm'
-import { UploadDocumentField } from 'app/pages/identity/components/UploadDocumentsForm/UploadDocumentField/UploadDocumentField'
 
 jest.mock(
   'app/pages/identity/components/UploadDocumentsForm/UploadDocumentField/UploadDocumentField',
@@ -10,44 +9,17 @@ jest.mock(
   })
 )
 
-describe('UploadDocumentsForm', () => {
+describe('IndividualUploadDocumentsForm', () => {
   afterEach(async () => {
     await cleanup()
     jest.clearAllMocks()
   })
 
   it('renders without errors', () => {
-    render(<UploadDocumentsForm />)
-  })
-
-  it('renders Upload fields correctly', () => {
-    render(<UploadDocumentsForm />)
-
-    expect(UploadDocumentField).toHaveBeenNthCalledWith(
-      1,
-      expect.objectContaining({
-        name: 'documents.proofOfIdentity',
-        label: 'Proof of Identity'
-      }),
-      {}
-    )
-
-    expect(UploadDocumentField).toHaveBeenNthCalledWith(
-      2,
-      expect.objectContaining({
-        name: 'documents.proofOfAddress',
-        label: 'Proof of Address'
-      }),
-      {}
-    )
-
-    expect(UploadDocumentField).toHaveBeenNthCalledWith(
-      3,
-      expect.objectContaining({
-        name: 'documents.evidenceOfAccreditation',
-        label: 'Evidence of Accreditation'
-      }),
-      {}
+    render(
+      <UploadDocumentsForm identityType='individual'>
+        <div />
+      </UploadDocumentsForm>
     )
   })
 })
