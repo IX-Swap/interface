815c6543c82c6604a504aacb5b6c34b3ce98ad27
Alex Solovev <alex@investax.io>
2021-03-08 16:22:03 +0800

    Fixed merge conflict
    Signed-off-by: Alex Solovev <alex@investax.io>

--- src/app/pages/identity/pages/individual/IndividualIdCreate.tsx
+++ src/app/pages/identity/pages/individual/IndividualIdCreate.tsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react'
 import { Grid } from '@material-ui/core'
 import { VSpacer } from 'components/VSpacer'
 import { useOnboardingDialog } from 'app/components/OnboardingDialog/hooks/useOnboardingDialog'
+import { CreateIndividual } from 'app/pages/_identity/pages/CreateIndividual'
 
 export const IndividualIdCreate: React.FC = () => {
-  const { paths } = useIdentitiesRouter()
   const { showPreIdentityCreateDialog } = useOnboardingDialog()
 
   useEffect(() => {
