613945cac5541d3dc16015888e7207299db61600
Глеб <gleb@MacBook-Pro-Gleb.local>
2021-03-18 15:40:35 +0300

    Added Error Page with new UI

---
+++ src/components/Page404/Page404.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Button, Box, Grid, Typography, Link } from '@material-ui/core'
+import { useTheme } from '@material-ui/core/styles'
+import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
+import { AppRouterLinkComponent } from 'components/AppRouterLink'
+import { AppLogo } from 'app/components/AppLogo/AppLogo'
+import { IconMail } from 'components/Page404/IconMail'
+import useStyles from './Pages404.style'
+
+export const Page404 = () => {
+  const theme = useTheme()
+  const classes = useStyles()
+  const { isMobile } = useAppBreakpoints()
+  const isLightThemeOn = theme.palette.type === 'light'
+
+  return (
+    <Box className={classes.container}>
+      <Grid container direction={'column'} className={classes.wrapper}>
+        <Grid
+          item
+          container
+          justify={isMobile ? 'center' : 'flex-start'}
+          alignItems={'flex-start'}
+          className={classes.topBlock}
+        >
+          <Grid item>
+            <AppLogo color={isLightThemeOn} />
+          </Grid>
+        </Grid>
+
+        <Grid
+          className={classes.mainContent}
+          container
+          spacing={2}
+          direction='column'
+          justify='center'
+          alignItems='center'
+        >
+          <Grid item>
+            <Typography
+              variant={'h1'}
+              align={'center'}
+              className={classes.title}
+            >
+              We lost this page
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <Typography
+              variant='body1'
+              align={'center'}
+              className={classes.description}
+            >
+              Our best engineers are fixing the problem. In the meantime, you
+              can explore our exciting features.
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <Button
+              component={AppRouterLinkComponent}
+              to='/'
+              color={'primary'}
+              variant={'contained'}
+              className={classes.button}
+            >
+              Return to home page
+            </Button>
+          </Grid>
+
+          <Grid item>
+            <Link href={'mailto:support@investax.io'} className={classes.link}>
+              <IconMail
+                fill={theme.palette.getContrastText(
+                  theme.palette.backgrounds.main
+                )}
+              />
+              <Typography variant={'body1'} className={classes.linkText}>
+                support@investax.io
+              </Typography>
+            </Link>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
