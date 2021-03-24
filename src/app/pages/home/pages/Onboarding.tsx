1722323b4363fe25473678526a89236c053add91
Глеб <gleb@MacBook-Pro-Gleb.local>
2021-03-24 14:36:27 +0300

    Corrected comments

--- src/app/pages/home/pages/Onboarding.tsx
+++ src/app/pages/home/pages/Onboarding.tsx
@@ -8,19 +8,12 @@ import { TopIssuers } from 'app/pages/home/components/TopIssuers'
 import { TopCorporates } from 'app/pages/home/components/TopCorporates'
 import { PromoBanner } from 'app/pages/invest/components/PromoBanner'
 import { Divider } from 'ui/Divider'
-import { useOnboardingJourneys } from 'app/components/OnboardingPanel/hooks/useOnboardingJourneys'
-import { LoadingFullScreen } from 'auth/components/LoadingFullScreen'
 
 export const Onboarding = () => {
   const { user } = useAuth()
   const label = `Welcome, ${user?.name ?? 'Unknown'}`
-  const { isIdentitiesLoaded } = useOnboardingJourneys()
 
   useSetPageTitle(label)
-
-  if (!isIdentitiesLoaded) {
-    return <LoadingFullScreen />
-  }
 
   return (
     <Fragment>
