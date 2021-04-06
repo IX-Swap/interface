3007da61cd8a5d6c149a047dcb4c97b3f863e6b0
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-04-06 15:50:51 +0800

    Issuance Details Create and Thankyou dialog box added

--- src/app/pages/_identity/hooks/useDetailsOfIssuance.ts
+++ src/app/pages/_identity/hooks/useDetailsOfIssuance.ts
@@ -18,7 +18,8 @@ export const useDetailsOfIssuance = () => {
 
   const { data, ...rest } = useQuery(
     [identityQueryKeys.getDetailsOfIssuance(userId)],
-    getDetailsOfIssuance
+    getDetailsOfIssuance,
+    { retry: false }
   )
 
   return {
--- src/app/pages/_identity/hooks/useSubmitDetailsOfIssuance.ts
+++ src/app/pages/_identity/hooks/useSubmitDetailsOfIssuance.ts
@@ -1,3 +1,4 @@
+import { useOnboardingDialog } from 'app/components/OnboardingDialog/hooks/useOnboardingDialog'
 import { identityURL } from 'config/apiURL'
 import { identityQueryKeys } from 'config/queryKeys'
 import { useServices } from 'hooks/useServices'
@@ -6,6 +7,7 @@ import { useMutation, useQueryCache } from 'react-query'
 export const useSubmitDetailsOfIssuance = (issuanceId: string) => {
   const { apiService, snackbarService } = useServices()
   const queryCache = useQueryCache()
+  const { showSubmitDetailsOfIssuanceDialog } = useOnboardingDialog()
 
   const updateDetailsOfIssuance = async () => {
     const uri = identityURL.detailsOfIssuance.submit(issuanceId)
@@ -16,6 +18,7 @@ export const useSubmitDetailsOfIssuance = (issuanceId: string) => {
     onSuccess: data => {
       void snackbarService.showSnackbar(data.message, 'success')
       void queryCache.invalidateQueries(identityQueryKeys.getDetailsOfIssuance)
+      showSubmitDetailsOfIssuanceDialog()
     },
     onError: (error: any) => {
       void snackbarService.showSnackbar(error.message, 'error')
