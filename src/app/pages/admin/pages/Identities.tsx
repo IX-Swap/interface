1b979a6a4094b149560121613e6b44e9c929df0a
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-04-21 19:04:55 +0800

    remove button from the Identities page

--- src/app/pages/admin/pages/Identities.tsx
+++ src/app/pages/admin/pages/Identities.tsx
@@ -1,6 +1,5 @@
 import { Grid } from '@material-ui/core'
 import { PageHeader } from 'app/components/PageHeader/PageHeader'
-import { AddVirtualAccountsButton } from 'app/pages/admin/components/AddVirtualAccountsButton/AddVirtualAccountsButton'
 import React from 'react'
 
 export const Identities = () => {
@@ -8,9 +7,6 @@ export const Identities = () => {
     <Grid container direction='column'>
       <Grid item xs={12}>
         <PageHeader title='Identities' />
-      </Grid>
-      <Grid item xs={12}>
-        <AddVirtualAccountsButton />
       </Grid>
     </Grid>
   )
