20d0c53f88f66c8a1143d72d8a2192e02edac4b5
Глеб Винокуров <glebvinokurov@MacBook-Pro-Gleb-2.local>
2021-04-29 22:41:35 +0300

    Added more tests

--- src/app/pages/authorizer/pages/issuanceDetails/__tests__/IssuanceDetailsAuthorization.spec.tsx
+++ src/app/pages/authorizer/pages/issuanceDetails/__tests__/IssuanceDetailsAuthorization.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render, cleanup } from 'test-utils'
 import { IssuanceDetailsAuthorization } from 'app/pages/authorizer/pages/issuanceDetails/IssuanceDetailsAuthorization'
+import { generateQueryResult } from '__fixtures__/useQuery'
+import * as useDetailsOfIssuance from 'app/pages/identity/hooks/useDetailsOfIssuance'
 
 describe('IssuanceDetailsAuthorization', () => {
   afterEach(async () => {
@@ -9,5 +11,33 @@ describe('IssuanceDetailsAuthorization', () => {
 
   it('renders without errors', async () => {
     render(<IssuanceDetailsAuthorization />)
+  })
+
+  it('renders empty container when data is undefined', () => {
+    const useDetailsOfIssuanceLoading = generateQueryResult({
+      data: undefined
+    })
+
+    jest
+      .spyOn(useDetailsOfIssuance, 'useDetailsOfIssuance')
+      .mockImplementation(() => useDetailsOfIssuanceLoading as any)
+
+    const { container } = render(<IssuanceDetailsAuthorization />)
+
+    expect(container).toBeEmpty()
+  })
+
+  it('renders empty container when isLoading', () => {
+    const useDetailsOfIssuanceLoading = generateQueryResult({
+      isLoading: true
+    })
+
+    jest
+      .spyOn(useDetailsOfIssuance, 'useDetailsOfIssuance')
+      .mockImplementation(() => useDetailsOfIssuanceLoading as any)
+
+    const { container } = render(<IssuanceDetailsAuthorization />)
+
+    expect(container).toBeEmpty()
   })
 })
