ca5e22295f7d9f6b27589abb7519af7852b7dbb0
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-05-05 14:25:43 +0800

    Tax Declaration dialog removed

--- src/app/pages/identity/components/TaxDeclarationForm/__tests__/TaxDeclarationForm.spec.tsx
+++ src/app/pages/identity/components/TaxDeclarationForm/__tests__/TaxDeclarationForm.spec.tsx
@@ -1,16 +1,8 @@
 import { TaxDeclarationForm } from 'app/pages/identity/components/TaxDeclarationForm/TaxDeclarationForm'
 import React from 'react'
 import { render, cleanup } from 'test-utils'
-import { TaxDeclarationInfoDialog } from 'app/pages/identity/components/TaxDeclarationForm/TaxDeclarationInfoDialog/TaxDeclarationInfoDialog'
 import { TaxResidencyFields } from 'app/pages/identity/components/TaxDeclarationForm/TaxResidencyFields/TaxResidencyFields'
 import { UsCitizenshipConfirmation } from 'app/pages/identity/components/TaxDeclarationForm/UsCitizenshipConfirmation/UsCitizenshipConfirmation'
-
-jest.mock(
-  'app/pages/identity/components/TaxDeclarationForm/TaxDeclarationInfoDialog/TaxDeclarationInfoDialog',
-  () => ({
-    TaxDeclarationInfoDialog: jest.fn(() => null)
-  })
-)
 
 jest.mock(
   'app/pages/identity/components/TaxDeclarationForm/TaxResidencyFields/TaxResidencyFields',
@@ -39,7 +31,6 @@ describe('TaxDeclarationForm', () => {
   it('renders child components correctly', () => {
     render(<TaxDeclarationForm />)
 
-    expect(TaxDeclarationInfoDialog).toHaveBeenCalled()
     expect(TaxResidencyFields).toHaveBeenCalled()
     expect(UsCitizenshipConfirmation).toHaveBeenCalled()
   })
