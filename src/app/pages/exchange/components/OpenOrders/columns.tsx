05316aa56276fd7e6ea4e6972704f056065700a1
Alex Solovev <alex@investax.io>
2021-05-18 14:19:47 +0800

    Open Orders table
    Signed-off-by: Alex Solovev <alex@investax.io>

---
+++ src/app/pages/exchange/components/OpenOrders/columns.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { CancelOrderButton } from 'app/pages/exchange/components/OpenOrders/CancelOrderButton'
+import { formatDateToMMDDYY } from 'helpers/dates'
+import { formatMoney } from 'helpers/numbers'
+import { renderPercentage } from 'helpers/rendering'
+import { TableColumn } from 'types/util'
+
+export interface Order {
+  createdAt: string
+  side: string
+  tif: string
+  price: number
+  amount: number
+  total: number
+  filled: number
+}
+
+export const columns: Array<TableColumn<Order>> = [
+  {
+    key: 'createdAt',
+    label: 'Date',
+    render: formatDateToMMDDYY
+  },
+  {
+    key: 'side',
+    label: 'Side'
+  },
+  {
+    key: 'tif',
+    label: 'Time-In-Force'
+  },
+  {
+    key: 'price',
+    label: 'Price',
+    render: (_, value) => formatMoney(value.price, '')
+  },
+  {
+    key: 'amount',
+    label: 'Amount',
+    render: (_, value) => formatMoney(value.amount, '')
+  },
+  {
+    key: 'total',
+    label: 'Total',
+    render: (_, value) => formatMoney(value.total, '')
+  },
+  {
+    key: 'filled',
+    label: 'Filled',
+    render: renderPercentage
+  },
+  {
+    key: 'cancel',
+    label: '',
+    render: (_, order) => <CancelOrderButton order={order} />
+  }
+]
