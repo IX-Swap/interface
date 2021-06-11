88a254359a6c5fc86fa655cd54adc2ee5ec6f371
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-06-11 14:08:49 +0800

    replace useResetSelectionOnUnmount hook with useUnmountCallback

--- src/app/pages/admin/components/AssignedVirtualAccountsTable/AssignedVirtualAccountsTable.tsx
+++ src/app/pages/admin/components/AssignedVirtualAccountsTable/AssignedVirtualAccountsTable.tsx
@@ -2,19 +2,20 @@ import { Grid } from '@material-ui/core'
 import { Actions } from 'app/pages/admin/components/AssignedVirtualAccountsTable/Actions'
 import { columns } from 'app/pages/admin/components/AssignedVirtualAccountsTable/columns'
 import { Filters } from 'app/pages/admin/components/AssignedVirtualAccountsTable/Filters'
-import { useResetSelectionOnUnmount } from 'app/pages/admin/hooks/useResetSelectionOnUnmount'
 import { useSelectionHelperContext } from 'components/SelectionHelper'
 import { TableView } from 'components/TableWithPagination/TableView'
 import { virtualAccounts } from 'config/apiURL'
 import { virtualAccountQueryKeys } from 'config/queryKeys'
 import { useQueryFilter } from 'hooks/filters/useQueryFilter'
+import { useUnmountCallback } from 'hooks/useUnmountCallback'
 import React from 'react'
 import { VirtualAccount } from 'types/virtualAccount'
 
 export const AssignedVirtualAccountsTable = () => {
-  useResetSelectionOnUnmount()
   const { getFilterValue } = useQueryFilter()
   const selectionHelperContext = useSelectionHelperContext()
+
+  useUnmountCallback(selectionHelperContext.resetSelection)
 
   const currencyFilterValue = getFilterValue('currency')
   const bothCurrency =
