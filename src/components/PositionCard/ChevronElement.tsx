ff254710d46487b769f0a62d3888bd82e9ec8a20
Victoria Florea <victoria.florea@titanium-soft.com>
2021-06-17 15:54:18 +0300

    [mk5und] fix chevron

--- src/components/PositionCard/ChevronElement.tsx
+++ src/components/PositionCard/ChevronElement.tsx
@@ -1,26 +1,31 @@
 import React from 'react'
-import { ChevronRight, ChevronLeft } from 'react-feather'
+import { ChevronUp } from 'react-feather'
 import { ButtonEmpty } from '../Button'
 import { RowFixed } from '../Row'
 
 import useTheme from 'hooks/useTheme'
+import styled from 'styled-components'
 
 interface Props {
   showMore: boolean
   setShowMore: (arg0: boolean) => void
 }
 
+const StyledChevron = styled(ChevronUp)<{ showMore: boolean }>`
+  transform: ${({ showMore }) => (!showMore ? 'rotate(180deg)' : '')};
+  transition: 0.4s;
+`
 export const ChevronElement = ({ showMore, setShowMore }: Props) => {
   const theme = useTheme()
   return (
     <RowFixed gap="8px">
       <ButtonEmpty padding="6px 8px" borderRadius="12px" width="100%" onClick={() => setShowMore(!showMore)}>
-        {showMore && (
-          <ChevronLeft size="20" color={theme.text2} style={{ marginLeft: '8px', height: '20px', minWidth: '20px' }} />
-        )}
-        {!showMore && (
-          <ChevronRight size="20" color={theme.text2} style={{ marginLeft: '8px', height: '20px', minWidth: '20px' }} />
-        )}
+        <StyledChevron
+          size="20"
+          color={theme.text2}
+          style={{ marginLeft: '8px', height: '20px', minWidth: '20px' }}
+          showMore={showMore}
+        />
       </ButtonEmpty>
     </RowFixed>
   )
