f240c1eac9936f2f0cf0538271b27afab3745342
Глеб Винокуров <glebvinokurov@MacBook-Pro-Gleb-2.local>
2021-06-22 20:33:30 +0300

    Added pagination for PrimaryOfferings and OTCMarkets sections

--- src/app/pages/invest/components/OTCMarkets.tsx
+++ src/app/pages/invest/components/OTCMarkets.tsx
@@ -1,31 +1,68 @@
 import React from 'react'
 import { InvestRoute } from 'app/pages/invest/router/config'
-import { Grid } from '@material-ui/core'
+import { Grid, TablePagination } from '@material-ui/core'
 import { OTCMarketCard } from 'app/pages/invest/components/OTCMarketCard/OTCMarketCard'
 import useStyles from 'app/pages/invest/components/styles/OTCMarket.style'
-import { useOTCMarketsList } from 'app/pages/invest/hooks/useOTCMarketsList'
+import { useTableWithPagination } from 'components/TableWithPagination/hooks/useTableWithPagination'
+import { OTCUrl } from 'config/apiURL'
+import { otcQueryKeys } from 'config/queryKeys'
+import { DigitalSecurityOffering } from 'types/dso'
 import { useQueryFilter } from 'hooks/filters/useQueryFilter'
 
 export const OTCMarket = () => {
   const { getFilterValue } = useQueryFilter()
   const search = getFilterValue('search')
   const otcMarketSearch = getFilterValue('otcMarketSearch')
-  const { data, status } = useOTCMarketsList(search ?? otcMarketSearch)
+  const {
+    items,
+    status,
+    page,
+    setPage,
+    setRowsPerPage,
+    rowsPerPage,
+    total
+  } = useTableWithPagination(
+    otcQueryKeys.getApprovedListingsList,
+    OTCUrl.getApprovedListingsList,
+    { search: search ?? otcMarketSearch },
+    true,
+    5,
+    true
+  )
   const classes = useStyles()
 
-  if (status === 'loading' || data.list.length === undefined) {
+  if (status === 'loading' || items.length === undefined) {
     return null
   }
 
-  const approvedListingsList = data.list
-
   return (
-    <Grid container wrap={'wrap'} className={classes.container}>
-      {approvedListingsList.map((otc, i) => (
-        <Grid item key={otc._id}>
-          <OTCMarketCard type={'OTC'} data={otc} viewURL={InvestRoute.view} />
-        </Grid>
-      ))}
+    <Grid container justify={'flex-end'}>
+      <Grid container item wrap={'wrap'} className={classes.container}>
+        {(items as DigitalSecurityOffering[]).map((otc, i) => (
+          <Grid item key={otc._id}>
+            <OTCMarketCard type={'OTC'} data={otc} viewURL={InvestRoute.view} />
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item style={{ marginTop: 14 }}>
+        {total > 0 && (
+          <TablePagination
+            component='div'
+            style={{ width: '100%', borderBottom: 'none' }}
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            count={total}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangeRowsPerPage={evt => {
+              setPage(0)
+              setRowsPerPage(parseInt(evt.target.value))
+            }}
+            onChangePage={(evt, newPage: number) => {
+              setPage(newPage)
+            }}
+          />
+        )}
+      </Grid>
     </Grid>
   )
 }
--- src/app/pages/invest/components/PrimaryOfferings.tsx
+++ src/app/pages/invest/components/PrimaryOfferings.tsx
@@ -1,35 +1,73 @@
 import React from 'react'
-import { usePromotedDSOs } from 'app/pages/invest/hooks/usePromotedDSOs'
 import { InvestRoute } from 'app/pages/invest/router/config'
-import { Grid } from '@material-ui/core'
+import { Grid, TablePagination } from '@material-ui/core'
 import { OTCMarketCard } from 'app/pages/invest/components/OTCMarketCard/OTCMarketCard'
 import useStyles from 'app/pages/invest/components/styles/OTCMarket.style'
+import { useTableWithPagination } from 'components/TableWithPagination/hooks/useTableWithPagination'
+import { dsoQueryKeys } from 'config/queryKeys'
+import { issuanceURL } from 'config/apiURL'
+import { DigitalSecurityOffering } from 'types/dso'
 import { useQueryFilter } from 'hooks/filters/useQueryFilter'
 
 export const PrimaryOfferings = () => {
+  const classes = useStyles()
   const { getFilterValue } = useQueryFilter()
   const search = getFilterValue('search')
   const primaryOfferingSearch = getFilterValue('primaryOfferingSearch')
-  const { data, status } = usePromotedDSOs(search ?? primaryOfferingSearch)
-  const classes = useStyles()
 
-  if (status === 'loading' || data.list.length === undefined) {
+  const {
+    items,
+    status,
+    page,
+    setPage,
+    setRowsPerPage,
+    rowsPerPage,
+    total
+  } = useTableWithPagination(
+    dsoQueryKeys.getPromoted,
+    issuanceURL.dso.getAllPromoted,
+    { search: search ?? primaryOfferingSearch },
+    true,
+    5,
+    true
+  )
+
+  if (status === 'loading' || items.length === undefined) {
     return null
   }
 
-  const promotedDSOs = data.list
-
   return (
-    <Grid container wrap={'wrap'} className={classes.container}>
-      {promotedDSOs.map((dso, i) => (
-        <Grid item key={dso._id}>
-          <OTCMarketCard
-            type={'Primary'}
-            data={dso}
-            viewURL={InvestRoute.view}
+    <Grid container justify={'flex-end'}>
+      <Grid container item wrap={'wrap'} className={classes.container}>
+        {(items as DigitalSecurityOffering[]).map((dso, i) => (
+          <Grid item key={dso._id}>
+            <OTCMarketCard
+              type={'Primary'}
+              data={dso}
+              viewURL={InvestRoute.view}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item style={{ marginTop: 14 }}>
+        {total > 0 && (
+          <TablePagination
+            component='div'
+            style={{ width: '100%', borderBottom: 'none' }}
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            count={total}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangeRowsPerPage={evt => {
+              setPage(0)
+              setRowsPerPage(parseInt(evt.target.value))
+            }}
+            onChangePage={(evt, newPage: number) => {
+              setPage(newPage)
+            }}
           />
-        </Grid>
-      ))}
+        )}
+      </Grid>
     </Grid>
   )
 }
