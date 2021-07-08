231bf0f3a9ec152880ba16b1740ac879c5405ca5
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-07-08 15:12:12 +0800

    VirtualAccountBalance component created

---
+++ src/app/pages/invest/components/VirtualAccountBalance/VirtualAccountBalance.styles.ts
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core/styles'
+import TopographyImage from 'assets/images/topography-pattern.png'
+
+export const useStyles = makeStyles(theme => ({
+  container: {
+    backgroundImage: `linear-gradient(90deg, #113B7D, #2A67C4)`
+  },
+  cardContent: {
+    backgroundImage: `url(${TopographyImage})`,
+    paddingLeft: 24,
+    paddingRight: 24,
+    '&:last-child': {
+      paddingBottom: 16
+    }
+  },
+  textContent: {
+    fontSize: 18,
+    lineHeight: '40px',
+    color: '#fff'
+  },
+  currencySelect: {
+    backgroundColor: '#fff'
+  },
+  depositLink: {
+    height: 40,
+    backgroundColor: '#fff',
+    '&:hover': {
+      backgroundColor: '#fff'
+    }
+  }
+}))
