dd4ff709120e6d4bf8025da861379764052e0872
Automation-Luch <lopezmichaela1b031@gmail.com>
2021-07-09 17:12:34 +0300

    cryptocurrency swap

---
+++ __tests__/lib/launch-settings.ts
@@ -0,0 +1,31 @@
+import { chromium } from 'playwright'
+import { SwapIX } from './page-objects/ixswap'
+import { Metamask } from './page-objects/metamask'
+import { navigate, getRequest } from './helpers/helpers'
+
+async function launchPersistent() {
+  const ARGS = [`--disable-extensions-except=${__dirname + '/metamask'}`, `--load-extension=${__dirname + '/metamask'}`]
+  const userDataDir = ''
+  const context = await chromium.launchPersistentContext(userDataDir, {
+    headless: false,
+    args: ARGS,
+    timeout: 50000,
+    viewport: { width: 1720, height: 880 },
+  })
+  // Connect ot the Metamask
+  const page = await context.newPage()
+  await navigate('http://localhost:3000/#/swap', page)
+  await context.pages()[0].close()
+  const metamaskPage = (await context.pages())[1]
+  const metamaskAuth = new Metamask(metamaskPage)
+  await metamaskAuth.loginToMetamask()
+  await page.reload()
+  await metamaskPage.close()
+  const wallet = new SwapIX(page)
+  await wallet.connectToWallet()
+  await page.waitForTimeout(5000)
+  await metamaskAuth.confirmConnection((await context.pages())[1])
+  return [page, context]
+}
+
+export { launchPersistent }
