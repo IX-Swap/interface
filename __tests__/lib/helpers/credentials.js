9073eb56acde7eddc42c23d600baf53da6cc6f1b
Automation-Luch <lopezmichaela1b031@gmail.com>
2021-07-13 14:15:55 +0300

    ci

--- __tests__/lib/helpers/credentials.js
+++ __tests__/lib/helpers/credentials.js
@@ -6,4 +6,9 @@ module.exports = {
     SECRET_WORDS: 'prosper weasel indicate flock candy person there buyer direct repeat detail ahead',
     PASSWORD: 'i1iarydotat!!!111',
   },
+
+  ixswap: {
+    URL: '',
+    PASSWORD: '',
+  },
 }
--- __tests__/lib/helpers/text-helpers.js
+++ __tests__/lib/helpers/text-helpers.js
@@ -8,19 +8,7 @@ module.exports = {
     ADD_GROUP: 'This may take up to 30 seconds. Please wait and then refresh the page.',
   },
 
-  groups: {
-    WHATS_APP: {
-      link: 'https://chat.whatsapp.com/E4tw5cpgOh2EEA63G082Xe',
-      name: 'ahaha-Test-alty',
-    },
 
-    TELEGRAM: {
-      link: 'https://t.me/Che_job',
-      name: 'Cherkasy_Job',
-    },
   },
-  stackoverflow: {
-    LINK: 'https://stackoverflow.com',
-    TITLE: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
-  },
+
 };
