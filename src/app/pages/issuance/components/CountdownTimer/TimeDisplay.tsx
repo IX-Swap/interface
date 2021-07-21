770c918803ccc45a86a0ddc934ce05ca4d36ccc1
Глеб Винокуров <glebvinokurov@MacBook-Pro-Gleb-2.local>
2021-07-21 16:07:36 +0600

    Upgrade UI for CountdownTimer component

--- src/app/pages/issuance/components/CountdownTimer/TimeDisplay.tsx
+++ src/app/pages/issuance/components/CountdownTimer/TimeDisplay.tsx
@@ -6,19 +6,35 @@ import { UnitMap } from 'app/pages/issuance/hooks/useCountdown'
 export interface TimeDisplayProps {
   unitsToDisplay: string[]
   units: UnitMap
+  isNewThemeOn?: boolean
 }
 
-export const TimeDisplay = ({ unitsToDisplay, units }: TimeDisplayProps) => {
+export const TimeDisplay = ({
+  unitsToDisplay,
+  units,
+  isNewThemeOn = false
+}: TimeDisplayProps) => {
+  const getLabel = (label: string) => {
+    if (isNewThemeOn && label === 'minutes') {
+      return 'mins'
+    }
+    return label
+  }
   return (
     <Grid container alignItems='flex-start' justify='center'>
       {unitsToDisplay.map((unit, i) => (
         <React.Fragment key={unit}>
-          <TimeUnit time={units[unit]} label={unit} />
-          {i < unitsToDisplay.length - 1 ? (
+          <TimeUnit
+            time={units[unit]}
+            label={getLabel(unit)}
+            isNewThemeOn={isNewThemeOn}
+          />
+          {i < unitsToDisplay.length - 1 && !isNewThemeOn ? (
             <Box>
               <Typography variant='h3'>:</Typography>
             </Box>
           ) : null}
+          {isNewThemeOn ? <Box width={8} /> : null}
         </React.Fragment>
       ))}
     </Grid>
--- src/app/pages/issuance/pages/Commitments.tsx
+++ src/app/pages/issuance/pages/Commitments.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react'
-import { Card, Grid, Hidden } from '@material-ui/core'
+import { Card, Grid } from '@material-ui/core'
 import { CountdownTimer } from '../components/CountdownTimer/CountdownTimer'
 import { AmountRaised } from '../components/IssuanceLanding/AmountRaised'
 import { useDSOById } from 'app/pages/invest/hooks/useDSOById'
 import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
-import { VSpacer } from 'components/VSpacer'
 import { useParams } from 'react-router-dom'
 import { TargetFundraise } from 'app/pages/issuance/components/IssuanceLanding/TargetFundraise'
 import { generatePath } from 'react-router'
 import { history } from 'config/history'
 import { PageHeader } from 'app/pages/issuance/components/Commitments/PageHeader'
 import { InvestorCommitmentTable } from 'app/pages/issuance/components/Commitments/InvestorCommitmentTable'
+import { VSpacer } from 'components/VSpacer'
 
 export const Commitments = () => {
   // TODO Remove this after complete backend api endpoints
@@ -26,14 +26,6 @@ export const Commitments = () => {
   const { data } = useDSOById(dsoId, issuerId)
   const { theme, isTablet } = useAppBreakpoints()
 
-  const divider = (
-    <Hidden mdUp>
-      <Grid item xs={12}>
-        <VSpacer size='small' />
-      </Grid>
-    </Hidden>
-  )
-
   return (
     <>
       <PageHeader title={data?.tokenName} />
@@ -42,15 +34,7 @@ export const Commitments = () => {
         justify='space-between'
         wrap={!isTablet ? 'wrap' : 'wrap-reverse'}
       >
-        <Grid
-          item
-          container
-          direction='row'
-          spacing={isTablet ? 0 : 3}
-          style={{ marginBottom: theme.spacing(1.5) }}
-          xs={12}
-          md={8}
-        >
+        <Grid item container direction='row' spacing={isTablet ? 0 : 3} xs={12}>
           <Grid item xs={12} md={4} lg={4}>
             <Card
               variant='outlined'
@@ -60,8 +44,6 @@ export const Commitments = () => {
             </Card>
           </Grid>
 
-          {divider}
-
           <Grid item xs={12} md={4} lg={4}>
             <Card
               variant='outlined'
@@ -70,20 +52,14 @@ export const Commitments = () => {
               <TargetFundraise isNewThemeOn />
             </Card>
           </Grid>
-        </Grid>
 
-        {divider}
-
-        <Grid
-          container
-          item
-          xs={12}
-          md={4}
-          style={{ marginBottom: isTablet ? 0 : theme.spacing(3) }}
-        >
-          <CountdownTimer />
+          <Grid item style={{ marginLeft: 'auto' }}>
+            <CountdownTimer my={0} mx={0} isNewThemeOn />
+          </Grid>
         </Grid>
       </Grid>
+
+      <VSpacer size={'extraMedium'} />
 
       <Grid container>
         <Grid item xs={12} style={{ paddingTop: 0, paddingLeft: 0 }}>
