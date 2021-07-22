f54d213a6eb9218dc21144b826fa6397ba8e4fa5
Глеб Винокуров <glebvinokurov@MacBook-Pro-Gleb-2.local>
2021-07-22 17:00:01 +0600

    Added CapitalCall button

--- src/app/pages/issuance/components/Commitments/CommitmentTableFilters.tsx
+++ src/app/pages/issuance/components/Commitments/CommitmentTableFilters.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
 import { SearchFilter } from 'app/components/SearchFilter'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { useTheme } from '@material-ui/core/styles'
@@ -16,8 +16,23 @@ export const CommitmentTableFilter = () => {
           <SearchFilter fullWidth placeholder='Search Name' />
         </Grid>
 
-        <Grid item xs={12} md={2}>
-          <FundStatusFilter />
+        <Grid item container xs={12} md={3} lg={3} justify={'space-between'}>
+          <Grid item xs={12} md={5} lg={6}>
+            <FundStatusFilter />
+          </Grid>
+          <Grid item xs={12} md={7} lg={6} container justify={'flex-end'}>
+            <Button
+              variant={'contained'}
+              color={'primary'}
+              style={{
+                fontSize: 14,
+                fontWeight: 400,
+                marginTop: isMobile ? theme.spacing(2) : 0
+              }}
+            >
+              Capital Call
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
