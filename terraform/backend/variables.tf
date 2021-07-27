6add36d4475f0d657964e4bc10a1bfc8bcf954e4
danu.dumbraveanu@titanium-soft.com <Wupos1029@>
2021-07-27 13:53:48 +0300

    CI: add infrastructure scripts

---
+++ terraform/backend/variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  description = "Deploy region"
+}
+
+variable "AWS_APP_NAME" {
+  description = "Application name"
+}
+
+variable "ENVIRONMENT" {
+  description = "Environment name"
+}
