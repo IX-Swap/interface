37a004dd5d3b31adaa58e70612623382ede1255d
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-08-06 13:31:54 +0800

    improve tests

--- src/app/components/FormStepper/__tests__/SkipButton.spec.tsx
+++ src/app/components/FormStepper/__tests__/SkipButton.spec.tsx
@@ -1,10 +1,31 @@
+import { fireEvent, waitFor } from '@testing-library/react'
 import { SkipButton } from 'app/components/FormStepper/SkipButton'
+import { IdentityRoute } from 'app/pages/identity/router/config'
+import { history } from 'config/history'
 import React from 'react'
 import { render, cleanup } from 'test-utils'
+import { DetailsOfIssuanceFormValues } from 'types/detailsOfIssuance'
 
 describe('SkipButton', () => {
-  const save = jest.fn()
+  const save = jest.fn(async () => await Promise.resolve({}))
   const mutation = [save, { isLoading: false }] as any
+
+  const skippedPayload: Partial<DetailsOfIssuanceFormValues> = {
+    fullName: ' ',
+    companyName: ' ',
+    companyRegistrationNumber: ' ',
+    contactNumber: ' ',
+    email: ' ',
+    industry: ' ',
+    fundRaisingAmount: 0,
+    detail: ' ',
+    companyRelated: undefined,
+    issuanceRelated: undefined,
+    financial: undefined,
+    skipped: true
+  }
+
+  beforeEach(() => {})
 
   afterEach(async () => {
     await cleanup()
@@ -13,5 +34,20 @@ describe('SkipButton', () => {
 
   it('renders without errors', () => {
     render(<SkipButton mutation={mutation} />)
+  })
+
+  it('invokes mutate function with correct data', async () => {
+    const { container } = render(<SkipButton mutation={mutation} />)
+    const skipButton = container.querySelector('button') as HTMLButtonElement
+    fireEvent.click(skipButton, { bubbles: true, cancellable: true })
+
+    await expect(save).toHaveBeenCalledWith(skippedPayload)
+
+    await waitFor(
+      () => {
+        expect(history.location.pathname).toEqual(IdentityRoute.createIssuer)
+      },
+      { timeout: 1000 }
+    )
   })
 })
