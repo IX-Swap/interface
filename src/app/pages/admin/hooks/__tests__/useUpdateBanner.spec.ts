5baadd8e56b73681d11d25d48bd7ff5834e33103
Глеб Винокуров <glebvinokurov@MacBook-Pro-Gleb-2.local>
2021-08-10 22:25:49 +0600

    Added tests for banners hooks

---
+++ src/app/pages/admin/hooks/__tests__/useUpdateBanner.spec.ts
@@ -0,0 +1,68 @@
+import { act } from '@testing-library/react-hooks'
+import { waitFor, cleanup, renderHookWithServiceProvider } from 'test-utils'
+import { unsuccessfulResponse, successfulResponse } from '__fixtures__/api'
+import { emptyBanner } from '__fixtures__/banner'
+import { useUpdateBanner } from 'app/pages/admin/hooks/useUpdateBanner'
+
+describe('useUpdateBanner', () => {
+  afterEach(async () => {
+    await cleanup()
+    jest.clearAllMocks()
+  })
+
+  it('it calls snackbarService.showSnackbar with success message', async () => {
+    await act(async () => {
+      const putFn = jest.fn().mockResolvedValueOnce(successfulResponse)
+      const showSnackbar = jest.fn()
+
+      const apiObj = { put: putFn }
+      const snackbarObj = { showSnackbar }
+      const { result } = renderHookWithServiceProvider(
+        () => useUpdateBanner(emptyBanner._id),
+        { apiService: apiObj, snackbarService: snackbarObj }
+      )
+
+      await waitFor(
+        () => {
+          const [mutate] = result.current
+          void mutate()
+
+          expect(showSnackbar).toHaveBeenNthCalledWith(
+            1,
+            successfulResponse.message,
+            'success'
+          )
+        },
+        { timeout: 1000 }
+      )
+    })
+  })
+
+  it('it calls snackbarService.showSnackbar with error message', async () => {
+    await act(async () => {
+      const putFn = jest.fn().mockRejectedValue(unsuccessfulResponse)
+      const showSnackbar = jest.fn()
+
+      const apiObj = { put: putFn }
+      const snackbarObj = { showSnackbar }
+      const { result } = renderHookWithServiceProvider(
+        () => useUpdateBanner(emptyBanner._id),
+        { apiService: apiObj, snackbarService: snackbarObj }
+      )
+
+      await waitFor(
+        () => {
+          const [mutate] = result.current
+          void mutate()
+
+          expect(showSnackbar).toHaveBeenNthCalledWith(
+            1,
+            unsuccessfulResponse.message,
+            'error'
+          )
+        },
+        { timeout: 1000 }
+      )
+    })
+  })
+})
