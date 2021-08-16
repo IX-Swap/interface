f7bb5ee6d3542bb7de37f8a01fa1d00ff38a43b6
Victoria Florea <victoria.florea@titanium-soft.com>
2021-08-16 20:12:57 +0300

    upgrade adresses

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -2,8 +2,10 @@ import { FACTORY_ADDRESS } from '@ixswap1/v2-sdk'
 import { constructSameAddressMap } from '../utils/constructSameAddressMap'
 
 export const MULTICALL2_ADDRESSES = constructSameAddressMap('0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696')
-export const IXS_ADDRESS = constructSameAddressMap('0x298A5711fCEE3Ac86230D6c5B1EA7A9FB4169f02')
-export const V2_ROUTER_ADDRESS = constructSameAddressMap('0xD19ce6100e987A34AF44804bE480188858a98112')
+export const IXS_ADDRESS = constructSameAddressMap('0xbE76627A5ce0e16507e24dBc30aE91a0DD078B93')
+export const IXS_GOVERNANCE_ADDRESS = constructSameAddressMap('0x95b85A1c9032D6A3459BeaD1ccd76FC7D735ff56')
+export const IXS_VESTING_ADDRESS = constructSameAddressMap('0x28180339ACf608747fEaCC0Ef24AA57080BCBDB9')
+export const V2_ROUTER_ADDRESS = constructSameAddressMap('0x232cbE40425BEA75cc483255B2EbadCDd6839C21')
 export const MERKLE_DISTRIBUTOR_ADDRESS: { [chainId: number]: string } = {
   [1]: '0x090D4613473dEE047c3f2706764f49E0821D256e',
 }
