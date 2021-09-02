1c9a25eb41d6b7509d645e513f20f98750d62762
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-09-02 15:28:13 +0800

    unnecessary files

--- src/app/pages/authorizer/components/withDSOExtraActions.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Actions, ActionsProps } from 'app/pages/authorizer/components/Actions'
-
-export type WithExtraActionsProps<T> = ActionsProps<T>
-
-export const withDSOExtraActions = <T,>() => (
-  actionsProps: ActionsProps<T>
-): JSX.Element => <Actions<T> {...actionsProps} />
