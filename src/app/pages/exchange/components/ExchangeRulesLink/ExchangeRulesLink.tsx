3d3fccf574df7d765ef8f5a09182fc771bf391f8
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-09-07 06:59:58 +0800

    move pdf to public folder

--- src/app/pages/exchange/components/ExchangeRulesLink/ExchangeRulesLink.tsx
+++ src/app/pages/exchange/components/ExchangeRulesLink/ExchangeRulesLink.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import ExchangeRulesPDF from 'assets/documents/IXExchangeRules.pdf'
 import { Link } from '@material-ui/core'
 
 export const ExchangeRulesLink = () => {
   return (
-    <Link href={ExchangeRulesPDF} target='_blank' rel='noopener noreferrer'>
+    <Link
+      href='/documents/IXExchangeRules.pdf'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
       Exchange Rules
     </Link>
   )
