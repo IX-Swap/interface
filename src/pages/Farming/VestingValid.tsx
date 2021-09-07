d0cb8a9edd29c49c54abf16445681d93166e0b0d
Leahu Ion <ion.leahu@titanium-soft.com>
2021-09-07 10:19:24 +0300

    add tooltip to vesting details

--- src/pages/Farming/VestingValid.tsx
+++ src/pages/Farming/VestingValid.tsx
@@ -1,21 +1,23 @@
 import { CurrencyAmount } from '@ixswap1/sdk-core'
-import { Trans } from '@lingui/macro'
+import { Trans, t } from '@lingui/macro'
 import { ButtonIXSWide } from 'components/Button'
 import Column from 'components/Column'
 import { TextRow } from 'components/TextRow/TextRow'
+import Tooltip from 'components/Tooltip'
 import { IXS_ADDRESS } from 'constants/addresses'
 import { useCurrency } from 'hooks/Tokens'
 import useTheme from 'hooks/useTheme'
 import { useActiveWeb3React } from 'hooks/web3'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAvailableClaim, useClaimAll, usePayouts, useVestingDetails, useVestingStatus } from 'state/vesting/hooks'
 import { getVestingDates } from 'state/vesting/utils'
 import { TYPE } from 'theme'
 import { formatCurrencyAmount } from 'utils/formatCurrencyAmount'
 import { closestFutureDate, getPayoutClosestToPresent, unixTimeToFormat } from 'utils/time'
-import { VestingContractDetails, VestingTableTitle } from './styleds'
+import { VestingContractDetails, InfoIcon, VestingDetailsTitle } from './styleds'
 
 export const VestingValid = () => {
+  const [showTooltip, setShowTooltip] = useState(false)
   const theme = useTheme()
   const vestingDetails = useVestingDetails()
   const payouts = usePayouts()
@@ -29,11 +31,18 @@ export const VestingValid = () => {
   return (
     <>
       <VestingContractDetails>
-        <VestingTableTitle>
+        <VestingDetailsTitle>
           <TYPE.title6 color={theme.text2} style={{ textTransform: 'uppercase', marginBottom: '25px' }}>
             <Trans>Details</Trans>
           </TYPE.title6>
-        </VestingTableTitle>
+          <Tooltip
+            placement="top"
+            show={showTooltip}
+            text={t`Your tokens will be released linearly over time. You can claim the releasable amount right now. Come back later to claim more tokens until the full balance is claimed.`}
+          >
+            <InfoIcon onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)} />
+          </Tooltip>
+        </VestingDetailsTitle>
         <Column style={{ gap: '16px' }}>
           {vestingDetails?.start && (
             <Column>
