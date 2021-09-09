808b8e144aabb567c825183f7acd200e5378c27e
Victoria Florea <victoria.florea@titanium-soft.com>
2021-09-09 16:40:26 +0300

    Adjust farming tabs. Hide Vesting on mainnet

--- src/pages/Farming/VestingTab.tsx
+++ src/pages/Farming/VestingTab.tsx
@@ -1,17 +1,89 @@
+import { Trans } from '@lingui/macro'
+import { RowCenter } from 'components/Row'
+import { useActiveWeb3React } from 'hooks/web3'
+import { SUPPORTED_TGE_CHAINS } from 'pages/App'
 import React from 'react'
+import { Text } from 'rebass'
+import styled from 'styled-components/macro'
+import { TextGradient, TYPE } from 'theme'
 import { LightBackground } from 'theme/Background'
-import { FARMING_TABS } from './enum'
-import { FarmingTabs } from './FarmingTabs'
+import { FARMING_STRINGS, FARMING_TABS } from './enum'
 import { Container } from './styleds'
 import { Vesting } from './Vesting'
 
+export const BlurredOverlay = styled.div`
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 3;
+  display: flex;
+  text-align: center;
+  vertical-align: center;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  backdrop-filter: blur(10px);
+  border-radius: 45px;
+  background: ${({ theme }) => theme.bgG16};
+  @media (max-width: 1300px) {
+    height: 50vh;
+  }
+`
+
+const HideOnSmallScreen = styled.div`
+  @media (max-width: 1300px) {
+    display: none;
+  }
+`
+
+const SmallerFont = styled.div`
+  @media (max-width: 1300px) {
+    font-size: 28px;
+  }
+`
+
 export const VestingTab = () => {
+  const { chainId } = useActiveWeb3React()
   return (
     <>
       <LightBackground />
       <Container width={['100%']} maxWidth={'1299px'}>
-        <FarmingTabs {...{ tab: FARMING_TABS.VESTING }} />
-        <Vesting />
+        {chainId === SUPPORTED_TGE_CHAINS.MAIN && (
+          <HideOnSmallScreen>
+            <TYPE.title4>
+              <SmallerFont>{FARMING_STRINGS[FARMING_TABS.VESTING]}</SmallerFont>
+            </TYPE.title4>
+          </HideOnSmallScreen>
+        )}
+        {chainId !== SUPPORTED_TGE_CHAINS.MAIN && <TYPE.title4>{FARMING_STRINGS[FARMING_TABS.VESTING]}</TYPE.title4>}
+        <div>
+          {chainId === SUPPORTED_TGE_CHAINS.MAIN && (
+            <BlurredOverlay>
+              <TYPE.titleBig>
+                <SmallerFont>
+                  <Trans>Vesting Balances will be available from</Trans>
+                </SmallerFont>
+              </TYPE.titleBig>
+              <Text fontSize={'40px'}>
+                <SmallerFont>
+                  <TextGradient>15th September</TextGradient>
+                </SmallerFont>
+              </Text>
+              <RowCenter style={{ marginTop: '40px' }}>
+                <TYPE.titleBig>
+                  <SmallerFont>Come back soon!</SmallerFont>
+                </TYPE.titleBig>
+              </RowCenter>
+            </BlurredOverlay>
+          )}
+          {chainId === SUPPORTED_TGE_CHAINS.MAIN && (
+            <HideOnSmallScreen>
+              <Vesting />
+            </HideOnSmallScreen>
+          )}
+          {chainId !== SUPPORTED_TGE_CHAINS.MAIN && <Vesting />}
+        </div>
       </Container>
     </>
   )
