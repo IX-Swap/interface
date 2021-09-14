027cca405096ba55bad52d3a10a3107a559bde7d
Sumen del Rosario <sumen.delrosario@gmail.com>
2021-09-14 15:08:44 +0800

    remove duplicated code

--- src/app/pages/accounts/components/Commitments/CommitmentInvestOTPDialog.tsx
+++ src/app/pages/accounts/components/Commitments/CommitmentInvestOTPDialog.tsx
@@ -1,20 +1,6 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Grid,
-  Typography,
-  Box
-} from '@material-ui/core'
-import { WithdrawCashFormValues } from 'app/pages/accounts/types'
-import { OTPField } from 'components/form/OTPField'
-import { TypedField } from 'components/form/TypedField'
-import { VSpacer } from 'components/VSpacer'
-import { plainValueExtractor } from 'helpers/forms'
+import { Dialog, DialogTitle, Typography, Box } from '@material-ui/core'
+import { OTPDialogContent } from 'app/pages/accounts/pages/banks/pages/WithdrawCash/OTPDialog'
 import React from 'react'
-import { useFormContext } from 'react-hook-form'
 
 interface CommitmentInvestOTPDialogProps {
   close: () => void
@@ -25,9 +11,6 @@ export const CommitmentInvestOTPDialog = ({
   close,
   open
 }: CommitmentInvestOTPDialogProps) => {
-  const { control, watch, formState } = useFormContext<WithdrawCashFormValues>()
-  const otpValue = watch('otp')
-
   return (
     <Dialog disablePortal open={open} maxWidth='md' onBackdropClick={close}>
       <Box py={4} px={10}>
@@ -45,45 +28,7 @@ export const CommitmentInvestOTPDialog = ({
             The required balance will be deducted from your account
           </Typography>
         </Box>
-        <DialogContent>
-          <TypedField
-            control={control}
-            customRenderer
-            component={OTPField}
-            name='otp'
-            label='Please enter your OTP from authenticator before proceeding'
-            variant='outlined'
-            valueExtractor={plainValueExtractor}
-            shouldAutoFocus
-          />
-        </DialogContent>
-        <VSpacer size='small' />
-        <DialogActions>
-          <Grid container spacing={2} justify='center'>
-            <Grid item>
-              <Button variant='outlined' color='primary' onClick={close}>
-                Cancel
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button
-                type='submit'
-                variant='contained'
-                color='primary'
-                disableElevation
-                disabled={
-                  otpValue === undefined ||
-                  otpValue === null ||
-                  otpValue === '' ||
-                  otpValue.length < 6 ||
-                  formState.isSubmitting
-                }
-              >
-                Confirm
-              </Button>
-            </Grid>
-          </Grid>
-        </DialogActions>
+        <OTPDialogContent close={close} actionLabel='Confirm' />
       </Box>
     </Dialog>
   )
