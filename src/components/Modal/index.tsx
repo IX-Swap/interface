e56ed533b62d7c7f6fd549bd32677d2d96e4379c
Leahu Ion <ion.leahu@titanium-soft.com>
2021-09-20 09:54:15 +0300

    responsive fix

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -22,11 +22,9 @@ const StyledDialogContent = styled(({ minHeight, maxHeight, mobile, isOpen, ...r
     box-shadow: 0 4px 8px 0 ${({ theme }) => transparentize(0.95, theme.shadow1)};
     padding: 0px;
     width: 50vw;
-    overflow-y: ${({ mobile }) => (mobile ? 'scroll' : 'hidden')};
+    overflow-y: ${({ mobile }) => (mobile ? 'auto' : 'hidden')};
     overflow-x: hidden;
-
-    align-self: ${({ mobile }) => (mobile ? 'flex-end' : 'center')};
-
+    align-self: center;
     max-width: 420px;
     ${({ maxHeight }) =>
       maxHeight &&
