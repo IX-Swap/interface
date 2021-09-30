a4c1079a5decb704ec0ef4a56ae04ebd06200f89
Iana Gamurar <iana.gamurar@titanium-soft.com>
2021-09-30 15:09:18 +0300

    fix withdraw request round small amounts

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -3,6 +3,9 @@ export function formatNumber(num: number): string {
 }
 
 export function removeSciNotation(num: number): string {
-  const toFixedNumber = Number.parseInt(num.toString().split('-')[1])
-  return num.toFixed(toFixedNumber)
+  if (/e-/.test(num.toString())) {
+    const toFixedNumber = Number.parseInt(num.toString().split('-')[1])
+    return num.toFixed(toFixedNumber)
+  }
+  return num.toString()
 }
