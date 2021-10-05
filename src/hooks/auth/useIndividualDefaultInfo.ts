9fc944ea51cc7f226d820250365f4c47ed6b84d8
Tony Thai <trung@investax.io>
2021-10-05 15:42:33 +0700

    add unit test for useIndividualDefaultInfo hook

---
+++ src/hooks/auth/useIndividualDefaultInfo.ts
@@ -0,0 +1,25 @@
+import { useAuth } from 'hooks/auth/useAuth'
+
+export const useIndividualDefaultInfo = (root: string | undefined) => {
+  const { user } = useAuth()
+  const isRepresentative = root?.startsWith('representatives') ?? false
+  const noRoot = root === undefined
+  const shouldUserIndividualInfo = noRoot || isRepresentative
+  const names = user?.name?.split(' ') ?? []
+
+  return {
+    email: shouldUserIndividualInfo ? user?.email : undefined,
+    firstName: shouldUserIndividualInfo ? names[0] : undefined,
+    lastName: shouldUserIndividualInfo
+      ? names.length > 1
+        ? names[names.length - 1]
+        : ''
+      : undefined,
+    middleName: shouldUserIndividualInfo
+      ? names.length > 2
+        ? names.slice(1, names.length - 1).join(' ')
+        : ''
+      : undefined,
+    isDisabled: shouldUserIndividualInfo
+  }
+}
