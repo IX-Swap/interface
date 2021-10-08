07dbd22f4302dffd9f854a9e54233baf03372258
itagaev <tagayev.ilias@gmail.com>
2021-10-08 15:35:34 +0600

    withdraw modal ui changes

--- src/components/Vault/WithdrawRequestForm.tsx
+++ src/components/Vault/WithdrawRequestForm.tsx
@@ -4,6 +4,7 @@ import { ButtonIXSWide } from 'components/Button'
 import Column from 'components/Column'
 import Row from 'components/Row'
 import useENS from 'hooks/useENS'
+import useTheme from 'hooks/useTheme'
 import { useActiveWeb3React } from 'hooks/web3'
 import React, { useEffect } from 'react'
 import { useUserSecTokens } from 'state/user/hooks'
@@ -14,6 +15,7 @@ import {
   useWithdrawState,
 } from 'state/withdraw/hooks'
 import { TYPE } from 'theme'
+import { shortenAddress } from 'utils'
 import { currencyId } from 'utils/currencyId'
 import { AddressInput } from '../AddressInputPanel/AddressInput'
 import { AmountInput } from './AmountInput'
@@ -24,6 +26,7 @@ interface Props {
   changeModal: (param: WithdrawModalView) => void
 }
 export const WithdrawRequestForm = ({ currency, changeModal }: Props) => {
+  const theme = useTheme()
   const { account } = useActiveWeb3React()
   const { amount, receiver, currencyId: cid } = useWithdrawState()
   const { secTokens } = useUserSecTokens()
@@ -72,10 +75,19 @@ export const WithdrawRequestForm = ({ currency, changeModal }: Props) => {
             </Trans>
           </TYPE.description3>
         </Row> */}
+        <Column>
+          <TYPE.description3>
+            <b>
+              <Trans>Info:</Trans>
+            </b>
+            &nbsp;
+            <Trans>Your wrapped AMPS will be extracted from your Ethereum wallet and burnt automatically.</Trans>
+          </TYPE.description3>
+        </Column>
         <Column style={{ gap: '11px' }}>
           <Row>
             <TYPE.body1>
-              <Trans>Amount</Trans>
+              <Trans>I want to withdraw:</Trans>
             </TYPE.body1>
           </Row>
           <AmountInput
@@ -85,18 +97,25 @@ export const WithdrawRequestForm = ({ currency, changeModal }: Props) => {
             value={amount ?? ''}
             onUserInput={onTypeAmount}
           />
+          <Row>
+            <TYPE.description2 color={`${theme.text2}80`}>
+              <Trans>{`${amount || '0'} ${
+                (secTokens[currency?.address || ''] as any)?.tokenInfo?.name
+              } tokens from your wallet ${shortenAddress(address || '')} will be burned during withdrawal`}</Trans>
+            </TYPE.description2>
+          </Row>
         </Column>
         <Column style={{ gap: '11px' }}>
           <Row>
             <TYPE.body1>
-              <Trans>Where to send?</Trans>
+              <Trans>{`To my ${(secTokens[currency?.address || ''] as any)?.tokenInfo?.network} wallet`}</Trans>
             </TYPE.body1>
           </Row>
           <AddressInput {...{ id: 'receiver-input', value: receiver ?? '', error, onChange: onTypeReceiver }} />
         </Column>
       </Column>
 
-      <Row style={{ marginTop: '37px', marginBottom: '24px' }}>
+      <Row style={{ marginTop: '50px', marginBottom: '24px' }}>
         <ButtonIXSWide style={{ textTransform: 'unset' }} disabled={!!inputError} onClick={onClick}>
           {inputError ?? <Trans>Send</Trans>}
         </ButtonIXSWide>
