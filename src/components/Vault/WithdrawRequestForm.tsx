702920caaf3addee5d9dc4364df57f5773ee838d
itagaev <tagayev.ilias@gmail.com>
2021-10-15 16:36:54 +0600

    QA comments withdraw modal

--- src/components/Vault/WithdrawRequestForm.tsx
+++ src/components/Vault/WithdrawRequestForm.tsx
@@ -36,6 +36,7 @@ export const WithdrawRequestForm = ({ currency, changeModal }: Props) => {
   const withdraw = useWithdrawCallback(cid, currency?.symbol)
   const { parsedAmount, inputError } = useDerivedWithdrawInfo()
   const tokenInfo = (secTokens[(currency as any)?.address || ''] as any)?.tokenInfo
+  const networkName = tokenInfo?.network.charAt(0).toUpperCase() + tokenInfo?.network.slice(1) || ''
 
   useEffect(() => {
     if (account) {
@@ -59,6 +60,8 @@ export const WithdrawRequestForm = ({ currency, changeModal }: Props) => {
     const id = currencyId(currency)
     onCurrencySet(id)
   }, [currency, onCurrencySet])
+
+  console.log('accouint', tokenInfo)
 
   return (
     <div style={{ position: 'relative' }}>
@@ -109,10 +112,18 @@ export const WithdrawRequestForm = ({ currency, changeModal }: Props) => {
         <Column style={{ gap: '11px' }}>
           <Row>
             <TYPE.body1>
-              <Trans>{`To my ${tokenInfo?.network} wallet`}</Trans>
+              <Trans>{`To my ${networkName} wallet`}</Trans>
             </TYPE.body1>
           </Row>
-          <AddressInput {...{ id: 'receiver-input', value: receiver ?? '', error, onChange: onTypeReceiver }} />
+          <AddressInput
+            {...{
+              id: 'receiver-input',
+              value: receiver ?? '',
+              error,
+              onChange: onTypeReceiver,
+              placeholder: `Paste your ${networkName} wallet`,
+            }}
+          />
         </Column>
       </Column>
 
