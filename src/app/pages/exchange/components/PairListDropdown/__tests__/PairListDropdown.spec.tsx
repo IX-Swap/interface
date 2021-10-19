d73fecbb98995f94b759eaed77afd347effed615
Глеб Винокуров <glebvinokurov@MacBook-Pro-Gleb-2.local>
2021-10-19 15:47:35 +0300

    Added some unit tests

--- src/app/pages/exchange/components/PairListDropdown/__tests__/PairListDropdown.spec.tsx
+++ src/app/pages/exchange/components/PairListDropdown/__tests__/PairListDropdown.spec.tsx
@@ -1,8 +1,12 @@
 import { PairListDropdown } from 'app/pages/exchange/components/PairListDropdown/PairListDropdown'
 import React from 'react'
 import { render, cleanup } from 'test-utils'
+import { ClickAwayListener } from '@material-ui/core'
+
+jest.mock('@material-ui/core/ClickAwayListener', () => jest.fn(() => null))
 
 describe('PairListDropdown', () => {
+  const setAnchorEl = jest.fn()
   afterEach(async () => {
     await cleanup()
     jest.clearAllMocks()
@@ -10,5 +14,27 @@ describe('PairListDropdown', () => {
 
   it('renders without errors', () => {
     render(<PairListDropdown pairName='IXPS/SGD' />)
+  })
+
+  it('renders ClickAwayListener when anchorEl is null', () => {
+    const anchorEl = null
+    jest
+      .spyOn(React, 'useState')
+      .mockImplementation(() => [anchorEl, setAnchorEl])
+
+    render(<PairListDropdown pairName='IXPS/SGD' />)
+
+    expect(ClickAwayListener).toHaveBeenCalledTimes(0)
+  })
+
+  it('renders ClickAwayListener when anchorEl is not null', () => {
+    const anchorEl = ''
+    jest
+      .spyOn(React, 'useState')
+      .mockImplementation(() => [anchorEl, setAnchorEl])
+
+    render(<PairListDropdown pairName='IXPS/SGD' />)
+
+    expect(ClickAwayListener).toHaveBeenCalledTimes(1)
   })
 })
