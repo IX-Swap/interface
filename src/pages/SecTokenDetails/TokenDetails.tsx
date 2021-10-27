a929c6de35315f756ef4413562dae1c659785777
itagaev <tagayev.ilias@gmail.com>
2021-10-27 13:20:16 +0600

    feat: display issuer

--- src/pages/SecTokenDetails/TokenDetails.tsx
+++ src/pages/SecTokenDetails/TokenDetails.tsx
@@ -1,7 +1,10 @@
 import { Token } from '@ixswap1/sdk-core'
 import { Trans } from '@lingui/macro'
+import { Logo } from './styleds'
+import { AccreditationRequest } from 'components/Vault/enum'
 import useCopyClipboard from 'hooks/useCopyClipboard'
 import React from 'react'
+import { ExternalLink } from 'theme'
 import { shortenAddress } from 'utils'
 import PortisIcon from '../../assets/images/portisIcon.png'
 import { DetailsElement } from './DetailsElement'
@@ -9,34 +12,37 @@ import { Details, IconWrapper } from './styleds'
 
 interface Props {
   currency?: Token
+  accreditationRequest: AccreditationRequest | null
 }
-export const TokenDetails = ({ currency }: Props) => {
+export const TokenDetails = ({ currency, accreditationRequest }: Props) => {
   const [isCopied, setCopied] = useCopyClipboard()
   return (
     <Details>
-      {false && (
-        <DetailsElement
-          title={<Trans>Issuer:</Trans>}
-          content={
-            <>
-              <IconWrapper size={16}>
-                <img src={PortisIcon} alt={'logo'} />
-              </IconWrapper>
-              investax.io
-            </>
-          }
-        />
-      )}
+      <div>
+        {accreditationRequest && (
+          <DetailsElement
+            title={<Trans>Issuer:</Trans>}
+            content={
+              <>
+                <Logo style={{ marginRight: '6px' }} currency={currency} size="27px" />
+                <ExternalLink href={accreditationRequest.custodian.website} style={{ textDecorationLine: 'underline' }}>
+                  {accreditationRequest.custodian.name}
+                </ExternalLink>
+              </>
+            }
+          />
+        )}
+        {currency?.address && (
+          <div onClick={() => setCopied(currency?.address ?? '')}>
+            <DetailsElement
+              title={<Trans>Contract:</Trans>}
+              content={isCopied ? <Trans>Copied!</Trans> : shortenAddress(currency?.address ?? '')}
+            />
+          </div>
+        )}
+      </div>
       {false && <DetailsElement title={<Trans>Initial Price:</Trans>} content="17$" />}
       {false && <DetailsElement title={<Trans>Total Issued:</Trans>} content="100000000" />}
-      {currency?.address && (
-        <div onClick={() => setCopied(currency?.address ?? '')}>
-          <DetailsElement
-            title={<Trans>Contract:</Trans>}
-            content={isCopied ? <Trans>Copied!</Trans> : shortenAddress(currency?.address ?? '')}
-          />
-        </div>
-      )}
       {false && <DetailsElement title={<Trans>Initial offering price:</Trans>} content="$25" />}
       {false && <DetailsElement title={<Trans>STO Med. price:</Trans>} content="$30" />}
     </Details>
