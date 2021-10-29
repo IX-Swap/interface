34ad470e8cdb39f80c9aa3cbf106885b7f911a4a
Automation-Luch <lopezmichaela1b031@gmail.com>
2021-10-29 13:56:51 +0300

    ci settings

--- __tests__/tests/listening.spec.ts
+++ __tests__/tests/listening.spec.ts
@@ -5,32 +5,33 @@ import { expect } from '@playwright/test'
 let present
 let docs
 test.describe.only('Check form`s view', () => {
-  test.beforeEach(async ({ page, dso, auth, issuanceSelectors }, testInfo) => {
-    // await dso.followToIssuanceTab(auth)
-    // await click(issuanceSelectors.sections.CREATE_EXCHANGE_LISTINGS, page)
-  })
+  test.beforeEach(
+    async ({ page, dso, auth, issuanceSelectors, baseURL }, testInfo) => {
+      console.log(baseURL)
+      console.log(process.env.NODE_ENV)
+
+      await dso.followToIssuanceTab(auth)
+      await click(issuanceSelectors.sections.CREATE_EXCHANGE_LISTINGS, page)
+    }
+  )
   test.afterEach(async ({ page }) => {
     await page.close()
   })
 
   test('New Listening should be created ', async ({ listing, dso }) => {
-    console.log(1, process.env.REACT_APP_API_URL)
-    console.log(2, process.env.ENVIRONMENT_URL)
-    console.log(3, process.env.PLAYWRIGHT_TEST_BASE_URL)
-
-    // const token = (await listing.fillListingGeneralInformationForm()).tokenName
-    // docs = await listing.fillDocumentsForm()
-    // await listing.addAndFillTeamMemberForm()
-    // await listing.fillListingPricingForm()
-    // await dso.fillDsoOfferingTermsForm()
-    // present = await listing.checkThatTheListingWasCreated(token)
+    const token = (await listing.fillListingGeneralInformationForm()).tokenName
+    docs = await listing.fillDocumentsForm()
+    await listing.addAndFillTeamMemberForm()
+    await listing.fillListingPricingForm()
+    await dso.fillDsoOfferingTermsForm()
+    present = await listing.checkThatTheListingWasCreated(token)
   })
 })
 
-// test('Listing should displayed on the My Listings section', async ({}) => {
-//   expect(present).toBe(true)
-// })
+test('Listing should displayed on the My Listings section', async ({}) => {
+  expect(present).toBe(true)
+})
 
-// test('3 inputs on the documents section', async ({}) => {
-//   expect(docs.length).toBe(3)
-// })
+test('3 inputs in the documents section', async ({}) => {
+  expect(docs.length).toBe(3)
+})
