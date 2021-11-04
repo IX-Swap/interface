c4fbf9666d04a1925e191c4f9407c281dbfa7213
itagaev <tagayev.ilias@gmail.com>
2021-11-04 18:18:01 +0600

    search by wallet address

---
+++ src/components/AdminTransactionsTable/Search.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent } from 'react'
+import { t } from '@lingui/macro'
+
+import { Input as SearchInput } from 'pages/AdminKyc/Search'
+import { useAdminState, useFetchBrokerDealerSwaps } from 'state/admin/hooks'
+
+let timer = null as any
+
+export const AdminTransactionsTableSearch = () => {
+  const {
+    brokerDealerSwaps: { offset },
+  } = useAdminState()
+  const getBrokerDealerSwaps = useFetchBrokerDealerSwaps()
+
+  const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    clearTimeout(timer)
+    timer = setTimeout(() => getBrokerDealerSwaps({ page: 1, offset, search: value }), 250)
+  }
+
+  return <SearchInput placeholder={t`Search for Wallet`} onChange={onSearchChange} />
+}
