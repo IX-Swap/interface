2ffdbddab38b750b20e2bb6c6a555bbbb56994ea
itagaev <tagayev.ilias@gmail.com>
2021-11-07 18:01:08 +0600

    do search for only valid addresses

--- src/components/AdminTransactionsTable/Search.tsx
+++ src/components/AdminTransactionsTable/Search.tsx
@@ -3,6 +3,7 @@ import { t } from '@lingui/macro'
 
 import { Input as SearchInput } from 'pages/AdminKyc/Search'
 import { useAdminState, useFetchBrokerDealerSwaps } from 'state/admin/hooks'
+import { isAddress } from 'utils'
 
 let timer = null as any
 
@@ -14,8 +15,11 @@ export const AdminTransactionsTableSearch = () => {
 
   const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
-    clearTimeout(timer)
-    timer = setTimeout(() => getBrokerDealerSwaps({ page: 1, offset, search: value }), 250)
+
+    if (isAddress(value) || value === '') {
+      clearTimeout(timer)
+      timer = setTimeout(() => getBrokerDealerSwaps({ page: 1, offset, search: value }), 250)
+    }
   }
 
   return <SearchInput placeholder={t`Search for Wallet`} onChange={onSearchChange} />
