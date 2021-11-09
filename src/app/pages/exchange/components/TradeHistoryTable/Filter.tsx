fad0e2cc5481e6963f19aa38ccf581d0c0ac7586
Gleb Vinokurov <gleb@investax.io>
2021-11-09 14:49:19 +0300

    Fixed code smells

--- src/app/pages/exchange/components/TradeHistoryTable/Filter.tsx
+++ src/app/pages/exchange/components/TradeHistoryTable/Filter.tsx
@@ -1,47 +1,14 @@
-import { Box, Grid, Typography } from '@material-ui/core'
-import { SearchFilter } from 'app/components/SearchFilter'
-import { DateFilter } from 'app/pages/admin/components/AssignedVirtualAccountsTable/DateFilter'
+import { Grid } from '@material-ui/core'
 import { PairFilter } from 'app/pages/exchange/components/TradeHistoryTable/PairFilter'
 import React from 'react'
+import { BaseFilters } from 'app/components/BaseFilters/BaseFilters'
 
 export const Filters = () => {
   return (
-    <Grid
-      container
-      justify='space-between'
-      style={{ paddingLeft: 24, paddingRight: 24 }}
-    >
-      <Grid item xs={12} md={6}>
-        <Box width={300}>
-          <SearchFilter
-            fullWidth
-            placeholder='Search'
-            inputAdornmentPosition='end'
-          />
-        </Box>
+    <BaseFilters>
+      <Grid item>
+        <PairFilter />
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Grid
-          container
-          direction='row'
-          justify='flex-end'
-          spacing={2}
-          alignItems='center'
-        >
-          <Grid item>
-            <Typography>Date:</Typography>
-          </Grid>
-          <Grid item>
-            <DateFilter name='fromDate' label='From' />
-          </Grid>
-          <Grid item>
-            <DateFilter name='toDate' label='To' />
-          </Grid>
-          <Grid item>
-            <PairFilter />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
+    </BaseFilters>
   )
 }
