979cb2536f15adc68890b5567b6fe7e13ca43ccc
Gleb Vinokurov <gleb@investax.io>
2021-11-20 19:46:59 +0300

    Fixed ui mistake

--- src/app/pages/accounts/pages/withdrawalAddresses/WithdrawalAddressCreate/WAOfferToCreateWallet/WAOfferToCreateWallet.styles.ts
+++ src/app/pages/accounts/pages/withdrawalAddresses/WithdrawalAddressCreate/WAOfferToCreateWallet/WAOfferToCreateWallet.styles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(theme => ({
   link: {
-    color: theme.palette.slider.activeBackground,
+    color: theme.palette.primary.main,
     cursor: 'pointer'
   }
 }))
