bc62077f48f3a6da3fabc5276959fb87ae556acf
Automation-Luch <lopezmichaela1b031@gmail.com>
2021-11-25 17:47:32 +0200

    improvments

--- __tests__/lib/page-objects/investments.ts
+++ __tests__/lib/page-objects/investments.ts
@@ -67,6 +67,7 @@ class Invest {
     await typeText(invest.fields.AMOUNT, '1', this.page)
     await click(invest.buttons.PLACE_ORDER, this.page)
     const toast = await this.page.innerText(invest.TOAST_NOTIFICATIONS)
+    console.log(toast)
     return toast.includes('Order created')
   }
 
