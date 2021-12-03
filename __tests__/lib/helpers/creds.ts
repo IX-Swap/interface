9d59f90c64f85fe9963b56aa1a715f069cb85bb6
Automation-Luch <lopezmichaela1b031@gmail.com>
2021-12-03 09:58:55 +0200

    credentials

--- __tests__/lib/helpers/creds.ts
+++ __tests__/lib/helpers/creds.ts
@@ -6,8 +6,8 @@ let baseCreds
 if (process.env.GH_REP?.includes('staging')) {
   baseCreds = {
     GH_ENV: process.env.GH_REP,
-    BASE_API: 'http://localhost:3000/',
-    URL: 'https://staging.mozork.com/',
+    BASE_API: 'https://api.staging.mozork.com/',
+    URL: 'http://localhost:3000/',
     EMAIL: 'xe2v112a@mailinator.com',
     EMAIL_APPROVED: 'lopezmichaela1b031@gmail.com',
     VIEW_PROFILE_EMAIL: 'vomyho@mailinator.com',
@@ -33,8 +33,13 @@ if (process.env.GH_REP?.includes('staging')) {
     BANK_ACCOUNT: 'byjywarago@mailinator.com',
     EMAIL_FOR_RESET: 'luch41635754715203@wwjmp.com',
     CREATE_DSO_MORE_OPTIONS: 'lorepag@mailinator.com',
+    VIEW_DSO_MORE_OPTIONS: 'qyle@mailinator.com',
     PASSWORD: 'Pa$$w0rd!qwe',
-    PASSWORD_RESET: 'Pa$$w0rd!qwe1A'
+    PASSWORD_RESET: 'Pa$$w0rd!qwe1A',
+    EDIT_CORPORATE: 'corporateideintety@mailinator.com',
+    EDIT_INDIVIDUAL: 'individual@mailinator.com',
+    EDIT_ISSUER: 'issuerinvest@mailinator.com',
+    DSO_EDIT: 'dsoCorporateUser@mailinator.com'
   }
 }
 
