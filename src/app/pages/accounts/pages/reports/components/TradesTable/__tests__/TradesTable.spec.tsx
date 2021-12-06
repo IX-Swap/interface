cb229d6653876f1cfede782f6f23c9da5c802871
Gleb Vinokurov <gleb@investax.io>
2021-12-06 19:59:55 +0300

    Fixed failed test

--- src/app/pages/accounts/pages/reports/components/TradesTable/__tests__/TradesTable.spec.tsx
+++ src/app/pages/accounts/pages/reports/components/TradesTable/__tests__/TradesTable.spec.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import { render, cleanup } from 'test-utils'
 import {
-  createRow,
   headCells,
   TradesTable
 } from 'app/pages/accounts/pages/reports/components/TradesTable/TradesTable'
 import { fakeTradeItem } from '__fixtures__/reports'
-import { TradesRow } from 'app/pages/accounts/pages/reports/components/TradesTable/TradesRow'
-
-jest.mock(
-  'app/pages/accounts/pages/reports/components/TradesTable/TradesRow',
-  () => ({
-    TradesRow: jest.fn(() => null)
-  })
-)
 
 describe('TradesTable', () => {
   afterEach(async () => {
@@ -26,30 +17,6 @@ describe('TradesTable', () => {
 
     headCells.forEach(({ label, align }, i) =>
       expect(getAllByTestId('cell')[i]).toHaveTextContent(label)
-    )
-  })
-
-  it('renders all row components in table body with correct text content', () => {
-    const fakeRow = createRow('Securities Pair')
-
-    render(<TradesTable data={[fakeTradeItem]} />)
-
-    expect(TradesRow).toHaveBeenNthCalledWith(
-      1,
-      expect.objectContaining({
-        index: 0,
-        row: fakeRow
-      }),
-      {}
-    )
-
-    expect(TradesRow).toHaveBeenNthCalledWith(
-      2,
-      expect.objectContaining({
-        row: fakeTradeItem,
-        index: 1
-      }),
-      {}
     )
   })
 })
