1fa3d92eca9c5749e24f5a10c971f34c3c93ebfc
Gleb Vinokurov <gleb@investax.io>
2021-12-06 10:23:45 +0300

    Made some changes after code review

--- src/app/pages/accounts/hooks/__tests__/useDepositAddress.spec.ts
+++ src/app/pages/accounts/hooks/__tests__/useDepositAddress.spec.ts
@@ -1,4 +1,3 @@
-import { act } from '@testing-library/react-hooks'
 import {
   waitFor,
   cleanup,
@@ -23,24 +22,20 @@ describe('useDepositAddress', () => {
   })
 
   it('expects request with correct parameters and correct response', async () => {
-    await act(async () => {
-      apiServiceMock.post.mockResolvedValue(sampleResponse)
+    apiServiceMock.post.mockResolvedValue(sampleResponse)
 
-      await act(async () => {
-        const { result } = renderHookWithServiceProvider(() => {
-          return useDepositAddress(fakeTokenSymbol)
-        })
-
-        await waitFor(() => result.current.data)
-
-        expect(result.current.data).toEqual(sampleResponse.data)
-        expect(apiServiceMock.post).toHaveBeenCalledWith(
-          accountsURL.digitalSecurities.getDepositAddress,
-          {
-            assetTicker: fakeTokenSymbol
-          }
-        )
-      })
+    const { result } = renderHookWithServiceProvider(() => {
+      return useDepositAddress(fakeTokenSymbol)
     })
+
+    await waitFor(() => result.current.data)
+
+    expect(result.current.data).toEqual(sampleResponse.data)
+    expect(apiServiceMock.post).toHaveBeenCalledWith(
+      accountsURL.digitalSecurities.getDepositAddress,
+      {
+        assetTicker: fakeTokenSymbol
+      }
+    )
   })
 })
