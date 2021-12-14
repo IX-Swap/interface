cdd4a0e4373ed256888b3d8ad833474fb54db435
Victoria Florea <victoria.florea@titanium-soft.com>
2021-12-14 22:21:16 +0200

    Add stablecoins. Add to metamask buttons

--- src/state/faucet/hooks.ts
+++ src/state/faucet/hooks.ts
@@ -1,22 +1,51 @@
 import { useCallback } from 'react'
 
-import { useFaucetContract } from 'hooks/useContract'
+import { useFaucetContract, useStableFaucetContract } from 'hooks/useContract'
+import { testStableCoinsTokens } from 'constants/addresses'
+import { useAddPopup } from 'state/application/hooks'
+import { useTransactionAdder } from 'state/transactions/hooks'
+import { t } from '@lingui/macro'
 
-export const useDistributeToken = (tokenAddress: string) => {
-  const faucetContract = useFaucetContract(tokenAddress)
-
+export const useDistributeToken = ({ address, symbol }: { address: string; symbol: string; name: string }) => {
+  const faucetContract = useFaucetContract(address)
+  const stableFaucetContract = useStableFaucetContract(address)
+  const addPopup = useAddPopup()
+  const addTransaction = useTransactionAdder()
   return useCallback(async () => {
-    if (faucetContract) {
-      const { _hex } = await faucetContract.timeToFaucet()
-      const minutesToWait = Math.ceil(parseInt(_hex, 16) / 60)
-      if (minutesToWait !== 0) return { transaction: null, minutesToWait }
-
-      const result = await faucetContract.faucet({
-        gasLimit: 900000,
+    const showError = (message: string) => {
+      addPopup({
+        info: {
+          success: false,
+          summary: message,
+        },
       })
-      const transaction = await result.wait()
-
-      return { transaction, minutesToWait }
     }
-  }, [faucetContract])
+    try {
+      const isStableCoin = testStableCoinsTokens.filter((token) => token.address === address).length > 0
+      const usedContract = isStableCoin ? stableFaucetContract : faucetContract
+      if (usedContract) {
+        const timeToFaucetFun = usedContract?.timeToFaucet || usedContract?.getTimeToFaucet
+        const { _hex } = await timeToFaucetFun()
+        const minutesToWait = Math.ceil(parseInt(_hex, 16) / 60)
+        if (minutesToWait !== 0) {
+          showError(`You have to wait ${minutesToWait} ${minutesToWait === 1 ? 'minute' : 'minutes'}`)
+          return
+        }
+
+        const result = await usedContract.faucet({
+          gasLimit: 900000,
+        })
+        addTransaction(
+          { ...result, hash: result.hash },
+          {
+            summary: `Sent ${isStableCoin ? '100' : '10'} ${symbol} to your wallet`,
+          }
+        )
+        const transaction = await result.wait()
+        return { transaction, minutesToWait }
+      }
+    } catch (e) {
+      showError(t`Could not use the faucet. Please try again later`)
+    }
+  }, [faucetContract, stableFaucetContract, address, symbol, addTransaction, addPopup])
 }
