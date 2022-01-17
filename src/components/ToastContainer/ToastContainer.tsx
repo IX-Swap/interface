f569b1bb751400ba31ad1e0b2c53349b549150a1
Victoria Florea <victoria.florea@titanium-soft.com>
2022-01-17 18:13:28 +0200

    Increase z-index of toastcontainer to show above mui dialog

---
+++ src/components/ToastContainer/ToastContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+  ToastContainerProps,
+  DefaultToastContainer
+} from 'react-toast-notifications'
+import useStyles from './ToastContainer.styles'
+
+export const ToastContainer = (props: ToastContainerProps) => {
+  const classes = useStyles()
+  return (
+    <DefaultToastContainer
+      {...props}
+      className={classes.root}
+    ></DefaultToastContainer>
+  )
+}
