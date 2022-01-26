acd416d4efed6e4ef0574275dcc531e65125ec13
Gleb Vinokurov <gleb@investax.io>
2022-01-26 12:35:15 +0300

    Fixed checkbox issue for old mui theme

--- src/components/form/Checkbox.tsx
+++ src/components/form/Checkbox.tsx
@@ -10,6 +10,7 @@ import { ReactComponent as SuccessIcon } from 'assets/icons/success_icon.svg'
 
 export interface CheckboxProps extends Omit<FormControlLabelProps, 'control'> {
   reverse?: boolean
+  withNewSuccessIcon?: boolean
 }
 
 export const Checkbox = (
@@ -22,6 +23,7 @@ export const Checkbox = (
     error = true,
     control,
     reverse = false,
+    withNewSuccessIcon = false,
     ...rest
   } = props
 
@@ -32,7 +34,12 @@ export const Checkbox = (
         alignItems: 'flex-start'
       }}
       checked={reverse ? !value : value}
-      control={<MUICheckbox name={name} checkedIcon={<SuccessIcon />} />}
+      control={
+        <MUICheckbox
+          name={name}
+          checkedIcon={withNewSuccessIcon ? <SuccessIcon /> : undefined}
+        />
+      }
       label={
         <Typography
           variant='body1'
