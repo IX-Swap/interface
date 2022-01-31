10bf85eeeb914b74233cc69512d62572d0a1d711
Gleb Vinokurov <gleb@investax.io>
2022-01-31 20:43:34 +0300

    Fixed failed tests, add some new ones

--- src/app/pages/security/router/config.ts
+++ src/app/pages/security/router/config.ts
@@ -1,7 +1,7 @@
 export const SecurityRoute = {
   landing: '/app/settings',
-  changePassword: '/app/settings/change2fa/changePassword',
-  setup2fa: '/app/settings/setup2fa',
-  change2fa: '/app/settings/change2fa',
-  guide: '/app/settings/guide2fa'
+  changePassword: '/app/settings/change2fa/change-password',
+  setup2fa: '/app/settings/setup-2fa',
+  change2fa: '/app/settings/change-2fa',
+  guide: '/app/settings/guide-2fa'
 }
