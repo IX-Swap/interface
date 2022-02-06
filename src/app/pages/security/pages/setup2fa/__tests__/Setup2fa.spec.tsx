86b95e6f196db4a8ddc7058802dc0a58e2316b5e
Gleb Vinokurov <gleb@investax.io>
2022-02-06 17:23:43 +0300

    Added fixes for setup2fa unit test

--- src/app/pages/security/pages/setup2fa/__tests__/Setup2fa.spec.tsx
+++ src/app/pages/security/pages/setup2fa/__tests__/Setup2fa.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'test-utils'
 import { Setup2fa } from 'app/pages/security/pages/setup2fa/Setup2fa'
+import * as useSetup2fa from 'app/pages/security/pages/setup2fa/hooks/useSetup2fa'
+import { fakeTwoFaData } from '__fixtures__/security'
 
 describe('Setup2fa', () => {
   afterEach(async () => {
@@ -8,6 +10,11 @@ describe('Setup2fa', () => {
   })
 
   it('should match snapshot', () => {
+    jest.spyOn(useSetup2fa, 'useSetup2fa').mockReturnValue({
+      data: fakeTwoFaData,
+      isLoading: false
+    } as any)
+
     const { container } = render(<Setup2fa />)
     expect(container).toMatchSnapshot()
   })
