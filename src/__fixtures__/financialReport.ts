56946bf8ef7cad24ef4445e0b7648ea870125234
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-02-07 18:21:22 +0800

    fix type check error

--- src/__fixtures__/financialReport.ts
+++ src/__fixtures__/financialReport.ts
@@ -1,9 +1,31 @@
 import { FinancialReport } from 'types/financitalReport'
+import { dso } from '__fixtures__/authorizer'
 
 export const sampleFinancialReport: FinancialReport = {
-  creationDate: '12/29/2021',
-  reportInterval: '12/29/2021 - 02/28/2022',
-  type: 'PDF',
-  reportOf: 'Hamilton Token',
-  documentId: '619752bb43bb8622657c5f14'
+  _id: '61fcb42223f48709b125fac5',
+  reportDocuments: [
+    {
+      _id: '61fcb42023f48709b125fabf',
+      user: '5fc0982ef02bc219055a0b9e',
+      title: 'Documents ',
+      type: 'Documents ',
+      originalFileName: 'Subscription.pdf',
+      createdAt: '2022-02-04T05:05:36.032Z',
+      updatedAt: '2022-02-04T05:05:36.032Z'
+    },
+    {
+      _id: '61fcb42023f48709b125fac1',
+      user: '5fc0982ef02bc219055a0b9e',
+      title: 'Documents ',
+      type: 'Documents ',
+      originalFileName: 'Supporting.pdf',
+      createdAt: '2022-02-04T05:05:36.092Z',
+      updatedAt: '2022-02-04T05:05:36.092Z'
+    }
+  ],
+  dso: dso,
+  nav: 1,
+  dateFrom: '2022-02-28T05:05:00.000Z',
+  dateTo: '2022-02-28T05:05:00.000Z',
+  createdAt: '2022-02-04T05:05:38.440Z'
 }
