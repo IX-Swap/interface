4f04dd672e794f8c928783ef8dd12e1dfa5cf1f9
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-02-07 14:09:28 +0800

    fix failing test

--- src/app/pages/issuance/components/FinancialReportsTable/__tests__/columns.spec.tsx
+++ src/app/pages/issuance/components/FinancialReportsTable/__tests__/columns.spec.tsx
@@ -8,9 +8,9 @@ describe('columns', () => {
   })
 
   it('renders correct keys', () => {
-    expect(columns[0].key).toBe('creationDate')
-    expect(columns[1].key).toBe('reportInterval')
-    expect(columns[2].key).toBe('type')
-    expect(columns[3].key).toBe('reportOf')
+    expect(columns[0].key).toBe('createdAt')
+    expect(columns[1].key).toBe('dateFrom')
+    expect(columns[2].key).toBe('reportDocuments')
+    expect(columns[3].key).toBe('dso')
   })
 })
