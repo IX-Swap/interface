f9ea87400cc326949fd3caae1f50030ba9c54b5a
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-02-08 16:55:38 +0800

    use Inter as default font

--- src/themes/new/typography.ts
+++ src/themes/new/typography.ts
@@ -1,73 +1,73 @@
 export const typography = {
-  fontFamily: ['Monument Grotesk'].join(','),
+  fontFamily: ['Inter'].join(','),
   h1: {
-    fontFamily: '"Monument Extended", sans-serif',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: '2.375rem',
     lineHeight: 1.2
   },
   h2: {
-    fontFamily: '"Monument Extended", sans-serif',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: '2rem',
     lineHeight: 1.2
   },
   h3: {
-    fontFamily: '"Monument Extended", sans-serif',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: '1.5rem',
     lineHeight: 1.2
   },
   h4: {
-    fontFamily: '"Monument Extended", sans-serif',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: '1.25rem',
     lineHeight: 1.2
   },
   h5: {
-    fontFamily: '"Monument Extended", sans-serif',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: '1.125rem',
     lineHeight: 1.2
   },
   subtitle1: {
-    fontFamily: '"Monument Grotesk", "Open Sans"',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: '0.875rem',
     lineHeight: 1.2
   },
   subtitle2: {
-    fontFamily: '"Monument Grotesk", "Open Sans"',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: '0.875rem',
     lineHeight: 1.2
   },
   body1: {
-    fontFamily: '"Monument Grotesk", "Open Sans"',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: '0.875rem',
     lineHeight: 1.2
   },
   body2: {
-    fontFamily: '"Monument Grotesk", "Open Sans"',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: '0.8125rem',
     lineHeight: 1.2
   },
   button: {
-    fontFamily: '"Monument Grotesk", "Open Sans"',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: '0.875rem',
     lineHeight: 1.2
   },
   caption: {
-    fontFamily: '"Monument Grotesk", "Open Sans"',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: '0.875rem',
     lineHeight: 1.2
   },
   overline: {
-    fontFamily: '"Monument Grotesk", "Open Sans"',
+    fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: '0.875rem',
     lineHeight: 1.2
