2d19d2dc457c7c9a8bb338d3c6d45b66c3c8a172
itagaev <tagayev.ilias@gmail.com>
2022-02-11 11:24:53 +0600

    rebase

--- src/pages/KYC/index.tsx
+++ src/pages/KYC/index.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import { StyledBodyWrapper } from 'pages/CustodianV2/styleds'
 
 export default function KYC() {
-  return <StyledBodyWrapper>IXSwap KYC</StyledBodyWrapper>
+  return <StyledBodyWrapper>IXSwap KYCCC</StyledBodyWrapper>
 }
