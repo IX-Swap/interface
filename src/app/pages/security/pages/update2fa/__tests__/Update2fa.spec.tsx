04290352ef8bde7d325a284e8460db2799d5974b
Gleb Vinokurov <gleb@investax.io>
2022-02-14 14:20:10 +0300

    Fixed test

--- src/app/pages/security/pages/update2fa/__tests__/Update2fa.spec.tsx
+++ src/app/pages/security/pages/update2fa/__tests__/Update2fa.spec.tsx
@@ -3,9 +3,13 @@ import { render } from 'test-utils'
 import { Update2fa } from 'app/pages/security/pages/update2fa/Update2fa'
 
 describe('Update2fa', () => {
-  it('should match snapshot, does not render Next and Back button if step is 0', () => {
-    const { container, queryByText } = render(<Update2fa />)
+  it('should match snapshot', () => {
+    const { container } = render(<Update2fa />)
     expect(container).toMatchSnapshot()
+  })
+
+  it('does not render Next and Back button if step is 0', () => {
+    const { queryByText } = render(<Update2fa />)
     expect(queryByText('Next')).not.toBeInTheDocument()
     expect(queryByText('Back')).not.toBeInTheDocument()
   })
