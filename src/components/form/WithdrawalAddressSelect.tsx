f27113bc05af25126d385fd911ca0612afc7c571
Gleb Vinokurov <gleb@investax.io>
2022-02-15 11:15:01 +0300

    Disabled select and added text when there are no wallet addresses

--- src/components/form/WithdrawalAddressSelect.tsx
+++ src/components/form/WithdrawalAddressSelect.tsx
@@ -2,28 +2,25 @@ import React from 'react'
 import { MenuItem, Select, SelectProps } from '@mui/material'
 import { queryStatusRenderer } from 'components/form/renderUtils'
 import { privateClassNames } from 'helpers/classnames'
-import { useWithdrawalAddresses } from 'app/pages/accounts/pages/withdrawalAddresses/hooks/useWithdrawalAddresses'
+import { WithdrawalAddress } from 'types/withdrawalAddress'
+import { QueryStatus } from 'react-query'
 
 export interface WithdrawalAddressSelectProps extends SelectProps {
-  network?: string
+  list: WithdrawalAddress[]
+  status: QueryStatus
 }
 
 export const WithdrawalAddressSelect: React.FC<
   WithdrawalAddressSelectProps
 > = props => {
-  const { network, ...rest } = props
-  const { data, status } = useWithdrawalAddresses({ network })
+  const { list, status, ...rest } = props
 
   const queryStatus = queryStatusRenderer(status)
   if (queryStatus !== undefined) return queryStatus
 
-  const filteredAddresses = data.list.filter(
-    ({ status }) => status === 'Approved'
-  )
-
   return (
     <Select {...rest}>
-      {filteredAddresses.map(({ label, _id }) => (
+      {list.map(({ label, _id }) => (
         <MenuItem key={_id} value={_id} className={privateClassNames()}>
           {label}
         </MenuItem>
