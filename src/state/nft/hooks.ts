6b1154a72eae620b55f323169367649d3459b7fd
Nurkassym <nurkassym.bakhramov@titanium-soft.com>
2022-02-17 00:38:14 +0600

    [IXSwap.Interface] - Updated NFT/Collection layouts

--- src/state/nft/hooks.ts
+++ src/state/nft/hooks.ts
@@ -181,38 +181,6 @@ export function useNFTState(): AppState['nft'] {
   return useSelector<AppState, AppState['nft']>((state) => state.nft)
 }
 
-// export const useGetUserTokens = () => {
-//   // make api call to my-tokens
-//   const dispatch = useDispatch<AppDispatch>()
-//   const nft = useNftContract()
-
-//   return useCallback(async () => {
-//     const tokenIds = [2, 3]
-//     //
-//     console.log('gettting metadata')
-//     const promises = tokenIds.map(async (id: number) => {
-//       const uri = await nft?.tokenURI(Number(id))
-//       return uri
-//     })
-//     const uris = await Promise.all(promises)
-//     const metadataPromises = uris.map(async (uri: string) => {
-//       console.log({ uri })
-//       const json = await axios.get(uri)
-//       await wait(4000)
-//       return json.data
-//     })
-//     const jsons = await Promise.all(metadataPromises)
-//     dispatch(saveImages({ images: jsons, ids: tokenIds }))
-//     console.log({ jsons })
-//   }, [nft, dispatch])
-// }
-
-// export const useGetNftData = () => {
-//   const getUserTokens = useGetUserTokens()
-//   useEffect(() => {
-//     getUserTokens()
-//   }, [getUserTokens])
-// }
 export const useCreateFullCollection = (history: H.History) => {
   const deployCollection = useDeployCollection()
   const { chainId } = useActiveWeb3React()
