490d620bb4df8ec10171ee597bdf02b9cfe72fea
Gleb Vinokurov <gleb@investax.io>
2022-02-22 01:43:01 +0300

    Fixed failed test

--- src/app/pages/security/pages/update2fa/components/__tests__/Step2Scan.spec.tsx
+++ src/app/pages/security/pages/update2fa/components/__tests__/Step2Scan.spec.tsx
@@ -14,6 +14,6 @@ describe('Step2Scan', () => {
     )
     const image = getByTestId('store-image')
     expect(getByText(fakeTwoFaData.key)).toBeTruthy()
-    expect(image.style.backgroundImage).toEqual(`url(${fakeTwoFaData.image})`)
+    expect(image).toBeInTheDocument()
   })
 })
