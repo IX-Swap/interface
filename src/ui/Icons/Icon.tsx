accc4c77a2aaaa0372cd7b50c5603b1ce9f73064
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-02-25 16:41:51 +0800

    update svg fill values

--- src/ui/Icons/Icon.tsx
+++ src/ui/Icons/Icon.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import Icons from 'assets/icons/sprite/symbol/svg/sprite.symbol.svg'
+import { StyledSVG } from 'ui/Icons/Icon.styles'
 
 export interface IconProps {
   name: string
   width?: number
   height?: number
   color?: string
+  hoverColor?: string
 }
 
-export const Icon = ({ name, color, width = 24, height = 24 }: IconProps) => {
+export const Icon = ({
+  name,
+  color = '#778194',
+  hoverColor = '#4C88FF',
+  width = 24,
+  height = 24
+}: IconProps) => {
   return (
-    <svg width={`${width}px`} height={`${height}px`} fill={color}>
+    <StyledSVG
+      width={`${width}px`}
+      height={`${height}px`}
+      fill={color}
+      hoverColor={hoverColor}
+    >
       <use href={Icons + `#${name}`} />
-    </svg>
+    </StyledSVG>
   )
 }
