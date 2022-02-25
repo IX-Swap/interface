69420626f8395b1e596da4aa6ab0834582496903
Talgat Yessenbekov <taxa1991@gmail.com>
2022-02-25 17:53:21 +0600

    some fixes by review comments

--- src/components/form/FundSourceSelect.tsx
+++ src/components/form/FundSourceSelect.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { MenuItem, Select } from '@mui/material'
+import { MenuItem } from '@mui/material'
 import { renderMenuItems } from 'helpers/rendering'
 import { FUNDSOURCES_OPTS } from 'app/pages/identity/const'
+import { TextFieldSelect } from 'components/form/TextFieldSelect'
 
 export const FundSourceSelect = (props: any): JSX.Element => {
   return (
-    <Select {...props}>
+    <TextFieldSelect {...props}>
       <MenuItem disabled value={undefined}>
         Source of funds
       </MenuItem>
       {renderMenuItems(FUNDSOURCES_OPTS)}
-    </Select>
+    </TextFieldSelect>
   )
 }
