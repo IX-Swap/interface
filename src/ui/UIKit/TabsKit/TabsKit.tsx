b93a07c7e861cf4221c7435d4dc47cca0119cf37
Victoria Florea <victoria.florea@titanium-soft.com>
2022-03-15 11:32:58 +0200

    use uikittheme wrapper for tabs

--- src/ui/UIKit/TabsKit/TabsKit.tsx
+++ src/ui/UIKit/TabsKit/TabsKit.tsx
@@ -1,45 +1,16 @@
-import { Grid, Typography } from '@mui/material'
+import { Paper } from '@mui/material'
 import React from 'react'
-import { TabDemo } from 'ui/UIKit/TabsKit/TabDemo'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import { lightTheme } from 'themes/new/light'
-import { getThemeOverrides } from 'themes/new/overrides'
-import { typography } from 'themes/new/typography'
-import { darkTheme } from 'themes/new/dark'
 import { ContainedTabDemo } from 'ui/UIKit/TabsKit/ContainedTabDemo'
-
-const theme = createTheme({ ...lightTheme, typography })
-theme.components = getThemeOverrides(theme)
-
-const dTheme = createTheme({ ...darkTheme, typography })
-dTheme.components = getThemeOverrides(dTheme)
+import { TabDemo } from 'ui/UIKit/TabsKit/TabDemo'
+import { UIKitThemeWrapper } from '../UIKitThemeWrapper'
 
 export const TabsKit = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant='h6'>Tabs</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <ThemeProvider theme={theme}>
-          <TabDemo />
-        </ThemeProvider>
-      </Grid>
-      <Grid item xs={6}>
-        <ThemeProvider theme={dTheme}>
-          <TabDemo />
-        </ThemeProvider>
-      </Grid>
-      <Grid item xs={6}>
-        <ThemeProvider theme={theme}>
-          <ContainedTabDemo />
-        </ThemeProvider>
-      </Grid>
-      <Grid item xs={6}>
-        <ThemeProvider theme={dTheme}>
-          <ContainedTabDemo />
-        </ThemeProvider>
-      </Grid>
-    </Grid>
+    <UIKitThemeWrapper>
+      <Paper sx={{ padding: 2 }} elevation={0}>
+        <TabDemo />
+        <ContainedTabDemo />
+      </Paper>
+    </UIKitThemeWrapper>
   )
 }
