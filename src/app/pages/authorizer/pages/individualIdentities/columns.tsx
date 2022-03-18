c52363b722d785f9a56e27b3c8a6dce7b0345144
Victoria Florea <vica@investax.io>
2022-03-18 17:04:05 +0200

    Add risk rating to individuals tabel

--- src/app/pages/authorizer/pages/individualIdentities/columns.tsx
+++ src/app/pages/authorizer/pages/individualIdentities/columns.tsx
@@ -1,7 +1,19 @@
+import React from 'react'
 import { TableColumn } from 'types/util'
 import { formatDateToMMDDYY } from 'helpers/dates'
 import { renderIncome, renderLastName } from 'helpers/tables'
 import { IndividualIdentity } from 'app/pages/identity/types/forms'
+import { AuthorizableStatus } from 'app/pages/authorizer/components/AuthorizableStatus'
+
+const renderRiskReport = (rating?: string) => {
+  return (
+    <AuthorizableStatus
+      status={rating ?? 'UNKNOWN'}
+      compact={false}
+      isNewTheme
+    />
+  )
+}
 
 export const columns: Array<TableColumn<IndividualIdentity>> = [
   {
@@ -26,5 +38,6 @@ export const columns: Array<TableColumn<IndividualIdentity>> = [
     key: 'annualIncome',
     label: 'Annual Income',
     render: renderIncome
-  }
+  },
+  { key: 'cynopsis.riskRating', label: 'Risk Report', render: renderRiskReport }
 ]
