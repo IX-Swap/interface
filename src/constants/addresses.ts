6e95f07b2adfad201cf0a9662e2918d92fdb040e
itagaev <tagayev.ilias@gmail.com>
2022-04-04 15:50:06 +0600

    rename contract names

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -119,7 +119,7 @@ export const testStableCoinsTokens = [
 ]
 export const ixSwapToken = [
   {
-    name: 'IX Swap Token',
+    name: 'IXSwap Token',
     symbol: 'IXS',
     address: '0xA1997c88a60dCe7BF92A3644DA21e1FfC8F96dC2',
     contractAddress: '0x10367e146e497267a9fc0c4f47702005f194a13c',
