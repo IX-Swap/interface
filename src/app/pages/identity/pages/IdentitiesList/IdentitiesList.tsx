7e3522709a03ad0bce97d798998ec36da6f616fb
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-04-13 14:49:01 +0800

    identity selection screen redesigned. Added temporary individual Identity button

--- src/app/pages/identity/pages/IdentitiesList/IdentitiesList.tsx
+++ src/app/pages/identity/pages/IdentitiesList/IdentitiesList.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Grid } from '@mui/material'
-import { PageHeader } from 'app/components/PageHeader/PageHeader'
+import { Grid, Typography } from '@mui/material'
 import { LoadingIndicator } from 'app/components/LoadingIndicator/LoadingIndicator'
 import { useGetIdentities } from 'app/components/OnboardingPanel/hooks/useGetIdentities'
 import { NoIdentityView } from 'app/pages/identity/components/NoIdentityView/NoIdentityView'
@@ -17,15 +16,20 @@ export const IdentitiesList: React.FC = () => {
   }
 
   return (
-    <AppContentWrapper container background='light'>
-      <RootContainer background='light'>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <PageHeader
-              title='Identity'
-              alignment='flex-start'
-              showBreadcrumbs={false}
-            />
+    <AppContentWrapper container background='default'>
+      <RootContainer background='default'>
+        <Grid container spacing={5}>
+          <Grid item xs={12} container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant='h2' align='center'>
+                Create your Identity
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant='body1' align='center' color='textSecondary'>
+                Choose the type of Identity
+              </Typography>
+            </Grid>
           </Grid>
           <Grid item xs={12}>
             {hasIdentity || detailsOfIssuance !== undefined ? (
