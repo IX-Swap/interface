210e7376e0d01fe85640526100c30d56a2b83efa
Gleb Vinokurov <gleb@investax.io>
2022-04-14 03:13:20 +0300

    Resolved conflicts

--- src/ui/UIKit/UIKitThemeWrapper.tsx
+++ src/ui/UIKit/UIKitThemeWrapper.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement } from 'react'
 import { useDarkMode } from 'storybook-dark-mode'
 import { AppTheme, getAppTheme } from 'themes/app'
 import { ThemeProvider } from '@mui/material/styles'
-import { getThemeOverrides } from 'themes/app/overrides'
 import { Theme } from '@mui/material'
 
 export interface ChildrenProps {
@@ -16,52 +15,6 @@ export interface UIKitThemeWrapperProps {
 export const UIKitThemeWrapper = ({ children }: UIKitThemeWrapperProps) => {
   const themeModeFromStorybook = useDarkMode() ? AppTheme.Dark : AppTheme.Light
   const theme = getAppTheme(themeModeFromStorybook, false)
-
-  theme.components = {
-    ...getThemeOverrides(theme),
-    MuiSwitch: {
-      ...getThemeOverrides(theme)?.MuiSwitch,
-      defaultProps: {
-        disableTouchRipple: true
-      }
-    },
-    MuiRadio: {
-      ...getThemeOverrides(theme)?.MuiRadio,
-      defaultProps: {
-        disableTouchRipple: true
-      }
-    },
-    MuiCheckbox: {
-      ...getThemeOverrides(theme)?.MuiCheckbox,
-      defaultProps: {
-        disableTouchRipple: true
-      }
-    },
-    MuiButton: {
-      ...getThemeOverrides(theme)?.MuiButton,
-      defaultProps: {
-        disableTouchRipple: true
-      }
-    },
-    MuiButtonBase: {
-      ...getThemeOverrides(theme)?.MuiButtonBase,
-      defaultProps: {
-        disableTouchRipple: true
-      }
-    },
-    MuiIconButton: {
-      ...getThemeOverrides(theme)?.MuiIconButton,
-      defaultProps: {
-        disableTouchRipple: true
-      }
-    },
-    MuiFab: {
-      ...getThemeOverrides(theme)?.MuiFab,
-      defaultProps: {
-        disableTouchRipple: true
-      }
-    }
-  }
 
   return (
     <ThemeProvider theme={theme}>
