bc3b8aa78cf6d251464f6a2faeb78e12a8b68c37
Talgat Yessenbekov <talgat@investax.io>
2022-04-20 14:25:22 +0600

    fixes by comments

--- src/app/components/PageHeader/PageHeader.styles.ts
+++ src/app/components/PageHeader/PageHeader.styles.ts
@@ -20,13 +20,29 @@ export const useStyles = makeStyles(theme => ({
     background: 'rgba(255, 255, 255, 0.75)',
     border: 'none',
     height: '120px',
-    width: `100vw`,
+    width: '100vw',
     left: `50%`,
     right: `50%`,
     marginLeft: `-50vw`,
     marginRight: `-50vw`,
-    marginTop: `-1vw`,
+    marginTop: `-3vw`,
     padding: `3% 16% 10% 16%`,
-    marginBottom: `2vw`
+    marginBottom: `2vw`,
+
+    [theme.breakpoints.down('md')]: {
+      width: `102vw`
+    },
+
+    [theme.breakpoints.down('sm')]: {
+      width: `125vw`
+    },
+
+    [theme.breakpoints.down('600')]: {
+      width: `105vw`
+    },
+
+    [theme.breakpoints.down('400')]: {
+      width: `31rem`
+    }
   }
 }))
