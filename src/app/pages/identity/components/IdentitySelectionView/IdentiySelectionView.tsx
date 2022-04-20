349fbbd17056d0df06a2da90da9eddd5cdc98e25
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-04-20 15:10:40 +0800

    put identity preview back

--- src/app/pages/identity/components/IdentitySelectionView/IdentiySelectionView.tsx
+++ src/app/pages/identity/components/IdentitySelectionView/IdentiySelectionView.tsx
@@ -5,72 +5,56 @@ import { ReactComponent as CorporateImage } from 'assets/images/corporate.svg'
 import { ReactComponent as IssuerImage } from 'assets/images/issuer.svg'
 import { Icon } from 'ui/Icons/Icon'
 import { IdentityRoute } from 'app/pages/identity/router/config'
-import { useGetIdentities } from 'app/components/OnboardingPanel/hooks/useGetIdentities'
 
 export const IdentitySelectionView = () => {
-  const { hasIdentity, individualIdentity, corporateIdentities } =
-    useGetIdentities()
-
-  const hasIndividual = individualIdentity !== undefined
-  const hasCorporate =
-    corporateIdentities !== undefined && corporateIdentities.list.length > 0
-
   return (
     <Grid container spacing={3} justifyContent='center'>
-      {!hasIdentity ||
-      (hasCorporate && corporateIdentities.list[0].type === 'investor') ? (
-        <Grid item>
-          <CorporateIdentityCard
-            title='Corporate'
-            description='Suitable for users who want to invest via a corporate capacity'
-            image={<CorporateImage />}
-            actionButton={
-              <Button
-                disableElevation
-                fullWidth
-                variant='outlined'
-                href={IdentityRoute.createCorporate}
-                startIcon={<Icon name='plus' />}
-              >
-                Corporate Identity
-              </Button>
-            }
-          />
-        </Grid>
-      ) : null}
-      {!hasIdentity ||
-      (hasCorporate && corporateIdentities.list[0].type === 'issuer') ? (
-        <Grid item>
-          <CorporateIdentityCard
-            title='Issuer'
-            description='Suitable for users who want to use platform for fundraising'
-            image={<IssuerImage />}
-            actionButton={
-              <Button
-                disableElevation
-                fullWidth
-                variant='outlined'
-                href={IdentityRoute.createIssuer}
-                startIcon={<Icon name='plus' />}
-              >
-                Issuer Identity
-              </Button>
-            }
-          />
-        </Grid>
-      ) : null}
-      {!hasIdentity || hasIndividual ? (
-        <Grid item xs={12} container justifyContent='center'>
-          <Button
-            disableElevation
-            variant='outlined'
-            href={IdentityRoute.createIndividual}
-            startIcon={<Icon name='plus' />}
-          >
-            Individual Identity
-          </Button>
-        </Grid>
-      ) : null}
+      <Grid item>
+        <CorporateIdentityCard
+          title='Corporate'
+          description='Suitable for users who want to invest via a corporate capacity'
+          image={<CorporateImage />}
+          actionButton={
+            <Button
+              disableElevation
+              fullWidth
+              variant='outlined'
+              href={IdentityRoute.createCorporate}
+              startIcon={<Icon name='plus' />}
+            >
+              Corporate Identity
+            </Button>
+          }
+        />
+      </Grid>
+      <Grid item>
+        <CorporateIdentityCard
+          title='Issuer'
+          description='Suitable for users who want to use platform for fundraising'
+          image={<IssuerImage />}
+          actionButton={
+            <Button
+              disableElevation
+              fullWidth
+              variant='outlined'
+              href={IdentityRoute.createIssuer}
+              startIcon={<Icon name='plus' />}
+            >
+              Issuer Identity
+            </Button>
+          }
+        />
+      </Grid>
+      <Grid item xs={12} container justifyContent='center'>
+        <Button
+          disableElevation
+          variant='outlined'
+          href={IdentityRoute.createIndividual}
+          startIcon={<Icon name='plus' />}
+        >
+          Individual Identity
+        </Button>
+      </Grid>
     </Grid>
   )
 }
--- src/app/pages/identity/pages/IdentitiesList/IdentitiesList.tsx
+++ src/app/pages/identity/pages/IdentitiesList/IdentitiesList.tsx
@@ -3,8 +3,12 @@ import { Grid, Typography } from '@mui/material'
 import { RootContainer } from 'ui/RootContainer'
 import { AppContentWrapper } from 'ui/AppContentWrapper'
 import { IdentitySelectionView } from 'app/pages/identity/components/IdentitySelectionView/IdentiySelectionView'
+import { useGetIdentities } from 'app/components/OnboardingPanel/hooks/useGetIdentities'
+import { IdentityPreview } from 'app/pages/identity/components/IdentityPreview/IdentityPreview'
 
 export const IdentitiesList: React.FC = () => {
+  const { hasIdentity } = useGetIdentities()
+
   return (
     <AppContentWrapper container background='default'>
       <RootContainer background='default'>
@@ -12,17 +16,23 @@ export const IdentitiesList: React.FC = () => {
           <Grid item xs={12} container spacing={1}>
             <Grid item xs={12}>
               <Typography variant='h2' align='center'>
-                Create your Identity
+                {hasIdentity ? 'Identity' : 'Create your Identity'}
               </Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant='body1' align='center' color='textSecondary'>
-                Choose the type of Identity
-              </Typography>
-            </Grid>
+            {!hasIdentity && (
+              <Grid item xs={12}>
+                <Typography
+                  variant='body1'
+                  align='center'
+                  color='textSecondary'
+                >
+                  Choose the type of Identity
+                </Typography>
+              </Grid>
+            )}
           </Grid>
           <Grid item xs={12}>
-            <IdentitySelectionView />
+            {hasIdentity ? <IdentityPreview /> : <IdentitySelectionView />}
           </Grid>
         </Grid>
       </RootContainer>
