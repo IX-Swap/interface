3451a4e63063983224a450d8ea93034bd0ef676a
Gleb Vinokurov <gleb@investax.io>
2022-04-22 23:59:10 +0300

    Removed some unusable files

--- src/app/components/OnboardingPanel/CorporateOnboardingSteps.tsx
+++
@@ -1,8 +0,0 @@
-import { useOnboardingSteps } from 'app/hooks/onboarding/useOnboardingSteps'
-import { OnboardingSteps } from 'app/components/OnboardingPanel/OnboardingSteps'
-import React from 'react'
-
-export const CorporateOnboardingSteps = () => {
-  const { steps, activeStep } = useOnboardingSteps('corporate')
-  return <OnboardingSteps steps={steps} activeStep={activeStep} />
-}
