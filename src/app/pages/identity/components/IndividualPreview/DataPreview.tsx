b7e8d8d557e90fe1aa5b649d0cd02c140c4a303a
AlexandrZemtsov <alexandr.zemtsov@titanium-tech.net>
2022-04-25 23:19:25 +0600

    changed avatar's position and added user's name

--- src/app/pages/identity/components/IndividualPreview/DataPreview.tsx
+++ src/app/pages/identity/components/IndividualPreview/DataPreview.tsx
@@ -1,28 +1,56 @@
 import React from 'react'
-import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
+import { useStyles } from './DataPreview.styles'
+// import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
 import { Avatar } from 'components/Avatar'
-import { Box } from '@mui/material'
+import { Box, Typography, Grid, Container } from '@mui/material'
 import { FieldsDisplay } from 'app/pages/identity/components/IndividualPreview/FieldDisplay'
 
 export interface DataPreviewProps {
   avatar?: string
   userId?: string
   fields?: Array<{ key: string; value?: string }>
+  name?: string
 }
 
-export const DataPreview = ({ avatar, userId, fields }: DataPreviewProps) => {
-  const { isMobile } = useAppBreakpoints()
+export const DataPreview = ({
+  avatar,
+  userId,
+  fields,
+  name
+}: DataPreviewProps) => {
+  // const { isMobile } = useAppBreakpoints()
+  const classes = useStyles()
 
   return (
-    <Box display='flex' flexDirection={isMobile ? 'column' : 'row'}>
-      <Avatar
-        documentId={avatar}
-        ownerId={userId}
-        variant='circular'
-        size={96}
-      />
-      <Box p={3} />
+    <>
+      <Container className={classes.container}>
+        <Grid>
+          <Grid
+            className={classes.container}
+            item
+            maxWidth={150}
+            marginBottom={'24px'}
+          >
+            <Avatar
+              documentId={avatar}
+              ownerId={userId}
+              variant='circular'
+              size={120}
+              borderRadius={100}
+            />
+          </Grid>
+          <Grid item style={{ textAlign: 'center' }}>
+            <Typography className={classes.textName}>{name}</Typography>
+          </Grid>
+          <Grid item style={{ textAlign: 'center', marginTop: '8px' }}>
+            <Typography className={classes.textCorporate}>
+              Corporate Issuer Identity
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+      <Box height={'40px'} />
       <FieldsDisplay fields={fields} />
-    </Box>
+    </>
   )
 }
