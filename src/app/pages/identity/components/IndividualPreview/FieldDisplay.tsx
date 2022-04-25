abfce809f902ce357b48709a428612a96b26ef1b
AlexandrZemtsov <alexandr.zemtsov@titanium-tech.net>
2022-04-25 22:19:56 +0600

    changed fields' size, made them containers

--- src/app/pages/identity/components/IndividualPreview/FieldDisplay.tsx
+++ src/app/pages/identity/components/IndividualPreview/FieldDisplay.tsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import { LabelledValue } from 'components/LabelledValue'
 import { Grid } from '@mui/material'
+import { useStyles } from './FieldsDsiplay.style'
 
 export interface FieldsDisplayProps {
   fields?: Array<{ key: string; value?: string }>
 }
 
 export const FieldsDisplay = ({ fields }: FieldsDisplayProps) => {
+  const classes = useStyles()
   if (fields === undefined || fields.length < 1) {
     return null
   }
 
   return (
-    <Grid container spacing={2}>
+    <Grid
+      container
+      spacing={2}
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
       {fields.map((field, i) => (
-        <Grid item xs={12} md={4} key={i}>
+        <Grid item key={i}>
           {field.key !== '' ? (
-            <LabelledValue label={field.key} value={field.value} />
+            <LabelledValue
+              className={classes.label}
+              label={field.key}
+              value={field.value}
+            />
           ) : null}
         </Grid>
       ))}
---
+++ src/app/pages/identity/components/IndividualPreview/FieldsDsiplay.style.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useStyles = makeStyles({
+  label: {
+    backgroundColor: '#E5E5E5',
+    borderRadius: '8px',
+    height: '92px',
+    width: '400px'
+  }
+})
