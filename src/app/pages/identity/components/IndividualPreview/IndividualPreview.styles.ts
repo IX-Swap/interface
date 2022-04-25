7abf82f9da5282c9dacfb95cb3ea401be6df12d4
AlexandrZemtsov <alexandr.zemtsov@titanium-tech.net>
2022-04-25 22:31:13 +0600

    changed edit and view buttons position and removed PreviewHeader

---
+++ src/app/pages/identity/components/IndividualPreview/IndividualPreview.styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useStyles = makeStyles({
+  editButton: {
+    position: 'absolute',
+    right: '184px',
+    top: '304px'
+  }
+})
