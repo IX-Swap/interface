013dcd6158708e062d5457b0601c3a6c606fbbce
AlexandrZemtsov <alexandr.zemtsov@titanium-tech.net>
2022-04-26 12:00:55 +0600

    added name as a prop to DataPreview

--- src/app/pages/identity/components/IndividualPreview/IndividualPreview.tsx
+++ src/app/pages/identity/components/IndividualPreview/IndividualPreview.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { useStyles } from './IndividualPreview.styles'
 import { Box, Card, CardContent, CardActions } from '@mui/material'
 import { EditButton } from 'app/pages/identity/components/EditButton/EditButton'
 import { IdentityRoute } from 'app/pages/identity/router/config'
 import { ViewButton } from 'app/pages/identity/components/ViewButton/ViewButton'
 import { IndividualIdentity } from 'app/pages/identity/types/forms'
-import { PreviewHeader } from 'app/pages/identity/components/IndividualPreview/PreviewHeader'
 import { DataPreview } from 'app/pages/identity/components/IndividualPreview/DataPreview'
 
 export interface IndividualPreviewProps {
@@ -12,6 +12,8 @@ export interface IndividualPreviewProps {
 }
 
 export const IndividualPreview = ({ data }: IndividualPreviewProps) => {
+  const classes = useStyles()
+
   if (data === undefined) {
     return null
   }
@@ -38,10 +40,6 @@ export const IndividualPreview = ({ data }: IndividualPreviewProps) => {
       value: data.nationality
     },
     {
-      key: '',
-      value: ''
-    },
-    {
       key: 'Email',
       value: data.email
     },
@@ -57,16 +55,21 @@ export const IndividualPreview = ({ data }: IndividualPreviewProps) => {
     <Card elevation={0}>
       <CardContent>
         <Box pt={2} pb={2}>
-          <PreviewHeader title='Individual Investor' status={data.status} />
           <DataPreview
             avatar={data.photo}
             userId={data.user._id}
             fields={individualIdentityFields}
+            name={data.user.name}
           />
         </Box>
       </CardContent>
       <CardActions>
-        <Box display='flex' justifyContent='flex-end' width='100%'>
+        <Box
+          display='flex'
+          justifyContent='flex-end'
+          width='100%'
+          className={classes.editButton}
+        >
           <EditButton
             link={IdentityRoute.editIndividual}
             params={{
