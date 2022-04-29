749df87ecad06b162b934f5336ae6fe00d879a8d
Gleb Vinokurov <gleb@investax.io>
2022-04-29 17:45:28 +0300

    Fixed failed tests

--- src/app/pages/security/pages/update2fa/components/__tests__/Step1RemoveAuthenticator.spec.tsx
+++ src/app/pages/security/pages/update2fa/components/__tests__/Step1RemoveAuthenticator.spec.tsx
@@ -1,31 +1,15 @@
 import React from 'react'
 import { render } from 'test-utils'
 import { Step1RemoveAuthenticator } from 'app/pages/security/pages/update2fa/components/Step1RemoveAuthenticator/Step1RemoveAuthenticator'
-import * as useGetEmailCode from 'app/pages/security/pages/update2fa/hooks/useGetEmailCode'
-import { fireEvent, waitFor } from '@testing-library/dom'
 
 describe('Step1RemoveAuthenticator', () => {
   const handleSuccessfulRemoveAuthenticator = jest.fn()
-
-  const getEmailCodeSuccessfulResponse = {
-    data: { email: 'test' },
-    refetch: jest.fn()
-  }
-
-  const getEmailCodeUnsuccessfulResponse = {
-    data: undefined,
-    refetch: jest.fn()
-  }
 
   afterEach(async () => {
     jest.clearAllMocks()
   })
 
   it('should match snapshot when useGetEmailCode has successful response', () => {
-    jest
-      .spyOn(useGetEmailCode, 'useGetEmailCode')
-      .mockImplementation(() => getEmailCodeSuccessfulResponse as any)
-
     const { container } = render(
       <Step1RemoveAuthenticator
         onSuccessRemoveAuthenticator={handleSuccessfulRemoveAuthenticator}
