405c66c8a9bc99da747b97b0f482fab8c9a94426
Victoria Florea <vica@investax.io>
2022-05-04 13:24:01 +0300

    Create snap of BlockchainAddress component

---
+++ src/app/pages/invest/components/__tests__/BlockchainAddress.spec.tsx
@@ -0,0 +1,19 @@
+import { BlockchainAddress } from 'app/pages/invest/components/BlockchainWallet/BlockchainAddress'
+import React from 'react'
+import { render } from 'test-utils'
+
+describe('BlockchainAddress', () => {
+  afterEach(async () => {
+    jest.clearAllMocks()
+  })
+
+  it('should match snapshot', () => {
+    const { container } = render(
+      <BlockchainAddress
+        account={'0x67ed490d810c41263758e7355cef720ffed68cbc'}
+        chainId={1}
+      />
+    )
+    expect(container).toMatchSnapshot()
+  })
+})
