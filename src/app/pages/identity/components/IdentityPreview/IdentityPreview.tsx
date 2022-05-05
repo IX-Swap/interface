57cf60b1ddc50e3c1456c08a5bade49b05789131
Alexandr Zemtsov <alexandr.zemtsov@titanium-tech.net>
2022-05-05 15:03:04 +0600

    put back identityBUtton

--- src/app/pages/identity/components/IdentityPreview/IdentityPreview.tsx
+++ src/app/pages/identity/components/IdentityPreview/IdentityPreview.tsx
@@ -2,9 +2,13 @@ import { Grid } from '@mui/material'
 import { LoadingIndicator } from 'app/components/LoadingIndicator/LoadingIndicator'
 import { useGetIdentities } from 'app/components/OnboardingPanel/hooks/useGetIdentities'
 import { CorporatesPreview } from 'app/pages/identity/components/CorporatesPreview/CorporatesPreview'
+// import { CorporateIdentityButton } from 'app/pages/identity/components/IdentityPreview/CorporateIdentityButton'
+// import { IndividualIdentityButton } from 'app/pages/identity/components/IdentityPreview/IndividualIdentityButton'
+// import { IssuerIdentityButton } from 'app/pages/identity/components/IdentityPreview/IssuerIdentityButton'
 import { IndividualPreview } from 'app/pages/identity/components/IndividualPreview/IndividualPreview'
+// import { CreateDetailsOfIssuanceButton } from 'app/pages/identity/components/NoIdentityView/CreateDetailsOfIssuanceButton'
+// import { CreateIssuerIdentityButton } from 'app/pages/identity/components/NoIdentityView/CreateIssuerIdentityButton'
 import React, { useState } from 'react'
-
 export const IdentityPreview = () => {
   const {
     hasIdentity,
@@ -13,23 +17,58 @@ export const IdentityPreview = () => {
     corporateIdentities,
     detailsOfIssuance
   } = useGetIdentities()
-
   const hasIndividual = individualIdentity !== undefined
+  // const hasCorporate =
+  //   corporateIdentities !== undefined && corporateIdentities.list.length > 0
   const hasDetailsOfIssuance = detailsOfIssuance !== undefined
+  // const detailsOfIssuanceApproved =
+  //   detailsOfIssuance?.status === 'Approved' ?? false
+  // const detailsOfIssuanceSkipped = detailsOfIssuance?.skipped ?? false
   const [selectedIdentity] = useState<'individual' | 'corporate'>(
     hasIndividual ? 'individual' : 'corporate'
   )
-
   if (isLoadingIdentities) {
     return <LoadingIndicator />
   }
-
   if (!hasIdentity && !hasDetailsOfIssuance) {
     return null
   }
-
   return (
     <Grid container>
+      <Grid item xs={12} container>
+        {/* {hasIndividual && (
+          <Grid item xs={12} md={4}>
+            <IndividualIdentityButton
+              active={selectedIdentity === 'individual'}
+              onClick={() => setSelectedIdentity('individual')}
+            />
+          </Grid>
+        )}
+        {hasIndividual && <Grid item xs={12} md={4}></Grid>}
+        {hasCorporate ? (
+          <Grid item xs={12} md={4}>
+            {corporateIdentities.list[0].type === 'issuer' ? (
+              <IssuerIdentityButton
+                active={selectedIdentity === 'corporate'}
+                onClick={() => setSelectedIdentity('corporate')}
+              />
+            ) : (
+              <CorporateIdentityButton
+                identity={corporateIdentities.list[0]}
+                active={selectedIdentity === 'corporate'}
+                onClick={() => setSelectedIdentity('corporate')}
+              />
+            )}
+          </Grid>
+        ) : (
+          hasDetailsOfIssuance &&
+          (detailsOfIssuanceApproved || detailsOfIssuanceSkipped ? (
+            <CreateIssuerIdentityButton />
+          ) : (
+            <CreateDetailsOfIssuanceButton />
+          ))
+        )} */}
+      </Grid>
       <Grid item xs={12}>
         {selectedIdentity === 'individual' ? (
           <IndividualPreview data={individualIdentity} />
