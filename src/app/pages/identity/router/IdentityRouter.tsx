7d6a4f5d71b6a17c32f430554ed451d873c990d0
Alexandr Zemtsov <alexandr.zemtsov@titanium-tech.net>
2022-05-23 19:29:35 +0600

    resolved conflict

--- src/app/pages/identity/router/IdentityRouter.tsx
+++ src/app/pages/identity/router/IdentityRouter.tsx
@@ -1,11 +1,11 @@
 import { IdentitiesList } from 'app/pages/identity/pages/IdentitiesList/IdentitiesList'
+import { CorporateRouter } from 'app/pages/identity/router/CorporateRouter'
+import { DetailsOfIssuanceRouter } from 'app/pages/identity/router/DetailsOfIssuanceRouter'
+import { IndividualRouter } from 'app/pages/identity/router/IndividualRouter'
+import { AppRoute } from 'components/AppRoute'
 import React from 'react'
 import { Switch } from 'react-router-dom'
 import { IdentityRoute } from './config'
-import { AppRoute } from 'components/AppRoute'
-import { IndividualRouter } from 'app/pages/identity/router/IndividualRouter'
-import { CorporateRouter } from 'app/pages/identity/router/CorporateRouter'
-import { DetailsOfIssuanceRouter } from 'app/pages/identity/router/DetailsOfIssuanceRouter'
 import { SuccessPage } from 'app/pages/identity/pages/SuccessPage/SuccessPage'
 
 export const IdentityRouter = () => {
