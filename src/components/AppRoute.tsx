a21be981ca178a45eff53ec3ead0e3f7ba0e58b9
Gleb Vinokurov <gleb@investax.io>
2022-05-26 19:57:19 +0300

    Made some fixes for resolved failed unit test

--- src/components/AppRoute.tsx
+++ src/components/AppRoute.tsx
@@ -4,6 +4,8 @@ import { SentryRoute } from 'components/SentryRoute'
 import { useCachedUser } from 'hooks/auth/useCachedUser'
 import React, { memo } from 'react'
 import { RouteProps, Redirect } from 'react-router-dom'
+import { useIsAccredited } from 'helpers/acl'
+import { AppRoute as AppPath } from 'app/router/config'
 
 export interface AppRouteProps extends RouteProps {
   path: string
@@ -14,10 +16,21 @@ export interface AppRouteProps extends RouteProps {
 export const AppRoute = memo((props: AppRouteProps) => {
   const { breadcrumb, path, children, ...rest } = props
   const user = useCachedUser()
+  const isAccredited = useIsAccredited()
 
   if (user === undefined) {
     if (!path.startsWith('/auth')) {
       return <Redirect to='/auth' />
+    }
+  } else {
+    if (
+      !isAccredited &&
+      !path.startsWith(AppPath.educationCentre) &&
+      !path.startsWith(AppPath.identity) &&
+      !path.startsWith(AppPath.security) &&
+      !path.startsWith(AppPath.notifications)
+    ) {
+      return <Redirect to={AppPath.identity} />
     }
   }
 
