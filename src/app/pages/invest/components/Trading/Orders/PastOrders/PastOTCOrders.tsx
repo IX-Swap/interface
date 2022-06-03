0b4202f579d427475febfc45a3287b846bf7ed66
Victoria Florea <vica@investax.io>
2022-06-03 09:24:47 +0300

    Remove yet from title

--- src/app/pages/invest/components/Trading/Orders/PastOrders/PastOTCOrders.tsx
+++ src/app/pages/invest/components/Trading/Orders/PastOrders/PastOTCOrders.tsx
@@ -33,7 +33,7 @@ export const PastOTCOrders = (props: PostOrderTableProps) => {
           noHeader={isMiniLaptop}
           noDataComponent={
             <EmptyState
-              title='No past orders yet'
+              title='No past orders'
               subtitle='You haven’t made any orders on this wallet'
               isRow={false}
             />
