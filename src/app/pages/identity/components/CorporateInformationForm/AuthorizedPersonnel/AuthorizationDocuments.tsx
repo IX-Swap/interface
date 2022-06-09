b53c4f9487c40949fd50c88a7ff6dc4235fe4523
Gleb Vinokurov <gleb@investax.io>
2022-06-09 14:23:08 +0300

    Fixed UI logic for Authorization Document block

--- src/app/pages/identity/components/CorporateInformationForm/AuthorizedPersonnel/AuthorizationDocuments.tsx
+++ src/app/pages/identity/components/CorporateInformationForm/AuthorizedPersonnel/AuthorizationDocuments.tsx
@@ -5,6 +5,7 @@ import { ValidateOnMount } from 'app/pages/identity/components/ValidateOnMount'
 import { pathToString } from 'helpers/forms'
 import { FormSectionHeader } from 'app/pages/identity/components/FormSectionHeader'
 import { UploadDocumentField } from 'app/pages/identity/components/UploadDocumentsForm/UploadDocumentField/UploadDocumentField'
+import { Divider } from 'ui/Divider'
 
 export const AuthorizationDocuments = ({
   fieldId,
@@ -19,14 +20,15 @@ export const AuthorizationDocuments = ({
       <Grid item xs={12}>
         <FormSectionHeader title='Authorization Document' />
       </Grid>
-      <Grid item>
-        <Typography variant='caption' color='#778194'>
+      <Grid item xs={12}>
+        <Typography variant='caption' color='#778194' mb={2} display={'block'}>
           Board Resolution, Power of Attorney, Partnership Deed, Trust Deed, and
           Others.
         </Typography>
+        <UploadDocumentField name={fieldName} label='' />
       </Grid>
       <Grid item xs={12}>
-        <UploadDocumentField name={fieldName} label='' />
+        <Divider />
       </Grid>
       <ValidateOnMount />
     </Grid>
