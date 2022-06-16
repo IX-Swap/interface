9caf50ac858e9f35ce86fba1a13352ae27f88084
Gleb Vinokurov <gleb@investax.io>
2022-06-16 19:26:52 +0300

    Fixed failed unit test

--- src/app/pages/identity/components/InvestorDeclarationForm/__tests__/InvestorAgreements.spec.tsx
+++ src/app/pages/identity/components/InvestorDeclarationForm/__tests__/InvestorAgreements.spec.tsx
@@ -2,29 +2,45 @@ import { InvestorAgreements } from 'app/pages/identity/components/InvestorDeclar
 import { Form } from 'components/form/Form'
 import React from 'react'
 import { render } from 'test-utils'
+import {
+  corporateInvestorAgreementsMap,
+  individualInvestorAgreementsMap
+} from 'app/pages/identity/components/InvestorDeclarationForm/InvestorAgreements/agreements'
 
 describe('InvestorAgreements', () => {
   afterEach(async () => {
     jest.clearAllMocks()
   })
 
-  it('should match snapshot when type is corporate', () => {
-    const { container } = render(
+  it('renders correct data when type is corporate', () => {
+    const { getByText } = render(
       <Form>
         <InvestorAgreements type='corporate' />
       </Form>
     )
 
-    expect(container).toMatchSnapshot()
+    expect(
+      getByText('I declare that I am "Corporate Accredited Investor"')
+    ).toBeTruthy()
+
+    for (const item of Object.values(corporateInvestorAgreementsMap)) {
+      expect(getByText(item)).toBeTruthy()
+    }
   })
 
-  it('should match snapshot when type is individual', () => {
-    const { container } = render(
+  it('renders correct data when type is individual', () => {
+    const { getByText } = render(
       <Form>
         <InvestorAgreements type='individual' />
       </Form>
     )
 
-    expect(container).toMatchSnapshot()
+    expect(
+      getByText('I declare that I am "Individual Accredited Investor"')
+    ).toBeTruthy()
+
+    for (const item of Object.values(individualInvestorAgreementsMap)) {
+      expect(getByText(item)).toBeTruthy()
+    }
   })
 })
