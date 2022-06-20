9ee7b2646a2e852026d6ae1a819a384b8b06a0cf
Gleb Vinokurov <gleb@investax.io>
2022-06-20 11:06:03 +0300

    Resolved conflicts, added more test cases

--- src/app/pages/identity/components/TaxDeclarationForm/__tests__/TaxDeclarationForm.spec.tsx
+++ src/app/pages/identity/components/TaxDeclarationForm/__tests__/TaxDeclarationForm.spec.tsx
@@ -1,37 +1,49 @@
 import { TaxDeclarationForm } from 'app/pages/identity/components/TaxDeclarationForm/TaxDeclarationForm'
 import React from 'react'
 import { render } from 'test-utils'
-import { TaxResidencyFields } from 'app/pages/identity/components/TaxDeclarationForm/TaxResidencyFields/TaxResidencyFields'
-import { UsCitizenshipConfirmation } from 'app/pages/identity/components/TaxDeclarationForm/UsCitizenshipConfirmation/UsCitizenshipConfirmation'
 import { Form } from 'components/form/Form'
+import { TaxResidencyFieldArray } from 'app/pages/identity/components/TaxDeclarationForm/TaxResidencyFields/TaxRecidencyFieldArray'
+import { FormSectionHeader } from 'app/pages/identity/components/FormSectionHeader'
+import { TaxDeclarationInfo } from 'app/pages/identity/components/TaxDeclarationForm/TaxDeclarationInfo/TaxDeclarationInfo'
 
 jest.mock(
-  'app/pages/identity/components/TaxDeclarationForm/TaxResidencyFields/TaxResidencyFields',
+  'app/pages/identity/components/TaxDeclarationForm/TaxResidencyFields/TaxRecidencyFieldArray',
   () => ({
-    TaxResidencyFields: jest.fn(() => null)
+    TaxResidencyFieldArray: jest.fn(() => null)
   })
 )
 
 jest.mock(
-  'app/pages/identity/components/TaxDeclarationForm/UsCitizenshipConfirmation/UsCitizenshipConfirmation',
+  'app/pages/identity/components/TaxDeclarationForm/TaxDeclarationInfo/TaxDeclarationInfo',
   () => ({
-    UsCitizenshipConfirmation: jest.fn(() => null)
+    TaxDeclarationInfo: jest.fn(() => null)
   })
 )
+
+jest.mock('app/pages/identity/components/FormSectionHeader', () => ({
+  FormSectionHeader: jest.fn(() => null)
+}))
 
 describe('TaxDeclarationForm', () => {
   afterEach(async () => {
     jest.clearAllMocks()
   })
 
-  it('renders child components correctly', () => {
+  it('renders child components with correct props', () => {
     render(
       <Form>
         <TaxDeclarationForm />
       </Form>
     )
 
-    expect(TaxResidencyFields).toHaveBeenCalled()
-    expect(UsCitizenshipConfirmation).toHaveBeenCalled()
+    expect(FormSectionHeader).toHaveBeenCalledWith(
+      { title: 'Tax Declaration' },
+      {}
+    )
+    expect(TaxDeclarationInfo).toHaveBeenCalled()
+    expect(TaxResidencyFieldArray).toHaveBeenCalledWith(
+      { identityType: 'individual' },
+      {}
+    )
   })
 })
