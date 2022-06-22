4663306fd671c0e2b4ddbe8a313425c341a5c4d4
axelforce <oleksandr.kulagin@gmail.com>
2022-06-22 11:15:27 +0300

    remove unused, old packages

--- e2e_tests/helpers/web3/metamaskData.ts
+++ e2e_tests/helpers/web3/metamaskData.ts
@@ -1,4 +1,4 @@
 export const metamaskWallet = {
-  ethAddress: '0x06cE64197bC6BC56ea772330f97E38923E46Aa78',
+  ethAddress: '0xaCDaA0810c5bb1A5F2ef9370b590C229464a1C65',
   privateKey: process.env.METAMASK_PRIVATE_KEY
 };
