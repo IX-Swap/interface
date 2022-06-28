14038ec9e937f41831e758bc3c97aff2bb062b68
Gleb Vinokurov <gleb@investax.io>
2022-06-28 18:26:11 +0300

    Added identity preview page, fixed some bugs

--- src/app/pages/identity/pages/ViewIndividual/ViewIndividual.tsx
+++ src/app/pages/identity/pages/ViewIndividual/ViewIndividual.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Grid } from '@mui/material'
-import { VSpacer } from 'components/VSpacer'
 import { RejectionMessage } from 'app/pages/authorizer/components/RejectionMessage'
 import { IndividualIdentityView } from 'app/pages/identity/components/IndividualIdentityView/IndividualIdentityView'
 import { IdentityRoute } from 'app/pages/identity/router/config'
@@ -9,8 +8,14 @@ import { getPersonName } from 'helpers/strings'
 import { IndividualIdentityContainer } from 'app/pages/identity/containers/IndividualIdentityContainer'
 import { EditButton } from 'app/pages/identity/components/EditButton/EditButton'
 import { RootContainer } from 'ui/RootContainer'
+import { useStyles } from 'app/components/FormStepper/FormStepper.styles'
+import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
+import { TwoFANotice } from 'app/components/FormStepper/TwoFANotice'
 
 export const ViewIndividual = () => {
+  const classes = useStyles()
+  const { isMobile, isTablet } = useAppBreakpoints()
+
   return (
     <IndividualIdentityContainer
       component={({ data }) => (
@@ -19,26 +24,31 @@ export const ViewIndividual = () => {
             <PageHeader title={getPersonName(data)} />
           </Grid>
           <RootContainer>
-            <Grid item xs={12}>
-              <RejectionMessage data={data} />
-            </Grid>
-            <Grid
-              container
-              item
-              xs={12}
-              justifyContent='flex-end'
-              alignItems='center'
-            >
-              <EditButton
-                link={IdentityRoute.editIndividual}
-                params={{ identityId: data._id, userId: data.user._id }} // TODO: ask backend to unify user field for all objects
-              />
-            </Grid>
-            <Grid item container xs={12}>
-              <VSpacer size='small' />
-            </Grid>
-            <Grid item xs={12}>
-              <IndividualIdentityView data={data} />
+            <Grid container direction={isTablet ? 'column-reverse' : 'row'}>
+              <Grid item xs={12}>
+                <RejectionMessage data={data} />
+              </Grid>
+
+              <Grid item className={classes.content}>
+                <IndividualIdentityView data={data} />
+              </Grid>
+
+              <Grid container item className={classes.rightBlock}>
+                <Grid item xs={12}>
+                  <EditButton
+                    fullWidth
+                    variant={'contained'}
+                    link={IdentityRoute.editIndividual}
+                    params={{ identityId: data._id, userId: data.user._id }} // TODO: ask backend to unify user field for all objects
+                  />
+                </Grid>
+
+                {!isMobile && (
+                  <Grid item xs={12}>
+                    <TwoFANotice />
+                  </Grid>
+                )}
+              </Grid>
             </Grid>
           </RootContainer>
         </Grid>
--- src/app/pages/identity/pages/ViewInvestor/ViewInvestor.tsx
+++ src/app/pages/identity/pages/ViewInvestor/ViewInvestor.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Grid } from '@mui/material'
-import { VSpacer } from 'components/VSpacer'
+import { Grid, Paper } from '@mui/material'
 import { RejectionMessage } from 'app/pages/authorizer/components/RejectionMessage'
 import { CorporateIdentityView } from 'app/pages/identity/components/CorporateIdentityView/CorporateIdentityView'
 import { IdentityRoute } from 'app/pages/identity/router/config'
@@ -8,8 +7,14 @@ import { PageHeader } from 'app/components/PageHeader/PageHeader'
 import { CorporateIdentityContainer } from 'app/pages/identity/containers/CorporateIdentityContainer'
 import { EditButton } from 'app/pages/identity/components/EditButton/EditButton'
 import { RootContainer } from 'ui/RootContainer'
+import { useStyles } from 'app/components/FormStepper/FormStepper.styles'
+import { TwoFANotice } from 'app/components/FormStepper/TwoFANotice'
+import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
 
 export const ViewInvestor = () => {
+  const classes = useStyles()
+  const { isMobile, isTablet } = useAppBreakpoints()
+
   const editLinkMap = {
     investor: IdentityRoute.editCorporate,
     'Fund Manager': IdentityRoute.editFundManager,
@@ -18,34 +23,48 @@ export const ViewInvestor = () => {
     issuer: IdentityRoute.editIssuer
   }
 
+  const getTitleText = (type: string) => {
+    if (type === 'issuer') {
+      return 'View Corporate Issuer Identity'
+    }
+
+    return 'View Corporate Investor Identity'
+  }
+
   return (
     <CorporateIdentityContainer
       component={({ data }) => (
         <Grid container style={{ display: 'table' }}>
           <Grid item xs={12}>
-            <PageHeader title={data.companyLegalName} />
+            <PageHeader title={getTitleText(data.type)} />
           </Grid>
           <RootContainer>
-            <Grid item xs={12}>
-              <RejectionMessage data={data} />
-            </Grid>
-            <Grid
-              xs={12}
-              container
-              item
-              justifyContent='flex-end'
-              alignItems='center'
-            >
-              <EditButton
-                link={editLinkMap[data.type]}
-                params={{ identityId: data._id, userId: data.user._id }}
-              />
-            </Grid>
-            <Grid item xs={12} container>
-              <VSpacer size='small' />
-            </Grid>
-            <Grid item xs={12}>
-              <CorporateIdentityView data={data} />
+            <Grid container direction={isTablet ? 'column-reverse' : 'row'}>
+              <Grid item xs={12}>
+                <RejectionMessage data={data} />
+              </Grid>
+
+              <Grid item className={classes.content}>
+                <CorporateIdentityView data={data} />
+              </Grid>
+
+              <Grid container item className={classes.rightBlock}>
+                <Grid item xs={12}>
+                  <Paper sx={{ p: 4, borderRadius: 2 }}>
+                    <EditButton
+                      fullWidth
+                      variant={'contained'}
+                      link={editLinkMap[data.type]}
+                      params={{ identityId: data._id, userId: data.user._id }}
+                    />
+                  </Paper>
+                </Grid>
+                {!isMobile && (
+                  <Grid item xs={12}>
+                    <TwoFANotice />
+                  </Grid>
+                )}
+              </Grid>
             </Grid>
           </RootContainer>
         </Grid>
