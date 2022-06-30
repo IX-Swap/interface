d1d4d1bad4f1130e1aa3552ca817065b2ba77795
Gleb Vinokurov <gleb@investax.io>
2022-06-30 16:29:50 +0300

    Fixed issue with not showing CommitmentStats and InvestmentGrowth charts

--- src/helpers/getWeekDays.ts
+++ src/helpers/getWeekDays.ts
@@ -5,16 +5,7 @@ export const getWeekDays = (data: InvestmentGrowthData) => {
   if (typeof data === 'undefined') {
     return []
   }
-  const weekDays = data.reduce<Date[]>((a, d, i) => {
-    const noHours = removeHours(d[0])
-    const found = a.find(
-      el => new Date(el).getTime() === new Date(noHours).getTime()
-    )
-    if (typeof found === 'undefined') {
-      a.push(new Date(noHours))
-    }
-    return a
-  }, [])
+  const weekDays = data.map(item => new Date(item[0]))
   return weekDays
 }
 
