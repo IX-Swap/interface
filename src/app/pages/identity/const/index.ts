12896503ea24f3714126a11c4c602dc34e1d4c32
Victoria Florea <vica@investax.io>
2022-06-30 14:12:19 +0300

    Delete Marital Status Select

--- src/app/pages/identity/const/index.ts
+++ src/app/pages/identity/const/index.ts
@@ -3,8 +3,7 @@ import nationalities from 'app/pages/identity/const/nationalities'
 import fundSources from 'app/pages/identity/const/fundSources'
 
 export * from 'app/pages/identity/const/countries'
-export const MARITAL_STATUSES = ['Married', 'Widowed', 'Separated', 'Single']
-export const MARITAL_STATUSES_OPTS = arrToOpts(MARITAL_STATUSES)
+
 export const GENDERS = ['M', 'F', 'O']
 export const GENDERS_OPTS = [
   { value: 'M', label: 'Male' },
