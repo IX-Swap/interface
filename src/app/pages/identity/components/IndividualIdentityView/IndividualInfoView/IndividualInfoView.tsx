ace9dee982fd0a7131aad9c7fd440eaa8add3ec7
Gleb Vinokurov <gleb@investax.io>
2022-07-06 00:05:29 +0300

    Completed redesign for IndividualInfoView component

--- src/app/pages/identity/components/IndividualIdentityView/IndividualInfoView/IndividualInfoView.tsx
+++ src/app/pages/identity/components/IndividualIdentityView/IndividualInfoView/IndividualInfoView.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Grid, Box, Link, useTheme } from '@mui/material'
+import { Grid } from '@mui/material'
 import { Avatar } from 'components/Avatar'
 import { LabelledValue } from 'components/LabelledValue'
 import { formatDateToDDMonYYYY } from 'helpers/dates'
 import { privateClassNames } from 'helpers/classnames'
-import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
 import { ExtendedIdentityProfile } from 'app/pages/identity/types/forms'
 import { GENDERS_OPTS } from 'app/pages/identity/const'
+import { ReactComponent as AvatarPhoto } from 'assets/icons/new/avatar_identity.svg'
 
 export interface IndividualInfoViewProps {
   data: ExtendedIdentityProfile
@@ -14,9 +14,7 @@ export interface IndividualInfoViewProps {
 
 export const IndividualInfoView = (props: IndividualInfoViewProps) => {
   const { data } = props
-  const { isMobile } = useAppBreakpoints()
   const email = data.email ?? '-'
-  const theme = useTheme()
 
   const extractGender = (gender: string) => {
     if (gender.length > 0) {
@@ -26,100 +24,96 @@ export const IndividualInfoView = (props: IndividualInfoViewProps) => {
   }
 
   return (
-    <Grid container>
-      <Box>
-        <Box marginRight={2}>
-          <Avatar
-            documentId={data.photo}
-            ownerId={data.user._id}
-            size={128}
-            borderRadius={16}
-          />
-        </Box>
-        <Grid
-          container
-          rowSpacing={4}
-          spacing={2}
-          style={{ marginTop: isMobile ? 8 : 20 }}
-        >
-          <Grid item xs={12} sm={6} md={6}>
+    <Grid container direction={'column'} spacing={5}>
+      <Grid item>
+        <Avatar
+          documentId={data.photo}
+          ownerId={data.user._id}
+          size={120}
+          borderRadius={16}
+          fallback={<AvatarPhoto />}
+        />
+      </Grid>
+      <Grid
+        item
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: { sx: '1fr', sm: '1fr 1fr' }
+        }}
+        container
+      >
+        <Grid item container direction={'column'} spacing={5}>
+          <Grid item>
             <LabelledValue
-              labelWeight='thin'
-              labelFontSize={14}
-              valueColor={theme.palette.text.secondary}
+              isRedesigned
               value={data.firstName}
               label='First Name'
             />
           </Grid>
 
-          <Grid item xs={12} sm={6} md={6}>
+          <Grid item>
             <LabelledValue
-              labelWeight='thin'
-              labelFontSize={14}
-              valueColor={theme.palette.text.secondary}
-              value={data.lastName}
-              label='Last Name'
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={6}>
-            <LabelledValue
-              labelWeight='thin'
-              labelFontSize={14}
-              valueColor={theme.palette.text.secondary}
+              isRedesigned
               label='Date of Birth'
               className={privateClassNames()}
               value={formatDateToDDMonYYYY(data.dob)}
             />
           </Grid>
 
-          <Grid item xs={12} sm={6} md={6}>
+          <Grid item>
             <LabelledValue
-              labelWeight='thin'
-              labelFontSize={14}
-              valueColor={theme.palette.text.secondary}
-              label='Contact Number'
-              className={privateClassNames()}
-              value={
-                <Link href={`tel:${data.contactNumber}`}>
-                  {data.contactNumber}
-                </Link>
-              }
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={6}>
-            <LabelledValue
-              labelWeight='thin'
-              labelFontSize={14}
-              valueColor={theme.palette.text.secondary}
+              isRedesigned
               label='Email Address'
               className={privateClassNames()}
               value={email}
             />
           </Grid>
 
-          <Grid item xs={12} sm={6} md={6}>
+          <Grid item>
             <LabelledValue
-              labelWeight='thin'
-              labelFontSize={14}
-              valueColor={theme.palette.text.secondary}
+              isRedesigned
               value={data.nationality}
               label='Nationality'
             />
           </Grid>
+        </Grid>
 
-          <Grid item xs={12} sm={6} md={6}>
+        <Grid item container direction={'column'} spacing={5}>
+          <Grid item>
             <LabelledValue
-              labelWeight='thin'
-              labelFontSize={14}
-              valueColor={theme.palette.text.secondary}
+              isRedesigned
+              value={data.lastName}
+              label='Last Name'
+            />
+          </Grid>
+
+          <Grid item>
+            <LabelledValue
+              isRedesigned
               value={extractGender(data.gender)}
               label='Gender'
             />
           </Grid>
+
+          <Grid item>
+            <LabelledValue
+              isRedesigned
+              label='Contact Number'
+              className={privateClassNames()}
+              value={data.contactNumber}
+            />
+          </Grid>
+
+          <Grid item>
+            <LabelledValue
+              isRedesigned
+              label='NRIC/FIN'
+              className={privateClassNames()}
+              value={data.nric}
+            />
+          </Grid>
         </Grid>
-      </Box>
+      </Grid>
     </Grid>
   )
 }
