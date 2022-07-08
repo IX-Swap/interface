ea4d269e108388c2fa2c4e4411e415ccd52d5dcf
axelforce <oleksandr.kulagin@gmail.com>
2022-07-08 14:06:13 +0300

    change timeouts

--- e2e_tests/helpers/timeouts.ts
+++ e2e_tests/helpers/timeouts.ts
@@ -1,7 +1,7 @@
 export const timeouts = {
   globalTestsTimeout: 750000,
   testTimeout: 250000,
-  tinyTimeout: 2000,
+  tinyTimeout: 5000,
   shortTimeout: 15000,
   mediumTimeout: 35000,
   longTimeout: 75000
