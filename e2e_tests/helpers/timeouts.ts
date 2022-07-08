4817e82baf60a8bab88aefdc3c9dc4c2a1c16f24
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-07-08 16:40:12 +0400

    Updated timeouts

--- e2e_tests/helpers/timeouts.ts
+++ e2e_tests/helpers/timeouts.ts
@@ -1,6 +1,6 @@
 export const timeouts = {
-  globalTestsTimeout: 710000,
-  testTimeout: 210000,
+  globalTestsTimeout: 810000,
+  testTimeout: 230000,
   tinyTimeout: 2000,
   shortTimeout: 10000,
   mediumTimeout: 35000,
