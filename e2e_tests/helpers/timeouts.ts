7a39c052adba9ba643ced455725c8c688a6503ab
axelforce <oleksandr.kulagin@gmail.com>
2022-07-08 14:48:43 +0300

    Revert "change timeouts"
    This reverts commit ea4d269e

--- e2e_tests/helpers/timeouts.ts
+++ e2e_tests/helpers/timeouts.ts
@@ -1,7 +1,7 @@
 export const timeouts = {
   globalTestsTimeout: 750000,
   testTimeout: 250000,
-  tinyTimeout: 5000,
+  tinyTimeout: 6000,
   shortTimeout: 15000,
   mediumTimeout: 35000,
   longTimeout: 75000
