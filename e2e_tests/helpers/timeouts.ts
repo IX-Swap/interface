7b0c672d7c7a7d3062eefa1e539db42f3d7cd817
Vasil Shlapak <vasilsh@titanium-tech.net>
2022-07-11 17:51:52 +0400

    Added ability to run tests in parallel

--- e2e_tests/helpers/timeouts.ts
+++ e2e_tests/helpers/timeouts.ts
@@ -2,6 +2,7 @@ export const timeouts = {
   globalTestsTimeout: 1000000,
   testTimeout: 300000,
   tinyTimeout: 2000,
+  timeoutForSignWindow: 5000,
   shortTimeout: 25000,
   mediumTimeout: 35000,
   longTimeout: 80000
