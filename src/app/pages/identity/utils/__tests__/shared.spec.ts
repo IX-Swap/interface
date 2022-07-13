213f9b1b94b1e028f2ce1dcf2d76bec3657a48fa
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-07-13 16:23:49 +0800

    add tests for shared utility functions

--- src/app/pages/identity/utils/__tests__/shared.spec.ts
+++ src/app/pages/identity/utils/__tests__/shared.spec.ts
@@ -3,7 +3,10 @@ import { documents } from '__fixtures__/identity'
 import { Maybe } from 'types/util'
 import {
   adjustIdentityOccupation,
-  prepareDocumentsForUpload
+  checkSingPassDisabled,
+  getCorporateTitleText,
+  prepareDocumentsForUpload,
+  titleCase
 } from 'app/pages/identity/utils/shared'
 
 const documentsWithGuide: FormArray<Maybe<DataroomFile>> = [
@@ -35,5 +38,85 @@ describe('adjustIdentityOccupation', () => {
     expect(adjustIdentityOccupation('PAINTER/DESIGNER')).toEqual(
       'Painter/Designer'
     )
+  })
+})
+
+describe('getCorporateTitleText', () => {
+  it('returns correct title for view issuer', () => {
+    expect(getCorporateTitleText('issuer', 'view')).toEqual(
+      'View Corporate Issuer Identity'
+    )
+  })
+
+  it('returns correct title for edit issuer', () => {
+    expect(getCorporateTitleText('issuer', 'edit')).toEqual(
+      'Edit Corporate Issuer Identity'
+    )
+  })
+
+  it('returns correct title for create issuer', () => {
+    expect(getCorporateTitleText('issuer', 'create')).toEqual(
+      'Create Corporate Issuer Identity'
+    )
+  })
+
+  it('returns correct title for default issuer', () => {
+    expect(getCorporateTitleText('issuer')).toEqual('Corporate Issuer Identity')
+  })
+
+  it('returns correct title for view investor', () => {
+    expect(getCorporateTitleText('investor', 'view')).toEqual(
+      'View Corporate Investor Identity'
+    )
+  })
+
+  it('returns correct title for edit investor', () => {
+    expect(getCorporateTitleText('investor', 'edit')).toEqual(
+      'Edit Corporate Investor Identity'
+    )
+  })
+
+  it('returns correct title for create investor', () => {
+    expect(getCorporateTitleText('investor', 'create')).toEqual(
+      'Create Corporate Investor Identity'
+    )
+  })
+
+  it('returns correct title for default investor', () => {
+    expect(getCorporateTitleText('investor')).toEqual(
+      'Corporate Investor Identity'
+    )
+  })
+})
+
+describe('checkSingPassDisabled', () => {
+  it('returns correct boolean value when data is defined and from singpass', () => {
+    expect(checkSingPassDisabled(true, 'data-value')).toBeTruthy()
+  })
+
+  it('returns correct boolean value when data is undefined and not from singpass', () => {
+    expect(checkSingPassDisabled(false)).toBeFalsy()
+  })
+
+  it('returns correct boolean value when data is empty string and from singpass', () => {
+    expect(checkSingPassDisabled(true, '')).toBeFalsy()
+  })
+
+  it('returns correct boolean value when data is undefined and from singpass', () => {
+    expect(checkSingPassDisabled(true)).toBeFalsy()
+  })
+
+  it('returns correct boolean value when data is defined but not from singpass', () => {
+    expect(checkSingPassDisabled(false, 'data-value')).toBeFalsy()
+  })
+})
+
+describe('titleCase', () => {
+  it('returns empty string when there is no string value passed', () => {
+    expect(titleCase()).toEqual('')
+  })
+
+  it('returns correct capitalized string', () => {
+    expect(titleCase('data')).toEqual('Data')
   })
 })
