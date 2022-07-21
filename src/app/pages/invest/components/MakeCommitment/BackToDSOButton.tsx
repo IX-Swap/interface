0e2fbe2873c3b90feaf489951059c7248a39ffe9
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-07-21 15:10:49 +0800

    add back button to make commitment page header

---
+++ src/app/pages/invest/components/MakeCommitment/BackToDSOButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, ButtonProps, Box } from '@mui/material'
+import { useHistory } from 'react-router-dom'
+import { Icon } from 'ui/Icons/Icon'
+import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
+
+export interface BackToDSOButtonProps extends ButtonProps {}
+
+export const BackToDSOButton = (props: BackToDSOButtonProps) => {
+  const history = useHistory()
+  const { isMobile } = useAppBreakpoints()
+
+  return (
+    <Box height='100%' display='flex' alignItems='center'>
+      <Button
+        {...props}
+        endIcon={isMobile ? undefined : <Icon name='arrow-right' />}
+        startIcon={isMobile ? <Icon name='arrow-left' /> : undefined}
+        onClick={history.goBack}
+        sx={{
+          pl: {
+            xs: 0,
+            md: 4
+          }
+        }}
+      >
+        Back to DSO page
+      </Button>
+    </Box>
+  )
+}
--- src/app/pages/invest/pages/MakeCommitmentPage.tsx
+++ src/app/pages/invest/pages/MakeCommitmentPage.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid } from '@mui/material'
 import { PageHeader } from 'app/components/PageHeader/PageHeader'
+import { BackToDSOButton } from 'app/pages/invest/components/MakeCommitment/BackToDSOButton'
 import { MakeCommitment } from 'app/pages/invest/components/MakeCommitment/MakeCommitment'
 import React from 'react'
 
@@ -8,7 +9,10 @@ export const MakeCommitmentPage = () => {
     <Box width='100%'>
       <Grid container spacing={0}>
         <Grid item xs={12}>
-          <PageHeader title='Make Commitment' />
+          <PageHeader
+            title='Make Commitment'
+            endComponent={<BackToDSOButton />}
+          />
         </Grid>
         <Grid item xs={12}>
           <MakeCommitment />
