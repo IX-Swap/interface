f95caf1c1c5e2d4116a2b1a27bb35d4d5831a6d8
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-07-21 11:04:24 +0800

    add no balance component

--- src/app/pages/invest/components/MakeCommitment/AssetBalance.tsx
+++ src/app/pages/invest/components/MakeCommitment/AssetBalance.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useBalancesByAssetId } from 'hooks/balance/useBalancesByAssetId'
 import { formatMoney } from 'helpers/numbers'
 import { OverviewValue } from 'app/pages/invest/components/MakeCommitment/OverviewValue'
+import { Grid } from '@mui/material'
+import { NoBalance } from 'app/pages/invest/components/MakeCommitment/NoBalance'
 
 export interface AssetBalanceProps {
   assetId: string
@@ -17,12 +19,21 @@ export const AssetBalance = (props: AssetBalanceProps) => {
   }
 
   return (
-    <OverviewValue
-      label='Available Balance'
-      value={formatMoney(
-        data.map[assetId]?.available ?? 0,
-        data.map[assetId]?.symbol ?? symbol
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <OverviewValue
+          label='Available Balance'
+          value={formatMoney(
+            data.map[assetId]?.available ?? 0,
+            data.map[assetId]?.symbol ?? symbol
+          )}
+        />
+      </Grid>
+      {(data.map[assetId]?.available ?? 0) <= 0 && (
+        <Grid item xs={12}>
+          <NoBalance />
+        </Grid>
       )}
-    />
+    </Grid>
   )
 }
