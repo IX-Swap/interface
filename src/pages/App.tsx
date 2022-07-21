be8aa8f8bc13baed86bc36732238f350d07ca764
Leahu Ion <ion.leahu@titanium-tech.net>
2022-07-21 12:15:00 +0300

    fix x scroll for whole page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -41,7 +41,7 @@ const AppWrapper = styled.div`
   flex-flow: column;
   align-items: flex-start;
   position: relative;
-  overflow-x: auto;
+  overflow-x: hidden;
 `
 
 const BodyWrapper = styled.div`
