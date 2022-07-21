94ae84f250e368aeb1cb2c2cca0fe6695587d5d7
Victoria Florea <vica@investax.io>
2022-07-21 21:18:02 +0300

    Remove unused setup component. Add a test for the new setup component

--- src/app/pages/accounts/pages/banks/pages/WithdrawCash/WithdrawView.tsx
+++ src/app/pages/accounts/pages/banks/pages/WithdrawCash/WithdrawView.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import { Grid } from '@mui/material'
 import { WithdrawForm } from 'app/pages/accounts/pages/banks/pages/WithdrawCash/WithdrawForm'
-import { Setup } from 'app/pages/accounts/pages/banks/pages/WithdrawCash/Setup'
 import { RecentWithdrawals } from 'app/pages/accounts/pages/banks/pages/WithdrawCash/RecentWithdrawals'
 
 export const WithdrawView: React.FC = () => {
   return (
     <WithdrawForm>
       <Grid container direction='column' spacing={5}>
-        <Grid item>
-          <Setup />
-        </Grid>
         <Grid item>
           <RecentWithdrawals />
         </Grid>
