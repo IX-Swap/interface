dff621d70e2887fb5c90e061b31bb8f5fc5d6290
Victoria Florea <vica@investax.io>
2022-07-25 18:06:58 +0300

    Customize bank list header for mobile

--- src/app/pages/accounts/pages/banks/pages/BanksList/BanksList.tsx
+++ src/app/pages/accounts/pages/banks/pages/BanksList/BanksList.tsx
@@ -1,18 +1,46 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import { Header } from 'app/pages/accounts/pages/banks/pages/BanksList/Header'
 import { Table } from 'app/pages/accounts/pages/banks/pages/BanksList/Table'
 import { PageHeader } from 'app/components/PageHeader/PageHeader'
 import { RootContainer } from 'ui/RootContainer'
+import { useAppBreakpoints } from 'hooks/useAppBreakpoints'
+import { useStyles } from 'app/pages/accounts/pages/banks/pages/BanksList/BanksList.styles'
 
 export const BanksList: React.FC = () => {
+  const classes = useStyles()
+  const { isTablet } = useAppBreakpoints()
   return (
-    <Grid container direction='column' spacing={2} style={{ display: 'table' }}>
+    <Grid
+      container
+      direction='column'
+      spacing={2}
+      className={classes.pageParent}
+    >
       <Grid item>
-        <PageHeader title='Bank Accounts' endComponent={<Header />} />
+        {!isTablet && (
+          <PageHeader title='Bank Accounts' endComponent={<Header />} />
+        )}
+        {isTablet && (
+          <Grid item container flexDirection={'row'} mt={3}>
+            <Grid item xs={12}>
+              <PageHeader
+                title='Bank accounts'
+                showBreadcrumbs={false}
+                styled={false}
+                variant='h3'
+              />
+            </Grid>
+            <Grid item xs={12} px={2} mt={3}>
+              <Box className={classes.buttonWrapper}>
+                <Header />
+              </Box>
+            </Grid>
+          </Grid>
+        )}
       </Grid>
       <RootContainer>
-        <Grid item ml='10px'>
+        <Grid item>
           <Table />
         </Grid>
       </RootContainer>
