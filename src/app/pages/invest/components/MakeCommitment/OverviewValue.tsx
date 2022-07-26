88de720d9d618c45787ebf00b00132f343cb89d9
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-07-26 15:30:37 +0800

    IPD1-34 Page Container and Header, DSO Overview redesigned

---
+++ src/app/pages/invest/components/MakeCommitment/OverviewValue.tsx
@@ -0,0 +1,22 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+
+export interface OverviewValueProps {
+  label: string
+  value?: string | number
+}
+
+export const OverviewValue = ({ label, value }: OverviewValueProps) => {
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <Typography variant='subtitle1' color='text.secondary'>
+          {label}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant='subtitle1'>{value}</Typography>
+      </Grid>
+    </Grid>
+  )
+}
