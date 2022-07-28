ae164633eba9d15942d0baf8dd2628a516144672
Victoria Florea <vica@investax.io>
2022-07-28 08:48:52 +0300

    Remove view bank page

--- src/app/pages/accounts/pages/banks/router/BanksRouter.tsx
+++ src/app/pages/accounts/pages/banks/router/BanksRouter.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import { BanksList } from 'app/pages/accounts/pages/banks/pages/BanksList/BanksList'
-import ViewBank from 'app/pages/accounts/pages/banks/pages/ViewBank/ViewBank'
-import { Switch } from 'react-router-dom'
-import { EditBank } from 'app/pages/accounts/pages/banks/pages/EditBank/EditBank'
 import { CreateBank } from 'app/pages/accounts/pages/banks/pages/CreateBank/CreateBank'
+import { EditBank } from 'app/pages/accounts/pages/banks/pages/EditBank/EditBank'
 import { BanksRoute } from 'app/pages/accounts/pages/banks/router/config'
-import { AppRoute } from 'components/AppRoute'
 import { AccountsRoute } from 'app/pages/accounts/router/config'
+import { AppRoute } from 'components/AppRoute'
+import React from 'react'
+import { Switch } from 'react-router-dom'
 
 export const BanksRouter = () => {
   return (
@@ -14,14 +13,9 @@ export const BanksRouter = () => {
       <AppRoute path={AccountsRoute.banks} exact>
         <BanksList />
       </AppRoute>
-      <AppRoute breadcrumb='View Bank' exact path={BanksRoute.view}>
-        <ViewBank />
-      </AppRoute>
-
       <AppRoute breadcrumb='Edit Bank Details' exact path={BanksRoute.edit}>
         <EditBank />
       </AppRoute>
-
       <AppRoute breadcrumb='Add Bank Account' exact path={BanksRoute.create}>
         <CreateBank />
       </AppRoute>
