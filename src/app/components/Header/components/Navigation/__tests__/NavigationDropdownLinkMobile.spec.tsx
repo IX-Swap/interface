a5cc08de517b1874458c1ce1fbf91ba46cbbfaac
Victoria Florea <vica@investax.io>
2022-08-01 16:29:05 +0300

    Add missing tests

--- src/app/components/Header/components/Navigation/__tests__/NavigationDropdownLinkMobile.spec.tsx
+++ src/app/components/Header/components/Navigation/__tests__/NavigationDropdownLinkMobile.spec.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { render } from 'test-utils'
+import { ExternalNavigationLink } from 'app/components/Header/components/Navigation/TopbarLinkContainer/ExternalNavigationLink'
+
 import {
   NavigationLinkDropdownMobileProps,
   NavigationDropdownLinkMobile
 } from 'app/components/Header/components/Navigation/NavigationDropdownLinkMobile'
 
+jest.mock(
+  'app/components/Header/components/Navigation/TopbarLinkContainer/ExternalNavigationLink',
+  () => ({
+    ExternalNavigationLink: jest.fn(() => null)
+  })
+)
+
 const props: NavigationLinkDropdownMobileProps = {
   link: '/test',
   label: 'label',
   dropdownLinksItems: []
+}
+const externalProps = {
+  ...props,
+  dropdownLinksItems: [
+    { label: 't', path: '/test', external: true, onClick: jest.fn() }
+  ]
 }
 
 describe('NavigationDropdownLinkMobile', () => {
@@ -20,5 +35,16 @@ describe('NavigationDropdownLinkMobile', () => {
     const { container } = render(<NavigationDropdownLinkMobile {...props} />)
 
     expect(container).toMatchSnapshot()
+  })
+  it('should render ExternalNavigationLink', () => {
+    render(<NavigationDropdownLinkMobile {...externalProps} />)
+    expect(ExternalNavigationLink).toHaveBeenCalledWith(
+      expect.objectContaining({
+        link: externalProps.dropdownLinksItems[0].path,
+        label: externalProps.dropdownLinksItems[0].label
+      }),
+
+      {}
+    )
   })
 })
