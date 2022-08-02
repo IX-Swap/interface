e9ae969b7ea556cf7f16562a7887c0d7ed81bd9d
Victoria Florea <vica@investax.io>
2022-08-02 12:29:22 +0300

    Add tests for CashStatus, renderActionButton

--- src/app/pages/accounts/pages/cash/components/CashStatus.tsx
+++ src/app/pages/accounts/pages/cash/components/CashStatus.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import { ConvertedAssetBalance } from 'types/balance'
-import { ExpandButton } from 'ui/CompactTable/ExpandButton'
 import { Status } from 'ui/Status/Status'
 
 export interface CashStatusProps {
   item: ConvertedAssetBalance
 }
-export const renderActionButton = (item: ConvertedAssetBalance) => {
-  if (item.status !== 'Approved') {
-    return <CashStatus item={item} />
-  }
-  return <ExpandButton item={item} />
-}
+
 export const CashStatus = ({ item }: CashStatusProps) => {
   return (
     <Status
