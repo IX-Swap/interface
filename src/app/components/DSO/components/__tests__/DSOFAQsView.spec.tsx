7f9501d7422149f41bf72d4d6b7bdbc30f2470c5
Sumen del Rosario <sumen.delrosario@gmail.com>
2022-08-02 11:00:33 +0800

    fix failing tests

--- src/app/components/DSO/components/__tests__/DSOFAQsView.spec.tsx
+++ src/app/components/DSO/components/__tests__/DSOFAQsView.spec.tsx
@@ -2,14 +2,7 @@ import React from 'react'
 import { render } from 'test-utils'
 import { faqItem } from '__fixtures__/issuance'
 import { DSOFAQsView } from 'app/components/DSO/components/DSOFAQsView'
-import { FormSectionHeader } from 'app/components/DSO/components/FormSectionHeader'
-// import * as Typography from '@mui/material'
-
-jest.mock('@mui/material/Typography', () => jest.fn(() => null))
-
-jest.mock('app/components/DSO/components/FormSectionHeader', () => ({
-  FormSectionHeader: jest.fn(() => null)
-}))
+import { dso } from '__fixtures__/authorizer'
 
 describe('DSOFAQsView', () => {
   afterEach(async () => {
@@ -18,60 +11,9 @@ describe('DSOFAQsView', () => {
 
   const faq = { faqs: [faqItem] } as any
 
-  it('renders title', () => {
-    render(<DSOFAQsView dso={faq} />)
+  it('matches snapshot', () => {
+    const { container } = render(<DSOFAQsView dso={dso} />)
 
-    expect(FormSectionHeader).toHaveBeenCalledTimes(0)
+    expect(container).toMatchSnapshot()
   })
-
-  it('renders title with correct props when isTitleVisible is true', () => {
-    render(<DSOFAQsView dso={faq} />)
-
-    expect(FormSectionHeader).toHaveBeenCalledTimes(1)
-    expect(FormSectionHeader).toHaveBeenCalledWith(
-      expect.objectContaining({ title: 'FAQs' }),
-      {}
-    )
-  })
-
-  // it('renders title with correct props when isTitleVisible and iaNewThemeOn is true', () => {
-  //   render(<DSOFAQsView dso={faq} isNewThemeOn isTitleVisible />)
-
-  //   expect(FormSectionHeader).toHaveBeenCalledTimes(0)
-  //   expect(Typography).toHaveBeenNthCalledWith(
-  //     1,
-  //     expect.objectContaining({
-  //       variant: 'h4',
-  //       color: 'primary',
-  //       style: { fontWeight: 700 },
-  //       children: 'Frequently Asked Questions'
-  //     }),
-  //     {}
-  //   )
-  // })
-
-  // it('renders content with correct props', () => {
-  //   render(<DSOFAQsView dso={faq} />)
-
-  //   expect(Typography).toHaveBeenCalledTimes(2)
-
-  //   expect(Typography).toHaveBeenNthCalledWith(
-  //     1,
-  //     expect.objectContaining({
-  //       children: [1, '. ', faqItem.question],
-  //       variant: 'subtitle1'
-  //     }),
-  //     {}
-  //   )
-
-  //   expect(Typography).toHaveBeenNthCalledWith(
-  //     2,
-  //     expect.objectContaining({
-  //       children: faqItem.answer,
-  //       variant: 'body1',
-  //       style: { paddingLeft: 16, paddingTop: 16 }
-  //     }),
-  //     {}
-  //   )
-  // })
 })
