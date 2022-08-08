b033b65b1f86c53ab23901b034aa19d5c695cf18
axelforce <oleksandr.kulagin@gmail.com>
2022-08-08 17:15:38 +0300

    fixes according to code review

--- e2e_tests/helpers/web3/metamaskData.ts
+++ e2e_tests/helpers/web3/metamaskData.ts
@@ -1,4 +1,4 @@
 export const metamaskWallet = {
-  ethAddress: '0xaCDaA0810c5bb1A5F2ef9370b590C229464a1C65',
+  ethAddress: process.env.METAMASK_ETH_ADDRESS,
   privateKey: process.env.METAMASK_PRIVATE_KEY
 };
