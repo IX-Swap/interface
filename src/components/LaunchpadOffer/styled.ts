3ccb1a0d0e2462615210b62443a58517b084aabd
Nurkassym <nurkassym.bakhramov@titanium-tech.net>
2022-11-28 17:30:51 +0600

    [IXSwap.Interface] - Added offer page - Added redirect from offer card to offer page

---
+++ src/components/LaunchpadOffer/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Separator = styled.hr`
+  border: 1px solid ${props => props.theme.launchpad.colors.border.default};
+  opacity: 0.8;
+
+  margin: 0;
+`
