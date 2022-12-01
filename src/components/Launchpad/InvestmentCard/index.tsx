54883bb960bc64c07e4ee35619ce159187ba0d83
Nurkassym <nurkassym.bakhramov@titanium-tech.net>
2022-12-01 12:13:47 +0600

    [IXSwap.Interface] - Fixed invest button on offer page;

--- src/components/Launchpad/InvestmentCard/index.tsx
+++ src/components/Launchpad/InvestmentCard/index.tsx
@@ -38,7 +38,7 @@ export const InvestmentCard: React.FC<Props> = ({ offer }) => {
   const toggleShowDetails = React.useCallback(() => setShowDetails(state => !state), [])
   const toggleKYCModal = React.useCallback(() => setShowKYCModal(state => !state), [])
 
-  const isClosed = React.useMemo(() => offer.status === OfferStatus.closed, [offer])
+  const isClosed = React.useMemo(() => [OfferStatus.closed, OfferStatus.claim].includes(offer.status), [offer])
 
   const currentTimeframe = React.useMemo(() => 
     offer.timeframes.find(frame => isWithinTimeframe(frame)) 
