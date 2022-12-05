168cce86b50d8fb7e9b14db6f612de98e7473430
kapil Dave <kapil@investax.io>
2022-12-05 23:00:52 +0530

    filter issue fixed

--- src/app/pages/invest/components/DSOTable/PriceFilter.tsx
+++ src/app/pages/invest/components/DSOTable/PriceFilter.tsx
@@ -3,11 +3,16 @@ import React from 'react'
 import { SelectChangeEvent } from '@mui/material'
 import { PriceSelect } from 'app/pages/invest/components/DSOTable/PriceSelect'
 
-export const PriceFilter = () => {
+interface FiltersFavProps {
+  setPage?: (page: number)=>void
+}
+
+export const PriceFilter = (props:FiltersFavProps) => {
   const { getFilterValue, updateFilters } = useQueryFilter()
   const value = getFilterValue('sortOrder')
 
   const handleChange = (event: SelectChangeEvent<any>) => {
+    props.setPage && props.setPage(0);
     updateFilters({
       sortField: 'price',
       sortOrder: event.target.value
