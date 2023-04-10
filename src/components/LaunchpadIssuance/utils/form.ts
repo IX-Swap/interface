e8ef1916fbaa28cc4e595ee3fb608f6071f6a518
harryng22 <harry@investax.io>
2023-04-10 17:03:07 +0700

    Check whether isDraft or not

--- src/components/LaunchpadIssuance/utils/form.ts
+++ src/components/LaunchpadIssuance/utils/form.ts
@@ -1,8 +1,9 @@
 export const isSubmitDisabled = (errors: any, touched: any) => {
-  return getError(errors, touched)
+  return getError(errors, touched, false)
 }
 
-const getIsDisabled = (message?: string) => {
+const getIsDisabled = (message?: string, isDraft?: boolean) => {
+  if (isDraft) return true;
   if (message) {
     const includesString = (keys: string[]) => keys.some((key: string) => message.includes(key))
     const invalidKeys = ['valid', 'Valid']
@@ -12,7 +13,7 @@ const getIsDisabled = (message?: string) => {
   return false
 }
 
-const getError = (errors: any, touched: any) => {
+const getError = (errors: any, touched: any, isDraft: boolean) => {
   const hasError = Object.entries(touched).some(([fieldName, value]: any) => {
     if (Array.isArray(value)) {
       return value.some((arrayItem, index) => {
@@ -21,22 +22,22 @@ const getError = (errors: any, touched: any) => {
         }
         return Object.keys(arrayItem).some((arrayItemKey) => {
           const error = errors[fieldName] && errors[fieldName][index] && errors[fieldName][index][arrayItemKey]
-          return getIsDisabled(error)
+          return getIsDisabled(error, isDraft)
         })
       })
     }
     if (typeof value === 'object') {
       return Object.keys(value).some((objectItemKey) => {
         const error = errors[fieldName] && errors[fieldName][objectItemKey]
-        return getIsDisabled(error)
+        return getIsDisabled(error, isDraft)
       })
     }
 
-    return value ? getIsDisabled(errors[fieldName]) : false
+    return value ? getIsDisabled(errors[fieldName], isDraft) : false
   })
   return hasError
 }
 
 export const isDraftDisabled = (errors: any, touched: any) => {
-  return getError(errors, touched)
+  return getError(errors, touched, true)
 }
