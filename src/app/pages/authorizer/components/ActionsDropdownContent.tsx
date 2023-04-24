a73b9507695e7bbf56862aa3d7fef9eac6b19f49
Ray Caballero <ray@investax.io>
2023-04-24 17:19:06 +0800

    Fix icons on approval pop-up

--- src/app/pages/authorizer/components/ActionsDropdownContent.tsx
+++ src/app/pages/authorizer/components/ActionsDropdownContent.tsx
@@ -2,16 +2,10 @@ import React from 'react'
 import { DropdownContentProps } from 'app/components/Dropdown/Dropdown'
 import { List } from '@mui/material'
 import { Action } from 'app/pages/authorizer/components/Action'
-import { useHistory } from 'react-router-dom'
 
-import {
-  ThumbUpOffAlt as ApproveIconKYC,
-  ThumbDownOffAlt as RejectIconKYC,
-  RemoveRedEyeOutlined as LaunchIconKYC,
-  AssignmentTurnedIn as ApproveIcon,
-  Gavel as RejectIcon,
-  Launch as LaunchIcon
-} from '@mui/icons-material'
+import { ReactComponent as ApproveIcon } from 'assets/icons/actions/approve.svg'
+import { ReactComponent as RejectIcon } from 'assets/icons/actions/reject.svg'
+import { ReactComponent as LaunchIcon } from 'assets/icons/actions/view.svg'
 
 export interface ActionsDropdownContentProps extends DropdownContentProps {
   approve: () => void
@@ -21,36 +15,11 @@ export interface ActionsDropdownContentProps extends DropdownContentProps {
 
 export const ActionsDropdownContent = (props: ActionsDropdownContentProps) => {
   const { approve, reject, view, injectedProps } = props
-  const { location } = useHistory()
   return (
-    <>
-      {location?.pathname?.includes('individuals') ? (
-        <List data-testid='dropdown' onClick={injectedProps.close}>
-          <Action
-            label='Approve'
-            color='#6ABC10'
-            icon={ApproveIconKYC}
-            onClick={approve}
-          />
-          <Action
-            label='Reject'
-            color='#FF8080'
-            icon={RejectIconKYC}
-            onClick={reject}
-          />
-          <Action
-            label='View Application'
-            icon={LaunchIconKYC}
-            onClick={view}
-          />
-        </List>
-      ) : (
-        <List data-testid='dropdown' onClick={injectedProps.close}>
-          <Action label='Approve' icon={ApproveIcon} onClick={approve} />
-          <Action label='Reject' icon={RejectIcon} onClick={reject} />
-          <Action label='View Application' icon={LaunchIcon} onClick={view} />
-        </List>
-      )}
-    </>
+    <List data-testid='dropdown' onClick={injectedProps.close}>
+      <Action label='Approve' icon={ApproveIcon} onClick={approve} />
+      <Action label='Reject' icon={RejectIcon} onClick={reject} />
+      <Action label='View Application' icon={LaunchIcon} onClick={view} />
+    </List>
   )
 }
