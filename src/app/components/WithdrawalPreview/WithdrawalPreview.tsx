7185ad9745edd352b93e074941289e1b1804a824
Ray Caballero <ray@investax.io>
2023-05-03 12:32:25 +0800

    Updated UI for Authorization > Cash Withdrawals and Security Token Withdrawals

--- src/app/components/WithdrawalPreview/WithdrawalPreview.tsx
+++ src/app/components/WithdrawalPreview/WithdrawalPreview.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Grid } from '@mui/material'
 import { formatMoney } from 'helpers/numbers'
 import { CashWithdrawal } from 'types/cashWithdrawal'
-import { LabelledValue } from 'components/LabelledValue'
 import { convertAddressToString } from 'app/pages/authorizer/components/utils'
 import { useSetPageTitle } from 'app/hooks/useSetPageTitle'
+import { FieldGrid } from 'ui/FieldGrid/FieldGrid'
 
 export interface WithdrawalViewProps {
   data: CashWithdrawal
@@ -15,66 +15,49 @@ export const WithdrawalPreview = (props: WithdrawalViewProps) => {
 
   useSetPageTitle(data.bank.bankName)
 
+  const items = [
+    {
+      label: 'Bank Name',
+      value: data.bank.bankName
+    },
+    {
+      label: 'Account Holder Name',
+      value: data.bank.accountHolderName
+    },
+    {
+      label: 'Virtual Account',
+      value: data.virtualAccount?.accountNumber
+    },
+    {
+      label: 'Swift Code',
+      value: data.bank.swiftCode
+    },
+    {
+      label: 'Currency',
+      value: data.asset.symbol
+    },
+    {
+      label: 'Bank Address',
+      value: convertAddressToString(data.bank.address)
+    },
+    {
+      label: 'Bank Account Number',
+      value: data.bank.bankAccountNumber
+    },
+    {
+      label: 'Withdrawal Method',
+      value: data.paymentMethodName
+    },
+    {
+      label: 'Withdrawal Amount',
+      value: formatMoney(data.amount, data.asset.symbol)
+    }
+  ]
+
   return (
-    <Grid container justifyContent='center' direction='column' spacing={4}>
-      <Grid item container spacing={4}>
-        <Grid item xs={4}>
-          <LabelledValue label='Bank Name' value={data.bank.bankName} />
-        </Grid>
-
-        <Grid item xs={4}>
-          <LabelledValue
-            label='Account Holder Name'
-            value={data.bank.accountHolderName}
-          />
-        </Grid>
-      </Grid>
-
-      <Grid item container spacing={4}>
-        <Grid item xs={4}>
-          <LabelledValue
-            label='Virtual Account'
-            value={data.virtualAccount?.accountNumber}
-          />
-        </Grid>
-
-        <Grid item xs={4}>
-          <LabelledValue label='Swift Code' value={data.bank.swiftCode} />
-        </Grid>
-
-        <Grid item xs={4}>
-          <LabelledValue label='Currency' value={data.asset.symbol} />
-        </Grid>
-      </Grid>
-
-      <Grid item container spacing={4}>
-        <Grid item xs={4}>
-          <LabelledValue
-            label='Bank Address'
-            value={convertAddressToString(data.bank.address)}
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <LabelledValue
-            label='Bank Account Number'
-            value={data.bank.bankAccountNumber}
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <LabelledValue
-            label='Withdrawal Method'
-            value={data.paymentMethodName}
-          />
-        </Grid>
-      </Grid>
-
-      <Grid item container spacing={4}>
-        <Grid item xs={4}>
-          <LabelledValue
-            label='Withdrawal Amount'
-            value={formatMoney(data.amount, data.asset.symbol)}
-          />
-        </Grid>
+    <Grid container pt={3} pl={3}>
+      <Grid item xs={12}>
+        <FieldGrid items={items} />
       </Grid>
     </Grid>
   )
--- src/app/pages/authorizer/pages/dsWithdrawals/DSWithdrawals.tsx
+++ src/app/pages/authorizer/pages/dsWithdrawals/DSWithdrawals.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { columns } from 'app/pages/authorizer/pages/dsWithdrawals/columns'
-import { AuthorizerList } from 'app/pages/authorizer/components/AuthorizerList'
+import { AuthorizerList } from 'app/pages/authorizer/components/upgrade/AuthorizerList/AuthorizerList'
 import { authorizerQueryKeys } from 'config/queryKeys'
 
 export const DSWithdrawals: React.FC = () => (
   <AuthorizerList
-    title='Authorize Security Token Withdrawal'
+    title='Authorize Security Token Withdrawals'
     uri='/accounts/security/withdrawals'
     name={authorizerQueryKeys.getSecurityWithdrawals}
     columns={columns}
