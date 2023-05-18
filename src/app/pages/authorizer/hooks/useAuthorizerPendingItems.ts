aa3c0106daa358c7f2d88f5239cd2a25e0b0242c
Ray Caballero <ray@investax.io>
2023-05-18 10:58:56 +0800

    Fix bug in Authorization pending items

--- src/app/pages/authorizer/hooks/useAuthorizerPendingItems.ts
+++ src/app/pages/authorizer/hooks/useAuthorizerPendingItems.ts
@@ -6,10 +6,14 @@ const categoryMap = {
   [AuthorizerCategory.CashWithdrawals]: '/accounts/cash/withdrawals',
   [AuthorizerCategory.Commitments]: '/issuance/commitments/list',
   [AuthorizerCategory.Corporates]: '/identity/corporates/list',
+  [AuthorizerCategory.CorporatesAccreditation]:
+    '/identity/accreditation/corporate/list',
   [AuthorizerCategory.DealClosure]: '/issuance/closure/list',
   [AuthorizerCategory.SecurityTokenWithdrawals]:
     '/accounts/security/withdrawals',
   [AuthorizerCategory.Individuals]: '/identity/individuals/list',
+  [AuthorizerCategory.IndividualsAccreditation]:
+    '/identity/accreditation/individual/list',
   [AuthorizerCategory.IssuanceDetails]: '/identity/issuance-detail/list',
   [AuthorizerCategory.Listings]: '/exchange/listing/list',
   [AuthorizerCategory.Offerings]: '/issuance/dso/list',
@@ -20,6 +24,7 @@ const categoryMap = {
 }
 
 export const useAuthorizerPendingItems = (category: AuthorizerCategory) => {
+  console.log(category)
   const isCommitment = category === AuthorizerCategory.Commitments
   const data = useTableWithPagination<any>({
     queryKey: `pending-${category}`,
