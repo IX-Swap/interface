11d65d70984a01b5c7b6c4e75f437c1f2861ab17
kapil Dave <kapil@investax.io>
2023-06-08 00:16:44 +0530

    roles bug fixed

--- src/components/form/InvestorRoleSelect.tsx
+++ src/components/form/InvestorRoleSelect.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ListItemText, SelectProps } from '@mui/material'
-import { INVESTORROLES } from 'config/roles'
+import { CORPORATE_INVESTOR, INDIVISUAL_INVESTOR } from 'config/roles'
 // import { UICheckbox } from 'components/UICheckbox/UICheckbox'
 import { Select } from 'ui/Select/Select'
 import { SelectItem } from 'ui/Select/SelectItem/SelectItem'
@@ -9,6 +9,7 @@ import { UIRadio } from 'components/UIRadio/UIRadio'
 export interface RoleSelectProps extends SelectProps {
   value: string[]
   roles: string[]
+  investorIdentity: any
 }
 
 export const InvestorRoleSelect = (props: RoleSelectProps) => {
@@ -24,30 +25,53 @@ export const InvestorRoleSelect = (props: RoleSelectProps) => {
       return 'Expert'
     }
     if (name === 'institutional') {
-      return 'Enstitutional'
+      return 'Institutional'
     }
     return name
   }
 
   return (
-    <Select
-      {...props}
-      multiple
-      MenuProps={{ BackdropProps: { 'data-testid': 'backdrop' } as any }}
-      renderValue={selected => (selected as string[]).join(', ')}
-    >
-      {INVESTORROLES.map(name => (
-        <SelectItem
-          key={name}
-          value={name}
-          disabled={name === 'user'}
-          sx={{ padding: '5px !important' }}
+    <>
+      {props?.investorIdentity === 'INDIVIDUAL' ? (
+        <Select
+          {...props}
+          multiple
+          MenuProps={{ BackdropProps: { 'data-testid': 'backdrop' } as any }}
+          renderValue={selected => (selected as string[]).join(', ')}
         >
-          <UIRadio checked={props.value.includes(name)} />
-          <ListItemText primary={getName(name)} />
-        </SelectItem>
-      ))}
-    </Select>
+          {INDIVISUAL_INVESTOR.map(name => (
+            <SelectItem
+              key={name}
+              value={name}
+              disabled={name === 'user'}
+              sx={{ padding: '5px !important' }}
+            >
+              <UIRadio checked={props.value.includes(name)} />
+              <ListItemText primary={getName(name)} />
+            </SelectItem>
+          ))}
+        </Select>
+      ) : (
+        <Select
+          {...props}
+          multiple
+          MenuProps={{ BackdropProps: { 'data-testid': 'backdrop' } as any }}
+          renderValue={selected => (selected as string[]).join(', ')}
+        >
+          {CORPORATE_INVESTOR.map(name => (
+            <SelectItem
+              key={name}
+              value={name}
+              disabled={name === 'user'}
+              sx={{ padding: '5px !important' }}
+            >
+              <UIRadio checked={props.value.includes(name)} />
+              <ListItemText primary={getName(name)} />
+            </SelectItem>
+          ))}
+        </Select>
+      )}
+    </>
   )
 }
 InvestorRoleSelect.displayName = 'Select_RoleSelect'
