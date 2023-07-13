29bc571e8485eef70f29acbfffed3699c2a80860
Ray Caballero <ray@investax.io>
2023-07-13 09:36:02 +0800

    Updated security token page

--- src/app/pages/accounts/pages/digitalSecurities/DSList/SelfCustodyList.tsx
+++ src/app/pages/accounts/pages/digitalSecurities/DSList/SelfCustodyList.tsx
@@ -1,26 +1,30 @@
+import React from 'react'
+import { Box } from '@mui/material'
 import { columns } from 'app/pages/accounts/pages/digitalSecurities/DSList/columns'
-import { TableView } from 'components/TableWithPagination/TableView'
+// import { TableView } from 'components/TableWithPagination/TableView'
+import { TableView } from 'ui/UIKit/TablesKit/components/TableView/TableView'
 import { accountsURL } from 'config/apiURL'
 import { digitalSecuritiesQueryKeys } from 'config/queryKeys'
 import { getIdFromObj } from 'helpers/strings'
 import { useAuth } from 'hooks/auth/useAuth'
-import React from 'react'
 
 export const SelfCustodyList = () => {
   const { user } = useAuth()
   const userId = getIdFromObj(user)
 
   return (
-    <TableView
-      uri={accountsURL.balance.getAll(userId)}
-      name={digitalSecuritiesQueryKeys.selfCustody(userId)}
-      columns={columns}
-      filter={{ type: 'Security' }}
-      paperProps={{
-        style: {
-          borderTop: 'none'
-        }
-      }}
-    />
+    <Box mt={2}>
+      <TableView
+        uri={accountsURL.balance.getAll(userId)}
+        name={digitalSecuritiesQueryKeys.selfCustody(userId)}
+        columns={columns}
+        filter={{ type: 'Security' }}
+        paperProps={{
+          style: {
+            borderTop: 'none'
+          }
+        }}
+      />
+    </Box>
   )
 }
