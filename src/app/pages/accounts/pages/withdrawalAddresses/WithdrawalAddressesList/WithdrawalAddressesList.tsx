c7e232cc8ab29d139b1843414296788d322ea7d4
Ray Caballero <ray@investax.io>
2023-07-18 14:14:34 +0800

    wallet address section in dashboard

--- src/app/pages/accounts/pages/withdrawalAddresses/WithdrawalAddressesList/WithdrawalAddressesList.tsx
+++ src/app/pages/accounts/pages/withdrawalAddresses/WithdrawalAddressesList/WithdrawalAddressesList.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Grid, Button } from '@mui/material'
-import { VSpacer } from 'components/VSpacer'
 import { AppRouterLinkComponent } from 'components/AppRouterLink'
 import { WithdrawalAddressesTable } from 'app/pages/accounts/pages/withdrawalAddresses/WithdrawalAddressesList/WithdrawalAddressesTable/WithdrawalAddressesTable'
 import { WithdrawalAddressesRoute } from 'app/pages/accounts/pages/withdrawalAddresses/router/config'
@@ -13,33 +12,37 @@ export const WithdrawalAddressesList: React.FC = () => {
     <>
       <Grid container direction='column' style={{ display: 'table' }}>
         <Grid item>
-          <PageHeader title='My Wallet Addresses' />
+          <PageHeader
+            title='My Wallet Addresses'
+            endComponent={
+              <Grid
+                item
+                container
+                xs={12}
+                justifyContent='flex-end'
+                spacing={1}
+                alignItems='center'
+              >
+                <Grid item>
+                  <Button
+                    component={AppRouterLinkComponent}
+                    to={WithdrawalAddressesRoute.create}
+                    size='large'
+                    color='primary'
+                    variant='contained'
+                    disableElevation
+                  >
+                    Add Wallet Address
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <WithdrawalAddressTooltip />
+                </Grid>
+              </Grid>
+            }
+          />
         </Grid>
         <RootContainer>
-          <Grid
-            item
-            container
-            xs={12}
-            justifyContent='flex-end'
-            spacing={1}
-            alignItems='center'
-          >
-            <Grid item>
-              <Button
-                component={AppRouterLinkComponent}
-                to={WithdrawalAddressesRoute.create}
-                variant='contained'
-                color='primary'
-                disableElevation
-              >
-                Add Address
-              </Button>
-            </Grid>
-            <Grid item>
-              <WithdrawalAddressTooltip />
-            </Grid>
-          </Grid>
-          <VSpacer size='small' />
           <Grid item>
             <WithdrawalAddressesTable />
           </Grid>
