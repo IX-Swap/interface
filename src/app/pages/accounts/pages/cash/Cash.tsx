46bcc50693b4709dfc116ed798c375ba4baed3eb
Ray Caballero <ray@investax.io>
2023-08-14 09:38:53 +0800

    move link to cash accounts page from settings page to cash page

--- src/app/pages/accounts/pages/cash/Cash.tsx
+++ src/app/pages/accounts/pages/cash/Cash.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, Button } from '@mui/material'
 import { PageHeader } from 'app/components/PageHeader/PageHeader'
 import React from 'react'
 import { RootContainer } from 'ui/RootContainer'
@@ -6,12 +6,29 @@ import { CashTable } from 'app/pages/accounts/pages/cash/components/CashTable'
 import { NoCashButtons } from './components/NoCashButtons'
 import { CashBalance } from 'app/pages/accounts/components/CashBalance'
 import { RecentTransactionsTable } from './components/RecentTransactionsTable'
+import { AppRouterLinkComponent } from 'components/AppRouterLink'
+import { AccountsRoute } from 'app/pages/accounts/router/config'
 
 export const Cash = () => {
   return (
     <Grid container direction='column' style={{ display: 'table' }}>
       <Grid item>
-        <PageHeader title='Cash' />
+        <PageHeader
+          title='Cash'
+          endComponent={
+            <Button
+              variant='contained'
+              color='primary'
+              size='medium'
+              disableElevation
+              sx={{ paddingX: 5, paddingY: 2 }}
+              component={AppRouterLinkComponent}
+              to={AccountsRoute.banks}
+            >
+              View Cash Accounts
+            </Button>
+          }
+        />
       </Grid>
       <RootContainer>
         <Grid container spacing={{ xs: 2, md: 6 }}>
