4b20259dd60f5ea25c00c09a9dd77ed561ea5d79
Ray Caballero <ray@investax.io>
2023-08-21 10:39:32 +0800

    Add error message for invalid video urls

--- src/app/components/DSO/components/DSOVideoLinksView.tsx
+++ src/app/components/DSO/components/DSOVideoLinksView.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, Alert } from '@mui/material'
 import { DigitalSecurityOffering } from 'types/dso'
 import ReactPlayer from 'react-player/lazy'
+import { FieldContainer } from 'ui/FieldContainer/FieldContainer'
 
 export interface DSOVideoLinksViewProps {
   dso: DigitalSecurityOffering
@@ -9,23 +10,34 @@ export interface DSOVideoLinksViewProps {
 
 export const DSOVideoLinksView = ({ dso }: DSOVideoLinksViewProps) => {
   return (
-    <Grid container spacing={3}>
-      {dso.videos.map(item => (
-        <Grid item xs={12} key={item.title}>
-          <ReactPlayer
-            width='100%'
-            height={434}
-            style={{
-              borderRadius: 8,
-              overflow: 'hidden'
-            }}
-            url={item.link}
-          />
-          <Typography variant='h5' sx={{ mt: 1 }}>
-            {item.title}
-          </Typography>
+    <Grid container direction='column' spacing={3}>
+      <FieldContainer>
+        <Grid container spacing={5}>
+          {dso.videos.map(item => (
+            <Grid item xs={12} key={item.title}>
+              {ReactPlayer.canPlay(item.link as string) ? (
+                <ReactPlayer
+                  width='100%'
+                  height={434}
+                  style={{
+                    borderRadius: 8,
+                    overflow: 'hidden'
+                  }}
+                  url={item.link}
+                />
+              ) : (
+                <Alert variant='filled' severity='error'>
+                  Invalid Video URL
+                </Alert>
+              )}
+
+              <Typography variant='h5' sx={{ mt: 2 }}>
+                {item.title}
+              </Typography>
+            </Grid>
+          ))}
         </Grid>
-      ))}
+      </FieldContainer>
     </Grid>
   )
 }
