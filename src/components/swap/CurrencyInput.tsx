2d61033de0ea29582fa01f0b58d2b1d41144bf48
Shibu <kapil@investax.io>
2023-09-22 14:09:13 +0530

    update the logic for test QA

--- src/components/swap/CurrencyInput.tsx
+++ src/components/swap/CurrencyInput.tsx
@@ -17,10 +17,12 @@ interface ParsedAmounts {
   INPUT: CurrencyAmount<Currency> | undefined
   OUTPUT: CurrencyAmount<Currency> | undefined
 }
+
 interface Currencies {
   INPUT?: Currency
   OUTPUT?: Currency
 }
+
 interface Props {
   parsedAmounts: ParsedAmounts
   maxInputAmount?: CurrencyAmount<Currency>
@@ -40,42 +42,26 @@ export const CurrencyInput = ({ parsedAmounts, maxInputAmount, showWrap, currenc
   const fiatValueOutput = useUSDCValue(parsedAmounts[Field.OUTPUT])
   const priceImpact = computeFiatValuePriceImpact(fiatValueInput, fiatValueOutput)
 
-  const handleTypeInput = useCallback(
-    (value: string) => {
-      onUserInput(Field.INPUT, value)
+  const handleUserInput = useCallback(
+    (field: Field, value: string) => {
+      onUserInput(field, value)
       handleHideConfirm()
     },
     [onUserInput, handleHideConfirm]
   )
 
-  const handleTypeOutput = useCallback(
-    (value: string) => {
-      onUserInput(Field.OUTPUT, value)
-      handleHideConfirm()
-    },
-    [onUserInput, handleHideConfirm]
-  )
-  const handleInputSelect = useCallback(
-    (inputCurrency: Currency) => {
+  const handleCurrencySelect = useCallback(
+    (field: Field, currency: Currency) => {
       setApprovalSubmitted(false) // reset 2 step UI for approvals
-      onCurrencySelection(Field.INPUT, inputCurrency)
+      onCurrencySelection(field, currency)
       handleHideConfirm()
     },
     [onCurrencySelection, setApprovalSubmitted, handleHideConfirm]
   )
 
-  const handleOutputSelect = useCallback(
-    (outputCurrency: Currency) => {
-      onCurrencySelection(Field.OUTPUT, outputCurrency)
-      handleHideConfirm()
-    },
-    [onCurrencySelection, handleHideConfirm]
-  )
-
   const handleMaxInput = useCallback(() => {
-    maxInputAmount && onUserInput(Field.INPUT, maxInputAmount.toExact())
-    handleHideConfirm()
-  }, [maxInputAmount, onUserInput, handleHideConfirm])
+    maxInputAmount && handleUserInput(Field.INPUT, maxInputAmount.toExact())
+  }, [maxInputAmount, handleUserInput])
 
   const formattedAmounts = {
     [independentField]: typedValue,
@@ -83,30 +69,28 @@ export const CurrencyInput = ({ parsedAmounts, maxInputAmount, showWrap, currenc
       ? parsedAmounts[independentField]?.toExact() ?? ''
       : parsedAmounts[dependentField]?.toSignificant(6) ?? '',
   }
+
   const showMaxButton = Boolean(maxInputAmount?.greaterThan(0) && !parsedAmounts[Field.INPUT]?.equalTo(maxInputAmount))
-  console.log(currencies[Field.INPUT]?.symbol, 'tetetet')
-  console.log(currencies[Field.OUTPUT]?.symbol, 'tetetet')
+
+  const isSwapBannerVisible =
+    (currencies[Field.INPUT]?.symbol === 'wSTOA' && currencies[Field.OUTPUT]?.symbol === 'TIXS') ||
+    (currencies[Field.INPUT]?.symbol === 'TIXS' && currencies[Field.OUTPUT]?.symbol === 'wSTOA') ||
+    (currencies[Field.INPUT]?.symbol === 'WIXS' && currencies[Field.OUTPUT]?.symbol === 'wTAU') ||
+    (currencies[Field.INPUT]?.symbol === 'wTAU' && currencies[Field.OUTPUT]?.symbol === 'WIXS')
+
   return (
     <>
-      {/* {(currencies[Field.INPUT]?.symbol === 'wSTOA' && currencies[Field.OUTPUT]?.symbol === 'TIXS') ||
-      (currencies[Field.INPUT]?.symbol === 'TIXS' && currencies[Field.OUTPUT]?.symbol === 'wSTOA') ? (
-        <SwapBanner />
-      ) : null} */}
-      {(currencies[Field.INPUT]?.symbol === 'WIXS' && currencies[Field.OUTPUT]?.symbol === 'wTAU') ||
-      (currencies[Field.INPUT]?.symbol === 'wTAU' && currencies[Field.OUTPUT]?.symbol === 'WIXS') ? (
-        <SwapBanner />
-      ) : null}
-      {/* <SwapBanner /> */}
+      {isSwapBannerVisible && <SwapBanner />}
       <div style={{ display: 'relative' }}>
         <CurrencyInputPanel
           label={independentField === Field.OUTPUT && !showWrap ? <Trans>From (at most)</Trans> : <Trans>From</Trans>}
           value={formattedAmounts[Field.INPUT]}
           showMaxButton={showMaxButton}
           currency={currencies[Field.INPUT]}
-          onUserInput={handleTypeInput}
+          onUserInput={(value) => handleUserInput(Field.INPUT, value)}
           onMax={handleMaxInput}
           fiatValue={fiatValueInput ?? undefined}
-          onCurrencySelect={handleInputSelect}
+          onCurrencySelect={(currency) => handleCurrencySelect(Field.INPUT, currency)}
           otherCurrency={currencies[Field.OUTPUT]}
           showCommonBases={true}
           title={<Trans>Select a token to swap</Trans>}
@@ -129,14 +113,14 @@ export const CurrencyInput = ({ parsedAmounts, maxInputAmount, showWrap, currenc
         </ArrowWrapper>
         <CurrencyInputPanel
           value={formattedAmounts[Field.OUTPUT]}
-          onUserInput={handleTypeOutput}
+          onUserInput={(value) => handleUserInput(Field.OUTPUT, value)}
           label={independentField === Field.INPUT && !showWrap ? <Trans>To (at least)</Trans> : <Trans>To</Trans>}
           showMaxButton={false}
           hideBalance={false}
           fiatValue={fiatValueOutput ?? undefined}
           priceImpact={priceImpact}
           currency={currencies[Field.OUTPUT]}
-          onCurrencySelect={handleOutputSelect}
+          onCurrencySelect={(currency) => handleCurrencySelect(Field.OUTPUT, currency)}
           otherCurrency={currencies[Field.INPUT]}
           showCommonBases={true}
           id="swap-currency-output"
