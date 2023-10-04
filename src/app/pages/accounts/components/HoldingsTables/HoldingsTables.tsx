2714981c0f10ffbf53662d1a2a385c68eb92f4bd
Ray Caballero <ray@investax.io>
2023-10-04 11:23:33 +0800

    Changes to IX Exchange orders page

--- src/app/pages/accounts/components/HoldingsTables/HoldingsTables.tsx
+++ src/app/pages/accounts/components/HoldingsTables/HoldingsTables.tsx
@@ -1,6 +1,6 @@
 import { TableTabsView } from 'app/components/TableTabsView/TableTabsView'
 // import { CurrentHoldingsTable } from 'app/pages/accounts/components/CurrentHoldingsTable/CurrentHoldingsTable'
-import { CustodyList } from '../../pages/digitalSecurities/DSList/CustodyList'
+// import { CustodyList } from '../../pages/digitalSecurities/DSList/CustodyList'
 import { TradeHistoryTable } from 'app/pages/accounts/components/TradeHistoryTable/TradeHistoryTable'
 import { YourOrdersTable } from 'app/pages/accounts/components/YourOrdersTable/YourOrderstable'
 import { useQueryFilter } from 'hooks/filters/useQueryFilter'
@@ -16,31 +16,30 @@ export interface HoldingsTablesProps {
   }) => void
 }
 
-export const HoldingsTables = ({ setHeaderContent }: HoldingsTablesProps) => {
-  const tabs = [
-    {
-      //   panel: <CurrentHoldingsTable />,
-      panel: <CustodyList />,
-      label: 'Current Holdings',
-      headerTitle: 'Current Holdings',
-      description: 'View, manage, and track your STOs.'
-    },
-    {
-      panel: <TradeHistoryTable />,
-      label: 'Trade History',
-      headerTitle: 'Trade History',
-      description:
-        'View, manage, and filter all the transactions you have made.'
-    },
-    {
-      panel: <YourOrdersTable />,
-      label: 'Orders',
-      headerTitle: 'Orders',
-      description:
-        'View, manage, and track the number of orders open on your order book.'
-    }
-  ]
+export const tabs = [
+  // {
+  //   //   panel: <CurrentHoldingsTable />,
+  //   panel: <CustodyList />,
+  //   label: 'Current Holdings',
+  //   headerTitle: 'Current Holdings',
+  //   description: 'View, manage, and track your STOs.'
+  // },
+  {
+    panel: <TradeHistoryTable />,
+    label: 'Past Orders',
+    headerTitle: 'Past Orders',
+    description: 'View, manage, and filter all the transactions you have made.'
+  },
+  {
+    panel: <YourOrdersTable />,
+    label: 'Open Orders',
+    headerTitle: 'Open Orders',
+    description:
+      'View, manage, and track the number of orders open on your order book.'
+  }
+]
 
+export const HoldingsTables = ({ setHeaderContent }: HoldingsTablesProps) => {
   const { removeFilters } = useQueryFilter()
 
   const handleChange = (event: object, value: any) => {
