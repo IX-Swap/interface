4e928a7a4515795d1d6773e1f76a1379a37c2682
Ray Caballero <ray@investax.io>
2023-10-20 18:41:46 +0800

    ask user to go to invest

--- src/app/pages/accounts/pages/digitalSecurities/DSList/DSList.tsx
+++ src/app/pages/accounts/pages/digitalSecurities/DSList/DSList.tsx
@@ -6,8 +6,13 @@ import { CustodyList } from 'app/pages/accounts/pages/digitalSecurities/DSList/C
 import { DSRoute } from 'app/pages/accounts/pages/digitalSecurities/router/config'
 import { AppRouterLinkComponent } from 'components/AppRouterLink'
 import { RootContainer } from 'ui/RootContainer'
+import { useAppState } from 'app/hooks/useAppState'
+import { FieldContainer } from 'ui/FieldContainer/FieldContainer'
+import { NoInvestments } from './NoInvestments'
 
 export const DSList: React.FC = () => {
+  const { tableHasData } = useAppState()
+
   return (
     <Grid container spacing={3} style={{ display: 'table' }}>
       <Grid item xs={12}>
@@ -32,16 +37,18 @@ export const DSList: React.FC = () => {
                   Deposit
                 </Button>
               </Grid>
-              <Grid item>
-                <Button
-                  component={AppRouterLinkComponent}
-                  variant='outlined'
-                  color='primary'
-                  to={DSRoute.withdraw}
-                >
-                  Withdraw
-                </Button>
-              </Grid>
+              {tableHasData && (
+                <Grid item>
+                  <Button
+                    component={AppRouterLinkComponent}
+                    variant='outlined'
+                    color='primary'
+                    to={DSRoute.withdraw}
+                  >
+                    Withdraw
+                  </Button>
+                </Grid>
+              )}
             </Grid>
           }
         />
@@ -49,7 +56,13 @@ export const DSList: React.FC = () => {
       <RootContainer padding={0}>
         <Grid item xs={12} mt={2} ml={1.5}>
           {/* <DSTabs /> */}
-          <CustodyList hasTopBorder={false} />
+          {tableHasData ? (
+            <CustodyList hasTopBorder={false} />
+          ) : (
+            <FieldContainer>
+              <NoInvestments />
+            </FieldContainer>
+          )}
         </Grid>
       </RootContainer>
     </Grid>
