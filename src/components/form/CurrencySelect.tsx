5a38b49bbdfa2d1116679b6bd2a1260e48ee468f
Ray Caballero <ray@investax.io>
2023-10-30 13:55:28 +0800

    Added currency dropdown

--- src/components/form/CurrencySelect.tsx
+++ src/components/form/CurrencySelect.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { Box } from '@mui/material'
 import { renderSelectItems } from 'helpers/rendering'
 import { Select } from 'ui/Select/Select'
 import { SelectItem } from 'ui/Select/SelectItem/SelectItem'
 import { InputLabel } from 'ui/Select/InputLabel/InputLabel'
+import { ReactComponent as SGDIcon } from 'assets/icons/flags/sgd.svg'
+import { ReactComponent as USDIcon } from 'assets/icons/flags/usd.svg'
 
 const CURRENCIES = [
   {
-    label: 'SGD',
+    label: (
+      <Box display={'flex'} alignItems={'center'}>
+        <SGDIcon style={{ marginRight: 16 }} />
+        SGD
+      </Box>
+    ),
     value: 'SGD'
   },
   {
-    label: 'USD',
+    label: (
+      <Box display={'flex'} alignItems={'center'}>
+        <USDIcon style={{ marginRight: 16 }} />
+        USD
+      </Box>
+    ),
     value: 'USD'
   }
 ]
@@ -24,9 +37,12 @@ export const CurrencySelect = (props: any): JSX.Element => {
         label={undefined}
         placeholder={String(props.label)}
         displayEmpty
+        renderValue={value =>
+          CURRENCIES.find(currency => currency.value === value)?.label
+        }
       >
         <SelectItem disabled value={undefined}>
-          Currency
+          Select Currency
         </SelectItem>
         {renderSelectItems(CURRENCIES)}
       </Select>
--- src/types/virtualAccountTransaction.ts
+++ src/types/virtualAccountTransaction.ts
@@ -2,6 +2,7 @@ export interface VirtualAccountTransactionFormValues {
   user: string
   accountId: string
   email: string
+  currency: 'SGD' | 'USD'
   amount: number
   type: 'Credit' | 'Debit'
   reference: string
