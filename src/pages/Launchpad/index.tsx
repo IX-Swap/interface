8d12657e81a86e40c571af040b714ff2a5728e08
Harry Nguyen <harrynguyen@MacBook-Pro.local>
2023-11-22 10:16:17 +0700

    657 Fix Users cannot access Launchpad without connecting wallet

--- src/pages/Launchpad/index.tsx
+++ src/pages/Launchpad/index.tsx
@@ -30,7 +30,7 @@ export default function Launchpad() {
   }, [])
 
   const blurred = React.useMemo(
-    () => ![...TGE_CHAINS_WITH_STAKING, SUPPORTED_TGE_CHAINS.MAIN].includes(chainId || 0),
+    () => chainId && ![...TGE_CHAINS_WITH_STAKING, SUPPORTED_TGE_CHAINS.MAIN].includes(chainId || 0),
     [account, chainId]
   )
 
