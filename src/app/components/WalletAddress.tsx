22cf094aed8078ec27573cd13ec54a44abde1124
Ray Caballero <ray@investax.io>
2023-12-06 16:42:34 +0800

    fix 500 error

--- src/app/components/WalletAddress.tsx
+++ src/app/components/WalletAddress.tsx
@@ -30,7 +30,7 @@ export const WalletAddress = ({
 
   const textContent = long
     ? address
-    : `${address.slice(0, 4)}...${address.slice(address.length - 4)}`
+    : `${address?.slice(0, 4)}...${address?.slice(address.length - 4)}`
 
   if (address === '') {
     return null
