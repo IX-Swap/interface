781fcc0e95f6352ecb99e3bb506a2d84fc1e27e9
thi-investax <thi@investax.io>
2024-03-04 15:09:40 +0700

    Upgrade web3 package

--- src/helpers/web3/web3.ts
+++ src/helpers/web3/web3.ts
@@ -1,12 +1,15 @@
 import Web3 from 'web3'
 import { Abi } from './web3Data'
+import { ethers } from 'ethers'
 
 export class Web3Helpers {
   web3
 
   constructor() {
     this.web3 = new Web3(
-      new Web3.providers.HttpProvider(`https://${process.env.REACT_APP_INFURA_NETWORK_SUBDOMAIN}.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`)
+      new Web3.providers.HttpProvider(
+        `https://${process.env.REACT_APP_INFURA_NETWORK_SUBDOMAIN}.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`
+      )
     )
   }
 
@@ -18,9 +21,7 @@ export class Web3Helpers {
       .then(function (result: any) {
         return result
       })
-    const decimals = await contract.methods.decimals().call(function (error: any, d: any) {
-      return d
-    })
+    const decimals: number = await contract.methods.decimals().call()
     // return balance * 10 ** decimals
     return balance / 10 ** decimals
   }
@@ -29,24 +30,20 @@ export class Web3Helpers {
     try {
       const balance = await this.web3.eth.getBalance(address)
 
-      return await this.web3.utils.fromWei(balance, 'ether')
+      return ethers.utils.formatUnits(balance, 'ether')
     } catch (e) {
       throw new Error('Eth balance checking failed:' + e)
     }
   }
 
-  async sendCrypto(
-    wallet: { ethAddress: any; privateKey: string },
-    addressTo: any,
-    amount: { toString: () => import('bn.js') }
-  ) {
+  async sendCrypto(wallet: { ethAddress: any; privateKey: string }, addressTo: any, amount: any) {
     try {
       // create transaction
       const createTransaction = await this.web3.eth.accounts.signTransaction(
         {
           from: wallet.ethAddress,
           to: addressTo,
-          value: this.web3.utils.toWei(amount.toString()),
+          value: ethers.utils.formatUnits(amount.toString(), 'ether'),
           gas: '2000000',
         },
         wallet.privateKey
