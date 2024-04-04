00482a31f7e377bf5d52153907df546c05d8617b
Kapil <kapil@Kapils-MacBook-Pro.local>
2024-04-04 11:40:02 +0530

    debug chart

--- src/pages/LBP/LbpForm.tsx
+++ src/pages/LBP/LbpForm.tsx
@@ -1,68 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { Trans } from '@lingui/macro';
-import Column from 'components/Column';
-import { RowStart } from 'components/Row';
-import { FormContainer, FormRow } from 'pages/KYC/IndividualKycForm';
-import { FormCard, StyledStickyBox } from 'pages/KYC/styleds';
-import { isMobile } from 'react-device-detect';
-import { TYPE } from 'theme';
-import Branding from './components/Branding';
-import ProjectInfo from './components/ProjectInfo';
-import Tokenomics from './components/Tokenomics';
-import Approvals from './components/Approvals';
-import { KYCProgressBar } from 'pages/KYC/KYCProgressBar';
-import { Card } from 'rebass';
+import React, { useState, useEffect } from 'react'
+import { Trans } from '@lingui/macro'
+import Column from 'components/Column'
+import { RowStart } from 'components/Row'
+import { FormContainer, FormRow } from 'pages/KYC/IndividualKycForm'
+import { FormCard, StyledStickyBox } from 'pages/KYC/styleds'
+import { isMobile } from 'react-device-detect'
+import { TYPE } from 'theme'
+import Branding from './components/Branding'
+import ProjectInfo from './components/ProjectInfo'
+import Tokenomics from './components/Tokenomics'
+import Approvals from './components/Approvals'
+import { KYCProgressBar } from 'pages/KYC/KYCProgressBar'
+import { Card } from 'rebass'
+import Graph from './components/Graph'
 
 export default function LBPForm() {
   const [formData, setFormData] = useState({
     branding: {},
     projectInfo: {},
     tokenomics: {},
-  });
-  const [canSubmit, setCanSubmit] = useState(false);
+  })
+  const [canSubmit, setCanSubmit] = useState(false)
 
   useEffect(() => {
-    updateSubmitButtonState(formData);
-  }, [formData]);
+    updateSubmitButtonState(formData)
+  }, [formData])
 
   const handleBrandingChange = (brandingData: any) => {
-    setFormData((prevData) => ({ ...prevData, branding: brandingData }));
-  };
+    setFormData((prevData) => ({ ...prevData, branding: brandingData }))
+  }
 
   const handleProjectInfoChange = (projectInfoData: any) => {
-    setFormData((prevData) => ({ ...prevData, projectInfo: projectInfoData }));
-  };
+    setFormData((prevData) => ({ ...prevData, projectInfo: projectInfoData }))
+  }
 
   const handleTokenomicsChange = (tokenomicsData: any) => {
-    setFormData((prevData) => ({ ...prevData, tokenomics: tokenomicsData }));
-  };
+    setFormData((prevData) => ({ ...prevData, tokenomics: tokenomicsData }))
+  }
 
   const updateSubmitButtonState = (formData: any) => {
-    const isComplete = (data: any) => Object.values(data).every((val) => !!val);
-    const brandingComplete = isComplete(formData.branding);
-    const projectInfoComplete = isComplete(formData.projectInfo);
-    const tokenomicsComplete = isComplete(formData.tokenomics);
-    const hasSocialLinks = formData.projectInfo.socialLinks?.length > 0;
-    const hasWhitepapers = formData.projectInfo.whitepapers?.length > 0;
-    const hasUploadDocs = formData.projectInfo.uploadDocs?.length > 0;
+    const isComplete = (data: any) => Object.values(data).every((val) => !!val)
+    const brandingComplete = isComplete(formData.branding)
+    const projectInfoComplete = isComplete(formData.projectInfo)
+    const tokenomicsComplete = isComplete(formData.tokenomics)
+    const hasSocialLinks = formData.projectInfo.socialLinks?.length > 0
+    const hasWhitepapers = formData.projectInfo.whitepapers?.length > 0
+    const hasUploadDocs = formData.projectInfo.uploadDocs?.length > 0
     setCanSubmit(
-      brandingComplete &&
-      projectInfoComplete &&
-      tokenomicsComplete &&
-      hasSocialLinks &&
-      hasWhitepapers &&
-      hasUploadDocs
-    );
-  };
+      brandingComplete && projectInfoComplete && tokenomicsComplete && hasSocialLinks && hasWhitepapers && hasUploadDocs
+    )
+  }
 
   const handleSubmit = () => {
-    console.log(formData);
-  };
+    console.log(formData)
+  }
 
   return (
     <FormRow>
       <FormContainer style={{ gap: '35px', margin: '20px 0px 0px 150px' }}>
-        <TYPE.title4 fontWeight={'800'} fontSize={isMobile ? 24 : 24} style={{ whiteSpace: 'nowrap' }} marginLeft="10px">
+        <TYPE.title4
+          fontWeight={'800'}
+          fontSize={isMobile ? 24 : 24}
+          style={{ whiteSpace: 'nowrap' }}
+          marginLeft="10px"
+        >
           <Trans>Serenity</Trans>
         </TYPE.title4>
         <Column style={{ gap: '35px' }}>
@@ -139,10 +140,12 @@ export default function LBPForm() {
           />
         </StyledStickyBox>
 
+        <Graph  step={1} graphData={formData.tokenomics} />
+
         {/* <Card style={{ marginTop: '200px', width: '300px', height: '400px', background: '#FFFFFF', position: 'fixed' }}>
           <div></div>
         </Card> */}
       </div>
     </FormRow>
-  );
+  )
 }
---
+++ src/pages/LBP/components/Graph.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Card } from 'rebass'
+import { FormCard, StyledStickyBox } from 'pages/KYC/styleds'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+
+interface GraphProps {
+  graphData: any
+  step: number
+}
+
+const StyledCard = styled(Card)`
+  width: 300px;
+  height: 400px;
+  background: #ffffff;
+  margin-top: 200px;
+`
+
+export default function Graph({ graphData, step }: GraphProps) {
+  const getPrice = (
+    shareReserve: number,
+    currentShareWeight: number,
+    assetReserve: number,
+    currentAssetWeight: number
+  ) => {
+    return (assetReserve * currentShareWeight) / (shareReserve * currentAssetWeight)
+  }
+  const getDecayAtStep = (shareStartWeight: number, shareEndWeight: number, step: number, totalStep: number) => {
+    return ((shareEndWeight - shareStartWeight) * step) / totalStep
+  }
+  const generateChartData = (step: number) => {
+
+    //totalHours
+    const issuanceDate = new Date(graphData.idIssuanceDate).getTime()
+    const expirationDate = new Date(graphData.idExpirationDate).getTime()
+    const totalHours = Math.ceil((expirationDate - issuanceDate) / (1000 * 60 * 60)) 
+    console.log(totalHours, 'totalHours')
+
+
+    const numPoints = Math.floor(totalHours / step) + 1
+    const chartData = []
+
+    //startWeight endWeight
+
+    const shareStartWeight = 90
+    const shareEndWeight = 30
+
+    //shareInput assetInput
+
+    const shareReserve = graphData?.shareInput
+    const assetReserve = graphData?.assetInput
+
+    for (let i = 0; i < numPoints; i++) {
+      const decay = getDecayAtStep(shareStartWeight, shareEndWeight, i, numPoints)
+      console.info(decay)
+      const currentShareWeight = shareStartWeight + decay
+      console.info('currentShareWeight', currentShareWeight)
+      const currentAssetWeight = 1 - currentShareWeight
+      console.info('currentAssetWeight', currentAssetWeight)
+
+      const price = getPrice(shareReserve, currentShareWeight, assetReserve, currentAssetWeight) 
+      console.info('price', price)
+      chartData.push({ hour: i, price })
+    }
+
+    return chartData
+  }
+  const data = generateChartData(step)
+  return (
+    <StyledCard>
+      <LineChart width={300} height={300} data={data} margin={{ top: 20, right: 30, bottom: 10 }}>
+        <CartesianGrid strokeDasharray="3 3" stroke="#8884d8" horizontal={true} vertical={false} />
+        <XAxis dataKey="hour" type="number" label={{ value: 'Hour', position: 'insideBottom', offset: -10 }} axisLine={false} tickLine={false} />
+        <YAxis dataKey="price" type="number" label={{ value: 'Price', angle: -90, position: 'insideLeft' }} axisLine={false} tickLine={false} />
+        <Tooltip />
+        <Legend />
+        <Line dataKey="price" stroke="#8884d8" dot={false} />
+      </LineChart>
+    </StyledCard>
+  )
+}
