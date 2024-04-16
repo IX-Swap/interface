60f2c9aaba68c975961ddfc7c8e2b3ff9bbe12db
thi-investax <thi@investax.io>
2024-04-16 13:36:52 +0700

    Format code, send assetSymbolAddress, assetTokenAddress in post lbp endpoint

--- src/pages/LBP/LbpForm.tsx
+++ src/pages/LBP/LbpForm.tsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Trans } from '@lingui/macro';
-import Column from 'components/Column';
-import { RowStart } from 'components/Row';
-import { FormContainer, FormRow } from 'pages/KYC/IndividualKycForm';
-import { FormCard, StyledStickyBox } from 'pages/KYC/styleds';
-import { isMobile } from 'react-device-detect';
-import { BrandingProps, ProjectInfoProps, TokenomicsProps } from '../../../src/components/LBP/types';
-import Branding from './components/Branding';
-import ProjectInfo from './components/ProjectInfo';
-import Tokenomics from './components/Tokenomics';
-import Approvals from './components/Approvals';
-import { KYCProgressBar } from 'pages/KYC/KYCProgressBar';
-import Graph from './components/Graph';
-import { useGetLbp, useSaveOrSubmitLbp } from 'state/lbp/hooks';
-import { LBP_ACTION_TYPES } from 'state/lbp/constants';
-import { useQueryParams } from 'hooks/useParams';
-import { LbpFile, LbpFormValues } from 'components/LBP/types';
-import { useLoader } from 'state/launchpad/hooks';
-import { useAddPopup } from 'state/application/hooks';
-import { useHistory } from 'react-router-dom';
-import { TYPE } from 'theme';
+import React, { useState, useEffect } from 'react'
+import { Trans } from '@lingui/macro'
+import Column from 'components/Column'
+import { RowStart } from 'components/Row'
+import { FormContainer, FormRow } from 'pages/KYC/IndividualKycForm'
+import { FormCard, StyledStickyBox } from 'pages/KYC/styleds'
+import { isMobile } from 'react-device-detect'
+import { BrandingProps, ProjectInfoProps, TokenomicsProps } from '../../../src/components/LBP/types'
+import Branding from './components/Branding'
+import ProjectInfo from './components/ProjectInfo'
+import Tokenomics from './components/Tokenomics'
+import Approvals from './components/Approvals'
+import { KYCProgressBar } from 'pages/KYC/KYCProgressBar'
+import Graph from './components/Graph'
+import { useGetLbp, useSaveOrSubmitLbp } from 'state/lbp/hooks'
+import { LBP_ACTION_TYPES } from 'state/lbp/constants'
+import { useQueryParams } from 'hooks/useParams'
+import { LbpFile, LbpFormValues } from 'components/LBP/types'
+import { useLoader } from 'state/launchpad/hooks'
+import { useAddPopup } from 'state/application/hooks'
+import { useHistory } from 'react-router-dom'
+import { TYPE } from 'theme'
 
 interface FormData {
-  id: number;
-  branding: BrandingProps;
-  projectInfo: ProjectInfoProps;
-  tokenomics: TokenomicsProps;
+  id: number
+  branding: BrandingProps
+  projectInfo: ProjectInfoProps
+  tokenomics: TokenomicsProps
 }
 
 export default function LBPForm() {
@@ -34,15 +34,15 @@ export default function LBPForm() {
     branding: {
       LBPLogo: {
         mimeType: undefined,
-        name: ''
+        name: '',
       },
       LBPBanner: {
         mimeType: undefined,
-        name: ''
-      }
+        name: '',
+      },
     },
     projectInfo: {
-      name:'',
+      name: '',
       title: '',
       description: '',
       website: '',
@@ -52,6 +52,8 @@ export default function LBPForm() {
     },
     tokenomics: {
       shareAddress: '',
+      assetTokenAddress: '',
+      assetTokenSymbol: '',
       shareInput: 0,
       maxSupply: 0,
       assetInput: 0,
@@ -62,87 +64,87 @@ export default function LBPForm() {
       startDate: '',
       endWeight: 0,
     },
-  });
-  const [canSubmit, setCanSubmit] = useState(false);
+  })
+  const [canSubmit, setCanSubmit] = useState(false)
 
-  const loader = useLoader(false);
-  const addPopup = useAddPopup();
-  const history = useHistory();
-  const getLbp = useGetLbp();
-  const saveOrSubmitLbp = useSaveOrSubmitLbp();
+  const loader = useLoader(false)
+  const addPopup = useAddPopup()
+  const history = useHistory()
+  const getLbp = useGetLbp()
+  const saveOrSubmitLbp = useSaveOrSubmitLbp()
 
   const {
     objectParams: { id: lbpId },
-  } = useQueryParams<{ id: number }>(['id']);
+  } = useQueryParams<{ id: number }>(['id'])
 
   useEffect(() => {
     const getLbpAsync = async () => {
-      const lbp = await getLbp(lbpId);
-      const loadedFormData = transformDataForLoading(lbp);
-      setFormData(loadedFormData);
-    };
+      const lbp = await getLbp(lbpId)
+      const loadedFormData = transformDataForLoading(lbp)
+      setFormData(loadedFormData)
+    }
 
-    getLbpAsync();
-  }, []);
+    getLbpAsync()
+  }, [])
 
   useEffect(() => {
-    updateSubmitButtonState(formData);
-  }, [formData]);
+    updateSubmitButtonState(formData)
+  }, [formData])
 
   const handleBrandingChange = (brandingData: BrandingProps) => {
-    setFormData((prevData) => ({ ...prevData, branding: brandingData }));
-  };
+    setFormData((prevData) => ({ ...prevData, branding: brandingData }))
+  }
 
   const handleProjectInfoChange = (projectInfoData: ProjectInfoProps) => {
     setFormData((prevData) => ({
       ...prevData,
-      projectInfo: { ...prevData.projectInfo, ...projectInfoData }
-    }));
-  };
+      projectInfo: { ...prevData.projectInfo, ...projectInfoData },
+    }))
+  }
 
   const handleTokenomicsChange = (tokenomicsData: TokenomicsProps) => {
     setFormData((prevData) => ({
       ...prevData,
-      tokenomics: { ...prevData.tokenomics, ...tokenomicsData }
-    }));
-  };
+      tokenomics: { ...prevData.tokenomics, ...tokenomicsData },
+    }))
+  }
 
   const updateSubmitButtonState = (formData: FormData) => {
-    formData.projectInfo.name = formData?.projectInfo?.name;
-    const isComplete = (data: any) => Object.values(data).every((val) => !!val);
-    const brandingComplete = isComplete(formData.branding);
-    const projectInfoComplete = isComplete(formData.projectInfo);
-    const tokenomicsComplete = isComplete(formData.tokenomics);
-    const hasSocialLinks = formData.projectInfo.socialLinks?.length > 0;
-    const hasWhitepapers = formData.projectInfo.whitepapers?.length > 0;
-    const hasUploadDocs = formData.projectInfo.uploadDocs?.length > 0;
+    formData.projectInfo.name = formData?.projectInfo?.name
+    const isComplete = (data: any) => Object.values(data).every((val) => !!val)
+    const brandingComplete = isComplete(formData.branding)
+    const projectInfoComplete = isComplete(formData.projectInfo)
+    const tokenomicsComplete = isComplete(formData.tokenomics)
+    const hasSocialLinks = formData.projectInfo.socialLinks?.length > 0
+    const hasWhitepapers = formData.projectInfo.whitepapers?.length > 0
+    const hasUploadDocs = formData.projectInfo.uploadDocs?.length > 0
     setCanSubmit(
       brandingComplete && projectInfoComplete && tokenomicsComplete && hasSocialLinks && hasWhitepapers && hasUploadDocs
-    );
-  };
+    )
+  }
 
   const saveLbp = async (actionType: string) => {
-    loader.start();
+    loader.start()
     try {
-      const data = transformDataForSaving(formData);
-      await saveOrSubmitLbp(actionType, data);
-      const summary = actionType === LBP_ACTION_TYPES.save ? 'LBP saved successfully' : 'LBP submitted successfully';
-      addPopup({ info: { success: true, summary } });
-      history.push('/lbp');
+      const data = transformDataForSaving(formData)
+      await saveOrSubmitLbp(actionType, data)
+      const summary = actionType === LBP_ACTION_TYPES.save ? 'LBP saved successfully' : 'LBP submitted successfully'
+      addPopup({ info: { success: true, summary } })
+      history.push('/lbp')
     } catch (err: any) {
-      addPopup({ info: { success: false, summary: err?.toString() } });
+      addPopup({ info: { success: false, summary: err?.toString() } })
     } finally {
-      loader.stop();
+      loader.stop()
     }
-  };
+  }
 
   const handleSubmit = async () => {
-    await saveLbp(LBP_ACTION_TYPES.submit);
-  };
+    await saveLbp(LBP_ACTION_TYPES.submit)
+  }
 
   const handleSaveDraft = async () => {
-    await saveLbp(LBP_ACTION_TYPES.save);
-  };
+    await saveLbp(LBP_ACTION_TYPES.save)
+  }
 
   const transformDataForSaving = (formData: FormData) => {
     const result: LbpFormValues = {
@@ -160,6 +162,8 @@ export default function LBPForm() {
       shareAmount: formData.tokenomics?.shareInput,
       shareMaxSupply: formData.tokenomics?.maxSupply,
       assetTokenAmount: formData.tokenomics?.assetInput,
+      assetTokenAddress: formData.tokenomics?.assetTokenAddress,
+      assetTokenSymbol: formData.tokenomics?.assetTokenSymbol,
       startWeight: formData.tokenomics?.startWeight,
       endWeight: formData.tokenomics?.endWeight,
       startDate: formData.tokenomics?.startDate,
@@ -167,10 +171,10 @@ export default function LBPForm() {
       minPrice: formData.tokenomics?.minPrice,
       maxPrice: formData.tokenomics?.maxPrice,
       additionalDocumentIds: [],
-    };
+    }
 
-    return result;
-  };
+    return result
+  }
 
   const transformDataForLoading = (data: LbpFormValues) => {
     return {
@@ -190,6 +194,8 @@ export default function LBPForm() {
       },
       tokenomics: {
         shareAddress: data.shareAddress,
+        assetTokenAddress: data.assetTokenAddress,
+        assetTokenSymbol: data.assetTokenSymbol,
         shareInput: data.shareAmount,
         maxSupply: data.shareMaxSupply,
         assetInput: data.assetTokenAmount,
@@ -200,15 +206,13 @@ export default function LBPForm() {
         startDate: data.startDate,
         endWeight: data.endWeight,
       },
-    };
-  };
-
-
+    }
+  }
 
   return (
     <FormRow>
       <FormContainer style={{ gap: '35px', margin: '20px 0px 0px 150px' }}>
-      <TYPE.title4
+        <TYPE.title4
           fontWeight={'800'}
           fontSize={isMobile ? 24 : 24}
           style={{ whiteSpace: 'nowrap' }}
@@ -222,7 +226,7 @@ export default function LBPForm() {
               <Trans>Branding</Trans>
             </RowStart>
             <Column style={{ gap: '20px' }}>
-              <Branding brandingData={formData.branding}  onChange={handleBrandingChange} />
+              <Branding brandingData={formData.branding} onChange={handleBrandingChange} />
             </Column>
           </FormCard>
 
@@ -286,5 +290,5 @@ export default function LBPForm() {
         <Graph step={1} graphData={formData.tokenomics} />
       </div>
     </FormRow>
-  );
+  )
 }
