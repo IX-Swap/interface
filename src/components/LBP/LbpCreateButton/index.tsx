ec4fbc545179a0df044078ad65d49127cbb1f233
harryng22 <harry@investax.io>
2024-04-20 21:09:03 +1000

    IXSPD1-1114: Remove the popup "New LBP" after clicking the New LBP button

--- src/components/LBP/LbpCreateButton/index.tsx
+++ src/components/LBP/LbpCreateButton/index.tsx
@@ -34,81 +34,20 @@ const schema = object().shape({
 
 export const LbpCreateButton: React.FC<Props> = (props) => {
   const theme = useTheme()
-  const [showLbpDialog, setShowLbpDialog] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const createLbp = useCreateLbp()
   const history = useHistory()
-  const getLbpByName = useGetLbpByName()
 
-  const toggleNewLbpDialog = useCallback(() => {
-    setShowLbpDialog((state) => !state)
-  }, [])
-
-  const openLbpForm = useCallback(
-    async (values: LbpNameFormValues) => {
-      const lbp = await getLbpByName(values.name)
-
-      if (lbp) {
-        throw new Error('Lbp with that name already exists')
-      }
-
-      setLoading(true)
-      const result = await createLbp(values.name)
-      setLoading(false)
-
-      setShowLbpDialog(false)
-      history.push(`/lbp/edit?id=${result.id}`)
-    },
-    [history]
-  )
+  const openLbpForm = useCallback(async () => {
+    history.push(`/lbp/edit`)
+  }, [history])
 
   return (
     <>
       <FlexVerticalCenter style={{ marginRight: '17px' }}>
-        <OutlineButton
-          onClick={toggleNewLbpDialog}
-          background={props.background}
-          color={props.color}
-          padding="0 1.5rem"
-        >
+        <OutlineButton onClick={openLbpForm} background={props.background} color={props.color} padding="0 1.5rem">
           <Plus size="15" color={props.color ?? theme.launchpad.colors.primary} />
           <NewLbpLabel>New Lbp</NewLbpLabel>
         </OutlineButton>
       </FlexVerticalCenter>
-
-      <LbpDialog title="New Lbp" show={showLbpDialog} onClose={toggleNewLbpDialog} width="480px">
-        <Formik initialValues={initialValues} validationSchema={schema} onSubmit={openLbpForm}>
-          {({ values, errors, submitForm, setFieldValue }) => (
-            <>
-              <Label style={{ margin: '6px 0px 0px 0px' }}>Name</Label>
-              <IssuanceTextField
-                // label="Name"
-                placeholder="Name"
-                value={values.name}
-                onChange={(v) => setFieldValue('name', v)}
-                inputFilter={textFilter}
-                maxLength={1000}
-              />
-
-              {errors.name && <ErrorText>{errors.name}</ErrorText>}
-
-              <Row gap="1rem" justifyContent="spaced-evenly" width="100%">
-                <OutlineButton
-                  style={{ border: '1px solid #6666FF33' }}
-                  color="#B8B8CC"
-                  grow={1}
-                  onClick={toggleNewLbpDialog}
-                >
-                  Cancel
-                </OutlineButton>
-                <FilledButton grow={1} onClick={submitForm} disabled={loading || !values.name}>
-                  Submit
-                </FilledButton>
-              </Row>
-            </>
-          )}
-        </Formik>
-      </LbpDialog>
     </>
   )
 }
