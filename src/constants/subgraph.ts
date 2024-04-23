1aa2cdf2e58933604032a6641a3d1b1205809293
thi-investax <thi@investax.io>
2024-04-23 20:11:30 +0700

    Integrate subgraph to render LBP trade history

---
+++ src/constants/subgraph.ts
@@ -0,0 +1,6 @@
+export const SUBGRAPH_URLS: Record<string, Record<number, string>> = {
+  LBP: {
+    [137]: '',
+    [80002]: 'https://api.studio.thegraph.com/proxy/71824/ixs-lbp-subgraph-test/version/latest',
+  },
+}
---
+++ src/hooks/useSubgraphQuery.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import { SUBGRAPH_URLS } from 'constants/subgraph'
+
+interface SubgraphQueryProps {
+  feature: string
+  chainId: number | undefined
+  query: string
+  autoPolling?: boolean
+  pollingInterval?: number
+}
+
+export const useSubgraphQuery = ({
+  feature,
+  chainId,
+  query,
+  autoPolling = false,
+  pollingInterval = 3000,
+}: SubgraphQueryProps) => {
+  console.info('chainId', chainId)
+  if (!chainId) {
+    return null
+  }
+
+  const [data, setData] = useState<{ [key: string]: any }>({})
+
+  const fetchData = async () => {
+    try {
+      const endpoint = SUBGRAPH_URLS[feature][chainId]
+
+      if (!endpoint || !query) return
+
+      const resp = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query, variables: null }),
+      })
+
+      const responseData = await resp.json()
+      setData(responseData.data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+
+    if (autoPolling) {
+      // Polling logic
+      const interval = setInterval(fetchData, pollingInterval)
+
+      // Clean up the interval when the component unmounts
+      return () => clearInterval(interval)
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chainId, pollingInterval, autoPolling, query])
+
+  return data
+}
