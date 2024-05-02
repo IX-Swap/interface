3f4c3263f843059d5842b35041b50710ed06f014
thi-investax <thi@investax.io>
2024-05-02 15:08:33 +0700

    Remove default item on socialMedia

--- src/pages/LBP/LbpForm.tsx
+++ src/pages/LBP/LbpForm.tsx
@@ -47,8 +47,8 @@ export default function LBPForm() {
       title: '',
       description: '',
       website: '',
-      socialLinks: [{ name: '', value: '' }],
-      whitepapers: [{ name: '', value: '' }],
+      socialLinks: [],
+      whitepapers: [],
       uploadDocs: [] as LbpFile[],
     },
     tokenomics: {
