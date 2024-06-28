82e2def808a2221dfd7ddc47ec9a00de7a9d00d5
kapildave07 <kapil@investax.io>
2024-06-28 12:25:18 +0530

    IXSPD1-1458 addressed the issue SAAS - The footer overrides the filter dropdown

--- src/components/WhiteLabelFooter/styleds.tsx
+++ src/components/WhiteLabelFooter/styleds.tsx
@@ -6,7 +6,7 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   background-color: ${({ theme }) => theme.white};
-  z-index: 2;
+  z-index: 0;
 `
 
 export const Content = styled.div`
