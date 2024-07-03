cfbbfb277fe39fc433fd0e97ef7bc75aabb09808
leonthuongto <leon@ixswap.io>
2024-07-03 16:48:18 +0700

    Restyle UI popup wrong chain

--- src/components/NotAvailablePage/styled.ts
+++ src/components/NotAvailablePage/styled.ts
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
-import { gradientBorder, MEDIA_WIDTHS } from 'theme'
+import { MEDIA_WIDTHS } from 'theme'
 
 export const Container = styled.div`
   max-width: 779px;
-  height: 606px;
+  min-height: 473px;
   width: 100%;
   border-radius: 6px;
   background: ${({ theme }) => theme.bg25};
-  padding: 52px 58px;
+  padding: 48px;
   text-align: center;
   @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
     padding: 16px;
@@ -17,30 +17,28 @@ export const Container = styled.div`
 
 export const Title = styled.div`
   font-weight: 700;
-  font-size: 32px;
+  font-size: 18px;
   line-height: 145%;
   background: ${({ theme }) => theme.config.text?.main || theme.bgG1};
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   background-clip: text;
-  margin-bottom: 32px;
-  @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
-    font-size: 24px;
-  }
+  margin-bottom: 8px;
+  color: #292933;
 `
 
 export const Info = styled.div`
-  font-weight: 400;
-  font-size: 12px;
+  font-weight: 500;
+  font-size: 14px;
   line-height: 18px;
-  color: ${({ theme }) => theme.text1};
+  color: #8f8fb2;
 `
 
 export const NetworksRow = styled.div<{ elements: number }>`
   display: grid;
   gap: 34px;
   grid-template-columns: ${({ elements }) => `repeat(${elements}, minmax(auto, 105px))`};
-  margin: 16px auto 40px;
+  margin: 48px auto 48px;
   justify-content: center;
   @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
     grid-template-columns: ${({ elements }) => `repeat(${elements}, 1fr)`};
@@ -49,9 +47,15 @@ export const NetworksRow = styled.div<{ elements: number }>`
 `
 
 export const NetworkCard = styled.div`
-  max-width: 134px;
+  width: 160px;
+  height: 160px;
+  border-radius: 6px;
+  border: 1px solid rgba(102, 102, 255, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
   cursor: pointer;
-  border-radius: 24px;
+  border-radius: 6px;
   padding: 16px;
   font-weight: 600;
   font-size: 14px;
@@ -59,10 +63,6 @@ export const NetworkCard = styled.div`
   text-align: center;
   color: ${({ theme }) => theme.text1};
   position: relative;
-  ${gradientBorder}
-  :before {
-    border-radius: 24px;
-  }
   display: flex;
   flex-direction: column;
   row-gap: 12px;
@@ -85,6 +85,12 @@ export const InfoRows = styled.div`
   flex-direction: column;
   align-items: center;
   row-gap: 8px;
+  color: #8f8fb2;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 21px */
+  letter-spacing: -0.28px;
 `
 
 export const PlaygroundBadge = styled.div`
