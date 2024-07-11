ee9301f23c6ed12041fd73d9b75cdf635e5ae699
thi-investax <thi@investax.io>
2024-07-11 18:09:26 +0700

    Update wallet connector to allow read.ixswap.io domain

--- src/connectors/walletConnectV2.ts
+++ src/connectors/walletConnectV2.ts
@@ -3,6 +3,7 @@ import { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
 
 import { MAINNET_CHAINS, TESTNET_CHAINS } from '../chains'
 
+const IXS_DOMAINS = ['app.ixswap.io', 'readi.ixswap.io']
 const POLYGON_CHAIN_ID = 137
 const POLYGON_AMOY_CHAIN_ID = 80002
 /* eslint-disable react/display-name */
@@ -15,7 +16,7 @@ export const [walletConnectV2, hooks] = initializeConnector<WalletConnectV2>(
       actions,
       options: {
         projectId: process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID || 'ba0a3724b8cab7c0db373ed9bf3a7f09',
-        chains: [POLYGON_CHAIN_ID, POLYGON_AMOY_CHAIN_ID],
+        chains: [IXS_DOMAINS.includes(window.location.hostname) ? POLYGON_CHAIN_ID : POLYGON_AMOY_CHAIN_ID],
         optionalChains: chains,
         showQrModal: true,
       },
