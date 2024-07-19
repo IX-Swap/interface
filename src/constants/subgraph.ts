465a5c55406d7e7a23608fbd1b0265b76c4424c7
thi-investax <thi@investax.io>
2024-07-19 16:05:49 +0700

    Add LBP subgraph endpoint for Base sepolia

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -2,5 +2,6 @@ export const SUBGRAPH_URLS: Record<string, Record<number, string>> = {
   LBP: {
     [137]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/lbp-subgraph-polygon/api',
     [80002]: 'https://api.studio.thegraph.com/proxy/71824/ixs-lbp-subgraph-test/version/latest',
+    [84532]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/ixs-lbp-base-sepolia/api',
   },
 }
