bf04b93788c9b6475adc292534c4ba2334d0d99a
leonthuongto <leon@ixswap.io>
2024-07-22 12:08:21 +0700

    IXSPD1-1589 Handle wrong chain for other network (#2564)
    * Handle wrong chain for other network
    
    * Update logic of checkWrongChain funciton
    
    * Fix checkWrongChain returns
    
    * Return if expectedChains length is 0
    
    * Update env and condition
    
    ---------
    
    Co-authored-by: thi-investax <thi@investax.io>
    Co-authored-by: thi-investax <118243127+thi-investax@users.noreply.github.com>

--- src/chains.ts
+++ src/chains.ts
@@ -1,5 +1,6 @@
 import type { AddEthereumChainParameter } from '@web3-react/types'
 import { SupportedChainId } from 'constants/chains'
+import { isProd } from 'utils/isEnvMode'
 
 const ETH: AddEthereumChainParameter['nativeCurrency'] = {
   name: 'Ether',
@@ -155,31 +156,41 @@ export const URLS: { [chainId: number]: string[] } = Object.keys(CHAINS).reduce<
   {}
 )
 
-export const checkIsTestnet = (chainId: number) => {
-  return [SupportedChainId.AMOY, SupportedChainId.BASE_SEPOLIA].includes(chainId)
-}
-
 export enum NetworkName {
   BASE = 'base',
   POLYGON = 'polygon',
 }
 
-export const checkWrongChain = (chainId: any, network: string) => {
-  let isWrongChain = false
-  let expectChain = null
+const Chains = {
+  // network name : tesnet, mainnet
+  [NetworkName.BASE]: [SupportedChainId.BASE_SEPOLIA, SupportedChainId.BASE],
+  [NetworkName.POLYGON]: [SupportedChainId.AMOY, SupportedChainId.MATIC],
+}
 
-  if ([SupportedChainId.AMOY, SupportedChainId.MATIC].includes(chainId) && network === NetworkName.BASE) {
-    isWrongChain = true
-    expectChain = checkIsTestnet(chainId) ? SupportedChainId.BASE_SEPOLIA : SupportedChainId.BASE
+export const checkWrongChain = (
+  chainId: any,
+  network: string
+): {
+  isWrongChain: boolean
+  expectChain: number | null
+} => {
+  const expectedChains = Chains[network as NetworkName] || [] // Default to an empty array if network is not found
+  if (!expectedChains.length) {
+    return {
+      isWrongChain: true,
+      expectChain: null,
+    }
   }
-
-  if ([SupportedChainId.BASE, SupportedChainId.BASE_SEPOLIA].includes(chainId) && network === NetworkName.POLYGON) {
-    isWrongChain = true
-    expectChain = checkIsTestnet(chainId) ? SupportedChainId.AMOY : SupportedChainId.MATIC
+  const [testChain, mainChain] = expectedChains
+  if (isProd) {
+    return {
+      isWrongChain: chainId != mainChain,
+      expectChain: mainChain,
+    }
   }
 
   return {
-    isWrongChain,
-    expectChain,
+    isWrongChain: chainId != testChain,
+    expectChain: testChain,
   }
 }
