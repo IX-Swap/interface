e2af379a173a94aa99a83b2637e1e6fe8e1fd1c3
leonthuongto <leon@ixswap.io>
2024-07-24 16:21:26 +0700

    Fix Approve button on Accreditation page

--- src/components/AdminAccreditationTable/CustodianStatus.tsx
+++ src/components/AdminAccreditationTable/CustodianStatus.tsx
@@ -10,6 +10,7 @@ import { ReactComponent as CheckIcon } from 'assets/images/CheckOutline.svg'
 import { RejectModal } from './RejectModal'
 import { getStatusIcon } from './utils'
 import { MoreActions } from './MoreActions'
+import { LoaderThin } from 'components/Loader/LoaderThin'
 
 interface Props {
   status: string
@@ -22,14 +23,20 @@ export const CustodianStatus = ({ status, id, custodian, searchValue }: Props) =
   const approveAccreditation = useApproveAccreditation()
 
   const [isModalOpen, handleIsModalOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const openModal = () => handleIsModalOpen(true)
   const closeModal = () => handleIsModalOpen(false)
 
   const approve = async () => {
     try {
+      setLoading(true)
       await approveAccreditation(id, searchValue)
-    } catch (e) {}
+    } catch (e) {
+      console.log('Error approving accreditation', e)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
@@ -39,8 +46,14 @@ export const CustodianStatus = ({ status, id, custodian, searchValue }: Props) =
           <>
             <RejectModal searchValue={searchValue} isModalOpen={isModalOpen} closeModal={closeModal} id={id} />
             <ButtonsContainer>
-              <CheckIcon onClick={approve} />
-              <CrossIcon onClick={openModal} />
+              {loading ? (
+                <LoaderThin size={20} />
+              ) : (
+                <>
+                  <CheckIcon onClick={approve} />
+                  <CrossIcon onClick={openModal} />
+                </>
+              )}
             </ButtonsContainer>
           </>
         )}
@@ -49,7 +62,7 @@ export const CustodianStatus = ({ status, id, custodian, searchValue }: Props) =
         {status !== AccreditationStatusEnum.PENDING && (
           <img src={getStatusIcon(status)} alt="icon" width="20px" height="20px" />
         )}
-        <EllipsisText style={{fontSize: '12px'}}>{custodian}</EllipsisText>
+        <EllipsisText style={{ fontSize: '12px' }}>{custodian}</EllipsisText>
         <MoreActions id={id} searchValue={searchValue} />
       </div>
     </Container>
