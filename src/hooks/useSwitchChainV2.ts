9f89f5275ba3fa068e7cb2b8b7d00c39d488642b
leonthuongto <leon@ixswap.io>
2024-07-29 13:15:49 +0700

    Reformat style

--- src/hooks/useSwitchChainV2.ts
+++ src/hooks/useSwitchChainV2.ts
@@ -1,18 +1,17 @@
-import { useWeb3React } from "@web3-react/core";
-import { WalletConnect } from "@web3-react/walletconnect-v2";
-import { getAddChainParameters } from "chains";
-
+import { useWeb3React } from '@web3-react/core'
+import { WalletConnect } from '@web3-react/walletconnect-v2'
+import { getAddChainParameters } from 'chains'
 
 export function useSwitchChain() {
-  const { connector } = useWeb3React();
+  const { connector } = useWeb3React()
 
   const switchChain = async (desiredChain: number) => {
     if (connector instanceof WalletConnect) {
-      await connector.activate(desiredChain === -1 ? undefined : desiredChain);
+      await connector.activate(desiredChain === -1 ? undefined : desiredChain)
     } else {
-      await connector.activate(desiredChain === -1 ? undefined : getAddChainParameters(desiredChain));
+      await connector.activate(desiredChain === -1 ? undefined : getAddChainParameters(desiredChain))
     }
-  };
+  }
 
-  return switchChain;
+  return switchChain
 }
