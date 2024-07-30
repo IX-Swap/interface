1f4c0549c341f0b1bb911d4c103d76887f2837a8
kapildave07 <kapil@investax.io>
2024-07-30 17:11:49 +0530

    create payout form background and heders and cards updated

--- src/pages/CreatePayoutEvent/Info.tsx
+++ src/pages/CreatePayoutEvent/Info.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import { Trans } from '@lingui/macro'
-
 import { TYPE } from 'theme'
-
-import { FormCard } from './styleds'
+import { PayoutHeaderCard } from './styleds'
 
 export const Info = () => {
   return (
-    <FormCard marginBottom="32px">
-      <TYPE.title6 marginBottom="8px" style={{ textTransform: 'uppercase' }}>
-        <Trans>What is a payout event?</Trans>
-      </TYPE.title6>
-      <TYPE.body1>
-        <Trans>A payout event allows issuers to distribute tokens to qualified token holders.</Trans>
-      </TYPE.body1>
-    </FormCard>
+    <PayoutHeaderCard marginTop='16px' marginBottom="32px">
+      <TYPE.body style={{ fontWeight: '600' }}>
+        What is a payout event?
+      </TYPE.body>
+      <TYPE.description8>
+        A payout event allows issuers to distribute tokens to qualified token holders.
+      </TYPE.description8>
+    </PayoutHeaderCard>
   )
 }
