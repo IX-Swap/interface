bedd980315015735ba06c5af00951936701559e5
kapildave07 <kapil@investax.io>
2024-07-31 09:47:01 +0530

    Payout Event section UI upgraded

--- src/pages/CreatePayoutEvent/PayoutType.tsx
+++ src/pages/CreatePayoutEvent/PayoutType.tsx
@@ -29,62 +29,66 @@ export const PayoutType: FC<Props> = ({ onValueChange, availableForEditing }) =>
   }
 
   return (
-    <Box marginBottom="24px">
-      <Label
-        marginBottom="8px"
-        label={`Payout Type`}
-        required={availableForEditing.includes('type')}
-        tooltipText="Select the type of payout applicable for this payout event."
-      />
-      {!availableForEditing.includes('type') ? (
-        <div>{values.type || values.otherType}</div>
-      ) : (
-        <>
-          <Card style={{ marginBottom: 8 }}>
+    <>
+      <Box marginBottom="24px">
+        {!availableForEditing.includes('type') ? (
+          <div>{values.type || values.otherType}</div>
+        ) : (
+          <>
             {payoutTypes.map(({ id, label }) => (
-              <Checkbox
-                key={`payout-type-${id}`}
-                scaleSize={1.1}
-                buttonStyles={{ marginRight: 32 }}
-                isRadio
-                label={label}
-                onClick={() => onTypeChange(label)}
-                checked={values.type === label}
+              <Card key={`payout-type-card-${id}`}>
+                <Checkbox
+                  key={`payout-type-${id}`}
+                  scaleSize={0.8}
+                  buttonStyles={{ marginRight: 32 }}
+                  isRadio
+                  label={label}
+                  onClick={() => onTypeChange(label)}
+                  checked={values.type === label}
+                  disabled={!availableForEditing.includes('type')}
+                />
+              </Card>
+            ))}
+            {touched.type && errors.type && (
+              <TYPE.small marginTop="4px" color={'red1'}>
+                {errors.type}
+              </TYPE.small>
+            )}
+            {values.type && (
+              <ExtraInfoCard style={{display: 'flex', gap: '8px', marginBottom: '10px'}}>
+                <TYPE.buttonMuted color={'#292933'}>{description?.split('-')[0]}</TYPE.buttonMuted>
+                <TYPE.buttonMuted color={'#666680'} fontWeight={400}>{description?.split('-')[1]}</TYPE.buttonMuted>
+              </ExtraInfoCard>
+            )}
+            {values.type === 'Other' && (
+              <TextInput
+                style={{ marginTop: 12 }}
+                placeholder="Write payout type"
+                onChange={(e: any) => onValueChange('otherType', e.currentTarget.value)}
+                value={values.otherType}
+                required
+                error={touched.otherType ? errors.otherType : ''}
                 disabled={!availableForEditing.includes('type')}
               />
-            ))}
-          </Card>
-          {touched.type && errors.type && (
-            <TYPE.small marginTop="4px" color={'red1'}>
-              {errors.type}
-            </TYPE.small>
-          )}
-          {values.type && (
-            <ExtraInfoCard>
-              <TYPE.buttonMuted opacity="50%">{description}</TYPE.buttonMuted>
-            </ExtraInfoCard>
-          )}
-          {values.type === 'Other' && (
-            <TextInput
-              style={{ marginTop: 12 }}
-              placeholder="Write payout type"
-              onChange={(e: any) => onValueChange('otherType', e.currentTarget.value)}
-              value={values.otherType}
-              required
-              error={touched.otherType ? errors.otherType : ''}
-              disabled={!availableForEditing.includes('type')}
-            />
-          )}
-        </>
-      )}
-    </Box>
+            )}
+          </>
+        )}
+      </Box>
+    </>
   )
 }
 
 const Card = styled.div`
-  background: ${({ theme }) => theme.bg19};
   padding: 12px 16px;
-  border-radius: 20px;
-  display: flex;
+  border-radius: 8px;
+  display: inline-flex;
   align-items: center;
-`
+  border: 1px solid #e6e6ff;
+  margin-right: 30px;
+  width: 180px;
+  margin-bottom: 20px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
