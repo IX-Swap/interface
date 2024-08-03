5a7c05d2b7007988fd6d5eeff876ee9a5b508cf6
leonthuongto <leon@ixswap.io>
2024-08-03 08:35:52 +0700

    Upgrade RPC private

--- src/components/Web3Provider/constants.ts
+++ src/components/Web3Provider/constants.ts
@@ -1,8 +1,11 @@
+import { baseSepolia, base, mainnet, polygon, polygonAmoy,  Chain as WagmiChain } from 'wagmi/chains'
+
 import GNOSIS_ICON from 'assets/wallets/gnosis.png'
 import COINBASE_ICON from 'assets/wallets/coinbase-icon.svg'
 import METAMASK_ICON from 'assets/wallets/metamask-icon.svg'
 import WALLET_CONNECT_ICON from 'assets/wallets/walletconnect-icon.svg'
 import { atomWithStorage, useAtomValue } from 'jotai/utils'
+import { ChainId } from 'types/chains'
 
 export const CONNECTION = {
   WALLET_CONNECT_CONNECTOR_ID: 'walletConnect',
@@ -27,3 +30,49 @@ export const recentConnectorIdAtom = atomWithStorage<string | undefined>('recent
 export function useRecentConnectorId() {
   return useAtomValue(recentConnectorIdAtom)
 }
+
+const getAlchemyUrlFor = (network: string) =>
+  process.env.REACT_APP_ALCHEMY_KEY ? `https://${network}.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}` : ''
+
+export const WAGMI_CHAIN_INFO: Record<ChainId, WagmiChain> = {
+  [ChainId.Mainnet]: {
+    ...mainnet,
+    rpcUrls: {
+      default: {
+        http: [getAlchemyUrlFor('eth-mainnet'), 'https://cloudflare-eth.com'],
+      },
+    },
+  },
+  [ChainId.Base]: {
+    ...base,
+    rpcUrls: {
+      default: {
+        http: [getAlchemyUrlFor('base-mainnet'), 'https://mainnet.base.org'],
+      },
+    },
+  },
+  [ChainId.Polygon]: {
+    ...polygon,
+    rpcUrls: {
+      default: {
+        http: [getAlchemyUrlFor('polygon-mainnet'), 'https://polygon-rpc.com'],
+      },
+    },
+  },
+  [ChainId.Amoy]: {
+    ...polygonAmoy,
+    rpcUrls: {
+      default: {
+        http: [getAlchemyUrlFor('polygon-amoy'), 'https://rpc-amoy.polygon.technology'],
+      },
+    },
+  },
+  [ChainId.BaseSepolia]: {
+    ...baseSepolia,
+    rpcUrls: {
+      default: {
+        http: [getAlchemyUrlFor('base-sepolia'), 'https://sepolia.base.org'],
+      },
+    },
+  },
+} as any;
