a673c859daa1b747c1410c8cc4c071a4828af706
leonthuongto <leon@ixswap.io>
2024-08-04 09:24:22 +0700

    Fix performace

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -1,17 +1,17 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useMemo } from 'react'
 import { WagmiProvider } from 'wagmi'
 import { QueryClientProvider } from '@tanstack/react-query'
 
-import { queryClient, wagmiConfig } from './wagmi'
+import { queryClient, createWagmiConfig } from './wagmi'
 import { ConnectionProvider } from 'hooks/useConnect'
 
 export default function Web3Provider({ children }: { children: ReactNode }) {
+  const wagmiConfig = useMemo(() => createWagmiConfig(), [])
+
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <ConnectionProvider>
-          {children}
-        </ConnectionProvider>
+        <ConnectionProvider>{children}</ConnectionProvider>
       </QueryClientProvider>
     </WagmiProvider>
   )
