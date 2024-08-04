0ffbe8357d3fd405e6a55885ea106c321bac1d32
leonthuongto <leon@ixswap.io>
2024-08-04 09:24:22 +0700

    Fix performace

--- src/components/Web3Provider/wagmi.ts
+++ src/components/Web3Provider/wagmi.ts
@@ -1,43 +1,39 @@
 import { QueryClient } from '@tanstack/react-query'
 import { createClient } from 'viem'
 import { createConfig, http } from 'wagmi'
-import { connect } from 'wagmi/actions'
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+import { coinbaseWallet, walletConnect } from 'wagmi/connectors'
 
 import { injectedWithFallback } from './injectedWithFallback'
 import { WC_PARAMS } from './walletConnect'
-import { ENV_SUPPORTED_TGE_CHAINS } from 'constants/addresses'
-import { WAGMI_CHAIN_INFO } from './constants'
+import { CHAINS, CLIENT_CONFIG, transports } from './constants'
 
-
-// @ts-ignore
-export const wagmiConfig = createConfig({
+export function createWagmiConfig() {
   // @ts-ignore
-  chains: [...ENV_SUPPORTED_TGE_CHAINS.map((chain) => WAGMI_CHAIN_INFO[chain])],
-  connectors: [
-    injectedWithFallback(),
-    walletConnect(WC_PARAMS),
-    coinbaseWallet({
-      appName: 'IX Swap',
-      appLogoUrl: 'https://app.ixswap.io/favicon.png',
-      reloadOnDisconnect: false,
-      enableMobileWalletLink: true,
-    }),
-  ],
-  // @ts-ignore
-  client({ chain }) {
-    return createClient({
-      chain,
-      batch: { multicall: true },
-      pollingInterval: 12_000,
-      transport: http(chain.rpcUrls.default.http[0]),
-    })
-  },
-})
+  return createConfig({
+    chains: CHAINS,
+    ssr: true,
+    syncConnectedChain: true,
+    transports,
+    ...CLIENT_CONFIG,
+    connectors: [
+      injectedWithFallback(),
+      walletConnect(WC_PARAMS),
+      coinbaseWallet({
+        appName: 'IX Swap',
+        appLogoUrl: 'https://app.ixswap.io/favicon.png',
+        reloadOnDisconnect: false,
+        enableMobileWalletLink: true,
+      }),
+    ],
+    // @ts-ignore
+    client({ chain }) {
+      return createClient({
+        chain,
+        ...CLIENT_CONFIG,
+        transport: http(chain.rpcUrls.default.http[0]),
+      })
+    },
+  })
+}
 
 export const queryClient = new QueryClient()
-
-// Automatically connect if running in Cypress environment
-if ((window as any).Cypress?.eagerlyConnect) {
-  connect(wagmiConfig, { connector: injected() })
-}
