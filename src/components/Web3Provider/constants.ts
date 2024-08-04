1a387d4310d09291723881ff3b78f7a750186b3b
leonthuongto <leon@ixswap.io>
2024-08-04 09:24:22 +0700

    Fix performace

--- src/components/Web3Provider/constants.ts
+++ src/components/Web3Provider/constants.ts
@@ -1,4 +1,6 @@
-import { baseSepolia, base, mainnet, polygon, polygonAmoy,  Chain as WagmiChain } from 'wagmi/chains'
+import { baseSepolia, base, mainnet, polygon, polygonAmoy, Chain } from 'wagmi/chains'
+import { fallback, http } from 'wagmi'
+import { Transport } from 'viem'
 
 import GNOSIS_ICON from 'assets/wallets/gnosis.png'
 import COINBASE_ICON from 'assets/wallets/coinbase-icon.svg'
@@ -34,45 +36,45 @@ export function useRecentConnectorId() {
 const getAlchemyUrlFor = (network: string) =>
   process.env.REACT_APP_ALCHEMY_KEY ? `https://${network}.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}` : ''
 
-export const WAGMI_CHAIN_INFO: Record<ChainId, WagmiChain> = {
-  [ChainId.Mainnet]: {
-    ...mainnet,
-    rpcUrls: {
-      default: {
-        http: [getAlchemyUrlFor('eth-mainnet'), 'https://cloudflare-eth.com'],
-      },
+export const CHAINS: [Chain, ...Chain[]] = [baseSepolia, base, mainnet, polygon, polygonAmoy]
+
+export const CLIENT_CONFIG = {
+  batch: {
+    multicall: {
+      batchSize: 1024 * 200,
+      wait: 16,
     },
   },
-  [ChainId.Base]: {
-    ...base,
-    rpcUrls: {
-      default: {
-        http: [getAlchemyUrlFor('base-mainnet'), 'https://mainnet.base.org'],
-      },
-    },
-  },
-  [ChainId.Polygon]: {
-    ...polygon,
-    rpcUrls: {
-      default: {
-        http: [getAlchemyUrlFor('polygon-mainnet'), 'https://polygon-rpc.com'],
-      },
-    },
-  },
-  [ChainId.Amoy]: {
-    ...polygonAmoy,
-    rpcUrls: {
-      default: {
-        http: [getAlchemyUrlFor('polygon-amoy'), 'https://rpc-amoy.polygon.technology'],
-      },
-    },
-  },
-  [ChainId.BaseSepolia]: {
-    ...baseSepolia,
-    rpcUrls: {
-      default: {
-        http: [getAlchemyUrlFor('base-sepolia'), 'https://sepolia.base.org'],
-      },
-    },
-  },
-} as any;
+  pollingInterval: 6_000,
+}
+
+const PUBLIC_MAINNET = 'https://ethereum.publicnode.com'
+
+export const PUBLIC_NODES = {
+  [ChainId.Mainnet]: [getAlchemyUrlFor('eth-mainnet'), 'https://cloudflare-eth.com'].filter(Boolean),
+  [ChainId.Polygon]: [getAlchemyUrlFor('polygon-mainnet'), 'https://polygon-rpc.com'].filter(Boolean),
+  [ChainId.Amoy]: [getAlchemyUrlFor('polygon-amoy'), 'https://rpc-amoy.polygon.technology/'].filter(Boolean),
+  [ChainId.Base]: [getAlchemyUrlFor('base-mainnet'), 'https://basechain.infura.io'],
+  [ChainId.BaseSepolia]: [getAlchemyUrlFor('base-sepolia'), 'https://sepolia.basechain.infura.io'],
+} as any
+
+export const transports = CHAINS.reduce((ts, chain) => {
+  let httpStrings: string[] | readonly string[] = []
+
+  if (process.env.NODE_ENV === 'test' && chain.id === mainnet.id) {
+    httpStrings = [PUBLIC_MAINNET]
+  } else {
+    httpStrings = PUBLIC_NODES[chain.id] ? PUBLIC_NODES[chain.id] : []
+  }
+
+  if (ts) {
+    return {
+      ...ts,
+      [chain.id]: fallback(httpStrings.map((t: any) => http(t))),
+    }
+  }
+
+  return {
+    [chain.id]: fallback(httpStrings.map((t: any) => http(t))),
+  }
+}, {} as Record<number, Transport>)
