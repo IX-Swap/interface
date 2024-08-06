98daae376e618653a51a3c2b2a82385bd72694d4
leonthuongto <leon@ixswap.io>
2024-08-06 14:00:38 +0700

    Integrate rainbowkit

--- src/components/Web3Provider/wagmi.ts
+++ src/components/Web3Provider/wagmi.ts
@@ -2,35 +2,48 @@ import { QueryClient } from '@tanstack/react-query'
 import { createClient } from 'viem'
 import { createConfig, http } from 'wagmi'
 import { coinbaseWallet, walletConnect } from 'wagmi/connectors'
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
 
 import { injectedWithFallback } from './injectedWithFallback'
 import { WC_PARAMS } from './walletConnect'
 import { CHAINS, CLIENT_CONFIG, PUBLIC_NODES, transports } from './constants'
 
 export function createWagmiConfig() {
-  // @ts-ignore
-  return createConfig({
+  const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: WC_PARAMS.projectId,
     chains: CHAINS,
-    ssr: true,
-    syncConnectedChain: true,
-    transports,
-    ...CLIENT_CONFIG,
-    connectors: [
-      injectedWithFallback(),
-      walletConnect(WC_PARAMS),
-      coinbaseWallet({
-        appName: 'IX Swap',
-        appLogoUrl: 'https://app.ixswap.io/favicon.png',
-      }),
-    ],
-    client({ chain }: any) {
-      return createClient({
-        chain,
-        ...CLIENT_CONFIG,
-        transport: http(PUBLIC_NODES[chain.id][0] || chain.rpcUrls.default.http[0]),
-      })
-    },
-  })
+    transports
+  });
+
+  return config;
+
+  // @ts-ignore
+  // return createConfig({
+  //   chains: CHAINS,
+  //   ssr: true,
+  //   syncConnectedChain: true,
+  //   transports,
+  //   ...CLIENT_CONFIG,
+  //   connectors: [
+  //     injectedWithFallback(),
+  //     walletConnect(WC_PARAMS),
+  //     coinbaseWallet({
+  //       appName: 'IX Swap',
+  //       appLogoUrl: 'https://app.ixswap.io/favicon.png',
+  //     }),
+  //   ],
+  //   client({ chain }: any) {
+  //     return createClient({
+  //       chain,
+  //       ...CLIENT_CONFIG,
+  //       transport: http(PUBLIC_NODES[chain.id][0] || chain.rpcUrls.default.http[0]),
+  //     })
+  //   },
+  // })
 }
 
 export const queryClient = new QueryClient()
