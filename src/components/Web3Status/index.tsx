5a8ce0b87307d0f17ce6001f609d038c822a1488
leonthuongto <leon@ixswap.io>
2024-08-07 15:03:55 +0700

    Handle chain and icon

--- src/components/Web3Status/index.tsx
+++ src/components/Web3Status/index.tsx
@@ -17,6 +17,7 @@ import { useETHBalances } from 'state/wallet/hooks'
 import { useNativeCurrency } from 'hooks/useNativeCurrencyName'
 import { formatAmount } from 'utils/formatCurrencyAmount'
 import { useAccount } from 'hooks/useAccount'
+import { CONNECTOR_ICON_OVERRIDE_MAP } from 'components/Web3Provider/constants'
 
 function newTransactionsFirst(a: TransactionDetails, b: TransactionDetails) {
   return b.addedTime - a.addedTime
@@ -24,7 +25,7 @@ function newTransactionsFirst(a: TransactionDetails, b: TransactionDetails) {
 
 export function StatusIcon() {
   const { connector } = useAccount()
-  const icon = connector ? connector?.icon : undefined
+  const icon = connector ? CONNECTOR_ICON_OVERRIDE_MAP[connector?.id] ?? connector?.icon : undefined
 
   return (
     <>
