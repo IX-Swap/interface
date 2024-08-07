caa8336576887b09e00dc37b6d5b1f43eef1d1b1
leonthuongto <leon@ixswap.io>
2024-08-07 09:57:01 +0700

    remove unuse

--- src/components/Web3Provider/wagmi.ts
+++ src/components/Web3Provider/wagmi.ts
@@ -1,20 +1,39 @@
 import { QueryClient } from '@tanstack/react-query'
-import {
-  getDefaultConfig,
-} from '@rainbow-me/rainbowkit';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import * as wallets from '@rainbow-me/rainbowkit/wallets'
 
-import { WC_PARAMS } from './walletConnect'
 import { CHAINS, transports } from './constants'
+
+const WALLET_CONNECT_PROJECT_ID = <string>process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID
 
 export function createWagmiConfig() {
   const config = getDefaultConfig({
-    appName: 'My RainbowKit App',
-    projectId: WC_PARAMS.projectId,
+    appName: 'IXSwap',
+    projectId: WALLET_CONNECT_PROJECT_ID,
+    wallets: [
+      {
+        groupName: 'Popular',
+        wallets: [wallets.coinbaseWallet, wallets.metaMaskWallet, wallets.walletConnectWallet],
+      },
+      {
+        groupName: 'Others',
+        wallets: [
+          wallets.trustWallet,
+          wallets.phantomWallet,
+          wallets.ledgerWallet,
+          wallets.braveWallet,
+          wallets.uniswapWallet,
+          wallets.safepalWallet,
+          wallets.rainbowWallet,
+          wallets.injectedWallet,
+        ],
+      },
+    ],
     chains: CHAINS,
-    transports
-  });
+    transports,
+  })
 
-  return config;
+  return config
 }
 
 export const queryClient = new QueryClient()
