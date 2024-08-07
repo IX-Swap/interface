d8f97a9057b8ec5b421ffb82ed3964c9e103f703
leonthuongto <leon@ixswap.io>
2024-08-07 09:57:01 +0700

    remove unuse

--- src/components/Web3Provider/walletConnect.ts
+++
@@ -1,9 +0,0 @@
-if (process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID === undefined) {
-  throw new Error('REACT_APP_WALLET_CONNECT_PROJECT_ID must be a defined environment variable')
-}
-const WALLET_CONNECT_PROJECT_ID = <string>process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID
-
-export const WC_PARAMS = {
-  projectId: WALLET_CONNECT_PROJECT_ID,
-  showQrModal: true,
-}
