c1fcd01dff8d67083c3f008924d6322172fafce6
leonthuongto <leon@ixswap.io>
2024-08-13 15:33:23 +0700

    Fix case not connected

--- src/components/NotAvailablePage/index.tsx
+++ src/components/NotAvailablePage/index.tsx
@@ -1,25 +1,19 @@
 import React from 'react'
-import { Trans } from '@lingui/macro'
 import { useLocation } from 'react-router-dom'
-import { useCookies } from 'react-cookie'
-import { Text } from 'rebass'
 import { useWeb3React } from 'hooks/useWeb3React'
 
 import { CHAIN_INFO, SupportedChainId } from 'constants/chains'
-import { PinnedContentButton } from 'components/Button'
 import { ENV_SUPPORTED_TGE_CHAINS } from 'constants/addresses'
-import { useWhitelabelState } from 'state/whitelabel/hooks'
-import { Container, Title, Info, NetworksRow, NetworkCard, InfoRows, ConnectWalletContainer } from './styled'
+import { Container, Title, Info, NetworksRow, NetworkCard, InfoRows } from './styled'
 import useSelectChain from 'hooks/useSelectChain'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
+import ConnectWalletCard from './ConnectWalletCard'
 
 export const NotAvailablePage = () => {
   const { openConnectModal } = useConnectModal()
   const selectChain = useSelectChain()
   const { chainId } = useWeb3React()
   const { pathname } = useLocation()
-  const [cookies] = useCookies(['announcementsSeen'])
-  const { config } = useWhitelabelState()
 
   const farming = ['/vesting', '/staking'].includes(pathname)
 
@@ -30,56 +24,7 @@ export const NotAvailablePage = () => {
   }
 
   if (openConnectModal) {
-    return (
-      <ConnectWalletContainer hasAnnouncement={!cookies.announcementsSeen}>
-        <Text>
-          <Trans>Welcome to {config?.name || 'IX Swap'}</Trans>
-        </Text>
-        <div>
-          Please Connect <br /> your Wallet to use <br /> the Application.
-        </div>
-        {openConnectModal && (
-          <PinnedContentButton style={{ boxShadow: '0px 16px 16px 0px #6666FF21' }} onClick={openConnectModal}>
-            <Text className="connect-wallet-button">
-              <Trans>Connect Wallet</Trans>
-            </Text>
-          </PinnedContentButton>
-        )}
-
-        {config?.isIxSwap ? (
-          <span>
-            While your wallet is not connected, you can see our New <br />
-            <a
-              style={{ color: '#6666FF', textDecoration: 'none' }}
-              href="https://ixswap.defiterm.io/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Staking Program
-            </a>
-            ,&nbsp;
-            <a
-              style={{ color: '#6666FF', textDecoration: 'none' }}
-              href="https://ixswap.defiterm.io/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Liquidity Mining on Polygon
-            </a>
-            &nbsp;and&nbsp; <br />
-            <a
-              style={{ color: '#6666FF', textDecoration: 'none' }}
-              href="https://app.uniswap.org/#/add/v2/ETH/0x73d7c860998CA3c01Ce8c808F5577d94d545d1b4?chain=polygon"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Liquidity Mining on Ethereum
-            </a>
-            .
-          </span>
-        ) : null}
-      </ConnectWalletContainer>
-    )
+    return <ConnectWalletCard />
   }
 
   const chains = ENV_SUPPORTED_TGE_CHAINS || [SupportedChainId.BASE]
