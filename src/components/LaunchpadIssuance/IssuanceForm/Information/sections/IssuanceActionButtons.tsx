ac7c2f1e7f8faf54ac1d19446a38264f2a3d0228
leonthuongto <leon@ixswap.io>
2024-08-13 13:30:35 +0700

    IXSPD1-1649 the issuance form does not change status after admin approves issuance form (#2628)
    * Handle refetch
    
    * Handle refetch
    
    * Add loading
    
    * remove logs

--- src/components/LaunchpadIssuance/IssuanceForm/Information/sections/IssuanceActionButtons.tsx
+++ src/components/LaunchpadIssuance/IssuanceForm/Information/sections/IssuanceActionButtons.tsx
@@ -12,6 +12,8 @@ import { useRole } from 'state/user/hooks'
 import { FormSubmitContainer } from '../../shared/styled'
 import { RequestChangesPopup } from '../../../utils/RequestChangesPopup'
 import { OfferStatus } from 'state/launchpad/types'
+import { LoaderContainer } from 'components/LaunchpadMisc/styled'
+import { Loader } from 'components/LaunchpadOffer/util/Loader'
 
 interface IssuanceButtonsProps {
   onSaveDraft: () => void
@@ -23,6 +25,7 @@ interface IssuanceButtonsProps {
   offerId?: string
   status: OfferStatus
   isReset: boolean
+  refetch?: () => void
 }
 
 export const IssuanceActionButtons = ({
@@ -35,9 +38,14 @@ export const IssuanceActionButtons = ({
   status,
   offerId,
   isReset,
+  refetch,
 }: IssuanceButtonsProps) => {
   const theme = useTheme()
   const { isAdmin } = useRole()
+  const showError = useShowError()
+  const { approve, reject, requestChanges } = useReviewOffer(offerId)
+  const showSuccess = useShowSuccess()
+
   const [showApprove, setShowApprove] = useState(false)
   const [showUpdate, setShowUpdate] = useState(false)
   const [showReject, setShowReject] = useState(false)
@@ -47,36 +55,47 @@ export const IssuanceActionButtons = ({
   const [reasonRejected, setReasonRejected] = useState('')
   const [changesRequested, setChangesRequested] = useState('')
   const [changesRejected, setChangesRejected] = useState('')
-  const showError = useShowError()
-  const { approve, reject, requestChanges } = useReviewOffer(offerId)
-  const showSuccess = useShowSuccess()
+  const [loading, setLoading] = useState(false)
+
   const onApprove = async () => {
     try {
+      setLoading(true)
       await approve()
+      await refetch?.()
       showSuccess('Offer approved successfully')
       setShowApprove(false)
     } catch (e: any) {
       showError(e?.message)
+    } finally {
+      setLoading(false)
     }
   }
   const onReject = async () => {
     try {
+      setLoading(true)
       await reject({ reasonRequested: reasonRejected, changesRequested: changesRejected })
+      await refetch?.()
       showSuccess('Offer rejected successfully')
       setShowConfirmReject(false)
       setShowReject(false)
     } catch (e: any) {
       showError(e?.message)
+    } finally {
+      setLoading(false)
     }
   }
   const onRequestChanges = async () => {
     try {
+      setLoading(true)
       await requestChanges({ reasonRequested, changesRequested })
+      await refetch?.()
       showSuccess('Requested changes for offer successfully')
       setShowConfirmUpdate(false)
       setShowUpdate(false)
     } catch (e: any) {
       showError(e?.message)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -99,6 +118,11 @@ export const IssuanceActionButtons = ({
 
   return (
     <Column style={{ gap: '1rem' }}>
+      {loading && (
+        <LoaderContainer width="100vw" height="100vh">
+          <Loader />
+        </LoaderContainer>
+      )}
       <ConfirmPopup
         isOpen={showApprove}
         onAccept={onApprove}
