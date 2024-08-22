11a510bb9ec572bfd99a485c2c67c4e754dc2202
thi-investax <thi@investax.io>
2024-08-22 08:48:36 +0700

    Update contract addresses and configuration for base mainnet production

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -3,5 +3,6 @@ export const SUBGRAPH_URLS: Record<string, Record<number, string>> = {
     [137]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/lbp-subgraph-polygon/api',
     [80002]: 'https://api.studio.thegraph.com/proxy/71824/ixs-lbp-subgraph-test/version/latest',
     [84532]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/ixs-lbp-base-sepolia/api',
+    [8453]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/lbp-base-mainnet/api',
   },
 }
