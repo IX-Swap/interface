591ab70d935a0176601d97259e455ac192f83b90
thi-investax <thi@investax.io>
2024-09-30 12:54:43 +0700

    Favour injected provider for useWeb3React hook

--- src/hooks/useWeb3React.ts
+++ src/hooks/useWeb3React.ts
@@ -2,18 +2,21 @@ import { useAccount } from 'hooks/useAccount'
 import { useEthersProvider } from 'hooks/useEthersProvider'
 import { useMemo } from 'react'
 import { ChainId } from 'types/chains'
+import { Web3Provider } from '@ethersproject/providers'
 
 export function useWeb3React() {
   const account = useAccount()
   const chainId = account?.chain?.id ?? ChainId.Base
   const provider = useEthersProvider({ chainId })
+  // Fallback to a wallet provider (e.g., MetaMask) if available for sending transactions
+  const walletProvider = typeof window !== 'undefined' && window.ethereum ? new Web3Provider(window.ethereum) : provider
 
   return useMemo(
     () => ({
       account: account.address,
       chainId,
-      provider,
+      provider: walletProvider,
     }),
-    [account.address, account.chainId, provider],
+    [account.address, account.chainId, provider]
   )
 }
