fdc193cbfd7cca5e927ae3733230a5a11ff178a0
Lê Viết Hưng <hung@ixswap.io>
2024-10-02 10:33:55 +0700

    feat: [IXSPD1-2018] enhance UI on payout detail (#2864)
    * update indicator position
    
    * highlight start date button
    
    * styles of token manager payout list

--- src/pages/PayoutItem/Timeline/TodayIndicator.tsx
+++ src/pages/PayoutItem/Timeline/TodayIndicator.tsx
@@ -5,61 +5,35 @@ import { MEDIA_WIDTHS, TYPE } from 'theme'
 
 interface Props {
   offset?: string
-  overlay: boolean
   isTodayStartDate: boolean
 }
 
-export const TodayIndicator: FC<Props> = ({ offset, overlay, isTodayStartDate }) => {
+export const TodayIndicator: FC<Props> = ({ offset, isTodayStartDate }) => {
   const theme = useContext(ThemeContext)
   return (
-    <Container offset={offset} overlay={overlay} isTodayStartDate={isTodayStartDate}>
-      <Label overlay={overlay}>
+    <Container offset={offset} isTodayStartDate={isTodayStartDate}>
+      <Label>
         <TYPE.main1 sx={{ color: theme.bg26 }}>
           <Trans>{`Today`}</Trans>
         </TYPE.main1>
       </Label>
-      <Indicator overlay={overlay} isTodayStartDate={isTodayStartDate}>
-        <Bullet overlay={overlay} isTodayStartDate={isTodayStartDate} />
-      </Indicator>
+      <BulletWrapper>
+        <Bullet />
+      </BulletWrapper>
     </Container>
   )
 }
 
-const Label = styled.div<{ overlay?: boolean }>`
-  ${({ overlay }) =>
-    overlay &&
-    css`
-      @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
-        display: block;
-      }
-    `}
-`
+const Label = styled.div``
 
-const Indicator = styled.div<{ overlay?: boolean; isTodayStartDate: boolean }>`
-  display: flex;
-  flex-direction: ${({ overlay }) => (overlay ? 'column-reverse' : 'column')};
-  align-items: center;
-
-  @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
-    flex-direction: ${({ isTodayStartDate }) => (isTodayStartDate ? 'row-reverse' : 'row')};
-  }
-  ${({ overlay, isTodayStartDate }) =>
-    overlay &&
-    css`
-      flex-direction: column-reverse;
-      @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
-        flex-direction: ${isTodayStartDate ? 'row-reverse' : 'row'};
-      }
-    `};
-`
-
-const Container = styled.div<{ offset?: string; overlay?: boolean; isTodayStartDate: boolean }>`
+const Container = styled.div<{ offset?: string; isTodayStartDate: boolean }>`
+  z-index: 1;
   position: absolute;
   left: ${({ offset }) => offset};
   display: flex;
   flex-direction: column;
   align-items: center;
-  top: -22px;
+  top: -23px;
   gap: 12px;
   height: 48px;
   transform: translateX(-50%);
@@ -75,10 +49,12 @@ const Container = styled.div<{ offset?: string; overlay?: boolean; isTodayStartD
   }
 `
 
-const Bullet = styled.div<{ overlay?: boolean; isTodayStartDate: boolean }>`
-  width: 8px;
-  height: 8px;
+const BulletWrapper = styled.div``
+
+const Bullet = styled.div`
+  width: 10px;
+  height: 10px;
   background: ${({ theme }) => theme.bg26};
   border-radius: 100%;
-  transform: ${({ overlay }) => (overlay ? 'unset' : 'translate(0px,-50%)')};
+  border: 2px solid ${({ theme }) => theme.bg1};
 `
