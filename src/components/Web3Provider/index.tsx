3f2f03dc5a34452b7a5496053685657cad971008
leonthuongto <leon@ixswap.io>
2024-10-07 16:28:58 +0700

    Fix reload disconnect

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -11,10 +11,6 @@ import '@rainbow-me/rainbowkit/styles.css'
 export default function Web3Provider({ children }: { children: ReactNode }) {
   const wagmiConfig = useMemo(() => createWagmiConfig(), [])
 
-  useEffect(() => {
-    indexedDB?.deleteDatabase('WALLET_CONNECT_V2_INDEXED_DB')
-  }, [])
-
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
