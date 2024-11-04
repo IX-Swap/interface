b645924cb1b8ab50814bc0e1c1f3d3aff0cf37d8
leonthuongto <leon@ixswap.io>
2024-11-04 08:17:28 +0700

    Add page  create pool

---
+++ src/pages/DexV2/Pool/components/SelectTokenModal.tsx
@@ -0,0 +1,158 @@
+import Portal from '@reach/portal'
+import React from 'react'
+import styled from 'styled-components'
+
+import TextInput from './TextInput'
+import { CenteredFixed } from 'components/LaunchpadMisc/styled'
+import { ReactComponent as CloseIcon } from 'assets/images/dex-v2/close.svg'
+import EthIcon from 'assets/images/dex-v2/eth.svg'
+import PolIcon from 'assets/images/dex-v2/pol.svg'
+
+interface SelectTokenModalProps {
+  // tokens: string[];
+  // onSelect: (token: string) => void;
+  onClose: () => void
+}
+
+const SelectTokenModal: React.FC<SelectTokenModalProps> = ({ onClose }) => {
+  return (
+    <Portal>
+      <CenteredFixed width="100vw" height="100vh">
+        <ModalContent>
+          <HeaderModal>
+            <TitleWrapper>
+              <Title>Select a token</Title>
+              <CloseButton onClick={onClose}>
+                <CloseIcon />
+              </CloseButton>
+            </TitleWrapper>
+            <TextInput placeholder="Search by name, symbol or address" />
+          </HeaderModal>
+
+          <BodyModal>
+            <TokenList>
+              <TokenItem>
+                <TokenInfo>
+                  <img src={EthIcon} alt="ETH" />
+                  <TokenDetails>
+                    <TokenSymbol>ETH</TokenSymbol>
+                    <TokenName>Ethereum</TokenName>
+                  </TokenDetails>
+                </TokenInfo>
+                <TokenBalance>3.88</TokenBalance>
+              </TokenItem>
+
+              <TokenItem>
+                <TokenInfo>
+                  <img src={PolIcon} alt="POL" />
+                  <TokenDetails>
+                    <TokenSymbol>POL</TokenSymbol>
+                    <TokenName>Polygon</TokenName>
+                  </TokenDetails>
+                </TokenInfo>
+                <TokenBalance>12,554.22</TokenBalance>
+              </TokenItem>
+            </TokenList>
+          </BodyModal>
+        </ModalContent>
+      </CenteredFixed>
+    </Portal>
+  )
+}
+
+export default SelectTokenModal
+
+const TokenList = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+`
+
+const TokenItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 0;
+  border-bottom: 1px solid #e6e6ff;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`
+
+const TokenInfo = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const TokenDetails = styled.div`
+  margin-left: 8px;
+`
+
+const TokenSymbol = styled.div`
+  font-weight: bold;
+`
+
+const TokenName = styled.div`
+  color: #666;
+`
+
+const TokenBalance = styled.div`
+  font-weight: bold;
+`
+
+const ModalContent = styled.div`
+  background: white;
+  border-radius: 16px;
+  width: 480px;
+`
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`
+
+const Title = styled.div`
+  color: rgba(41, 41, 51, 0.9);
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.6px;
+`
+
+const CloseButton = styled.div`
+  cursor: pointer;
+  color: rgba(41, 41, 51, 0.5);
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.42px;
+`
+
+const HeaderModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+  align-self: stretch;
+  background: #f3f3ff;
+  padding: 32px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+`
+
+const BodyModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 16px 32px;
+  height: 560px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+`
