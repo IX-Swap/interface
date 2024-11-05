744092e174040a209a5115e4b4f275c13181a915
leonthuongto <leon@ixswap.io>
2024-11-05 21:26:35 +0700

    Add utils

---
+++ src/lib/config/base-sepolia/contracts.ts
@@ -0,0 +1,28 @@
+import * as baseSepolia from 'assets/data/contracts/base-sepolia.json';
+import { Contracts } from 'lib/config/types';
+
+const contracts: Contracts = {
+  merkleRedeem: '',
+  merkleOrchard: '',
+  multicall: '0xca11bde05977b3631167028862be2a173976ca11',
+  authorizer: baseSepolia.Authorizer,
+  vault: baseSepolia.Vault,
+  weightedPoolFactory: baseSepolia.WeightedPoolFactory,
+  stablePoolFactory: baseSepolia.ComposableStablePoolFactory,
+  lidoRelayer: '',
+  balancerHelpers: baseSepolia.BalancerHelpers,
+  batchRelayer: baseSepolia.BalancerRelayer,
+  gaugeFactory: '',
+  balancerMinter: baseSepolia.BalancerMinter,
+  gaugeController: baseSepolia.GaugeController,
+  tokenAdmin: baseSepolia.BalancerTokenAdmin,
+  veBAL: baseSepolia.VotingEscrow,
+  veDelegationProxy: '',
+  veBALHelpers: '',
+  feeDistributor: '',
+  feeDistributorDeprecated: '',
+  faucet: '',
+  gaugeRewardsHelper: '',
+};
+
+export default contracts;
