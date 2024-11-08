cece5ac5f786ba8ee1be466a609f4f30c4719cf4
leonthuongto <leon@ixswap.io>
2024-11-08 12:12:57 +0700

    Add fetch balance

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -11,7 +11,6 @@ import '@rainbow-me/rainbowkit/styles.css'
 export const wagmiConfig = createWagmiConfig()
 
 export default function Web3Provider({ children }: { children: ReactNode }) {
-
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,4 +1,5 @@
 import { Config, Network } from './types'
+import tokenBaseSepolia from 'assets/data/tokenlists/tokens-84532.json'
 
 import baseSepolia from './base-sepolia'
 
@@ -7,3 +8,8 @@ const config: Record<Network | number, Config> = {
 }
 
 export default config
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const tokenLists: Record<Network | number, any> = {
+  [Network.BASE_SEPOLIA]: tokenBaseSepolia,
+}
