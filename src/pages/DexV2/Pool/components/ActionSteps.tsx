5ab0e2d09907f1d50dec66f3e331dd3003957b12
leonthuongto <leon@ixswap.io>
2024-11-25 22:26:32 +0700

    Update contract

--- src/pages/DexV2/Pool/components/ActionSteps.tsx
+++ src/pages/DexV2/Pool/components/ActionSteps.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
-import { TransactionActionInfo } from 'pages/DexV2/types/transactions'
-import { Step, TransactionAction } from 'pages/DexV2/types'
+import { TransactionActionInfo, TransactionActionState } from 'pages/DexV2/types/transactions'
+import { Step, StepState, TransactionAction } from 'pages/DexV2/types'
+import HorizSteps from './HorizSteps'
+import { BackButton, NavigationButtons, NextButton } from '../Create'
+
+type BalStepAction = {
+  label: string
+  loadingLabel: string
+  pending: boolean
+  step: Step
+  promise: () => Promise<void>
+  isSignAction?: boolean
+}
 
 interface ActionStepsProps {
-  actions: TransactionActionInfo[]
+  requiredActions: TransactionActionInfo[]
   primaryActionType: TransactionAction
   disabled?: boolean
   // override action state loading prop and show
@@ -13,6 +24,7 @@ interface ActionStepsProps {
   // override action state loading label
   // for all steps
   loadingLabel?: string
+  goBack: () => void
 }
 
 type StepAction = {
@@ -24,8 +36,99 @@ type StepAction = {
   isSignAction?: boolean
 }
 
-const ActionSteps: React.FC<ActionStepsProps> = ({ disabled = false, isLoading = false, loadingLabel = '' }) => {
-  return <div className="action-steps"></div>
+const defaultActionState: TransactionActionState = {
+  init: false,
+  confirming: false,
+  confirmed: false,
+  confirmedAt: '',
+}
+
+const ActionSteps: React.FC<ActionStepsProps> = ({
+  disabled = false,
+  isLoading = false,
+  loadingLabel = '',
+  requiredActions,
+  goBack,
+}) => {
+  const [currentActionIndex, setCurrentActionIndex] = useState(0)
+  const [actionStates, setActionStates] = useState<TransactionActionState[]>([])
+  const [actions, setActions] = useState<BalStepAction[]>([])
+
+  function getStepState(actionState: TransactionActionState, index: number): StepState {
+    if (currentActionIndex < index) return StepState.Todo
+    else if (actionState.confirming) return StepState.Pending
+    else if (actionState.init) return StepState.WalletOpen
+    else if (actionState.confirmed) return StepState.Success
+    return StepState.Active
+  }
+
+  async function submit(actionInfo: TransactionActionInfo, state: TransactionActionState): Promise<void> {
+    const { action } = actionInfo;
+    await action();
+    debugger
+    // try {
+    //   state.init = true;
+    //   state.error = null;
+    //   const tx = await action();
+    //   state.init = false;
+    //   state.confirming = true;
+    //   if (currentAction.value?.isSignAction) {
+    //     handleSignAction(state);
+    //     return;
+    //   }
+    //   if (tx) handleTransaction(tx, state, actionInfo);
+    // } catch (error) {
+    //   state.init = false;
+    //   state.confirming = false;
+    //   state.error = formatErrorMsg(error);
+    //   captureBalancerException({
+    //     error: (error as Error)?.cause || error,
+    //     action: props.primaryActionType,
+    //     context: { level: 'fatal' },
+    //   });
+    // }
+  }
+
+  const steps = useMemo((): Step[] => actions.map((action) => action.step), [JSON.stringify(actions)])
+
+  const currentAction = useMemo(
+    (): BalStepAction | undefined => actions[currentActionIndex],
+    [currentActionIndex, JSON.stringify(actions)]
+  )
+
+  useEffect(() => {
+    const newActionStates = requiredActions.map(() => ({
+      ...defaultActionState,
+    }))
+
+    const newActions = requiredActions.map((actionInfo, idx) => {
+      const actionState = newActionStates[idx]
+
+      return {
+        label: actionInfo.label,
+        loadingLabel: actionState.init ? actionInfo.loadingLabel : actionInfo.confirmingLabel,
+        pending: actionState.init || actionState.confirming,
+        isSignAction: actionInfo.isSignAction,
+        promise: submit.bind(null, actionInfo, actionState),
+        step: {
+          tooltip: actionInfo.stepTooltip,
+          state: getStepState(actionState, idx),
+        },
+      }
+    })
+    setActionStates(newActionStates)
+    setActions(newActions)
+  }, [JSON.stringify(requiredActions)])
+
+  console.log('steps', steps)
+  return (
+    <div className="action-steps">
+      {actions.length > 1 ? <HorizSteps steps={steps} /> : null}{' '}
+      <NavigationButtons>
+        <NextButton onClick={() => currentAction?.promise()}>{currentAction?.label}</NextButton>
+      </NavigationButtons>
+    </div>
+  )
 }
 
 export default ActionSteps
--- src/pages/DexV2/Pool/components/CreateActions.tsx
+++ src/pages/DexV2/Pool/components/CreateActions.tsx
@@ -6,6 +6,7 @@ import { usePoolCreationState } from 'state/dexV2/poolCreation/hooks'
 import useTokenApprovalActions, { ApprovalAction } from 'state/dexV2/tokens/hooks/useTokenApprovalActions'
 import { useWeb3React } from 'hooks/useWeb3React'
 import config from 'lib/config'
+import ActionSteps from './ActionSteps'
 
 interface Props {
   tokenAddresses: string[]
@@ -20,26 +21,14 @@ const CreateActions: React.FC<Props> = ({ amounts, tokenAddresses, goBack }) => 
   const { needsSeeding } = usePoolCreationState()
   const { getTokenApprovalActions } = useTokenApprovalActions()
   const { account, chainId } = useWeb3React()
-
-  const networkConfig = config[chainId]
-
-  const [isRestoredTxConfirmed, setIsRestoredTxConfirmed] = useState(false)
-
-  const amountsToApprove = amounts.map((amount, index) => {
-    return {
-      address: tokenAddresses[index],
-      amount,
-    }
-  })
-
-  const actions: TransactionActionInfo[] = [
-    {
-      label: 'Create Pool',
-      loadingLabel: 'Confirm create in wallet',
-      confirmingLabel: 'Confirming...',
-      action: createPool,
-      stepTooltip: `Create ${poolTypeString} pool`,
-    },
+  const initActions: TransactionActionInfo[] = [
+    // {
+    //   label: 'Create Pool',
+    //   loadingLabel: 'Confirm create in wallet',
+    //   confirmingLabel: 'Confirming...',
+    //   action: createPool,
+    //   stepTooltip: `Create ${poolTypeString} pool`,
+    // },
     {
       label: 'Fund pool',
       loadingLabel: 'Confirm funding in wallet',
@@ -49,21 +38,33 @@ const CreateActions: React.FC<Props> = ({ amounts, tokenAddresses, goBack }) => 
     },
   ]
 
+  const networkConfig = config[chainId]
+
+  const [isRestoredTxConfirmed, setIsRestoredTxConfirmed] = useState(false)
+  const [actions, setActions] = useState<TransactionActionInfo[]>(initActions)
+
+  const amountsToApprove = amounts.map((amount, index) => {
+    return {
+      address: tokenAddresses[index],
+      amount,
+    }
+  })
+
   const requiredActions = useMemo(() => {
     if ((hasRestoredFromSavedState && needsSeeding) || isRestoredTxConfirmed) {
       return actions.filter((action) => action.label === 'Fund pool')
     }
 
     return actions
-  }, [])
+  }, [JSON.stringify(actions)])
 
-  const onSubmit = async () => {
-    try {
-      createPool()
-    } catch (e) {
-      console.error(e)
-    }
-  }
+  // const onSubmit = async () => {
+  //   try {
+  //     createPool()
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  // }
 
   const getActions = async () => {
     const approvalActions = await getTokenApprovalActions({
@@ -72,18 +73,17 @@ const CreateActions: React.FC<Props> = ({ amounts, tokenAddresses, goBack }) => 
       actionType: ApprovalAction.AddLiquidity,
     })
 
-    console.log('approvalActions', approvalActions)
+    // setActions([...approvalActions, ...initActions])
   }
 
   useEffect(() => {
     getActions()
   }, [])
+
+  console.log('requiredActions', requiredActions)
   return (
     <div>
-      <NavigationButtons>
-        <BackButton onClick={goBack}>Back</BackButton>
-        <NextButton onClick={onSubmit}>Next</NextButton>
-      </NavigationButtons>
+      <ActionSteps requiredActions={requiredActions} primaryActionType="createPool" disabled={false} goBack={goBack} />
     </div>
   )
 }
