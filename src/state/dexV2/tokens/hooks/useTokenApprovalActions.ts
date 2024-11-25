767a76d3689e35cf13d4531092e51b47fb08ca3e
leonthuongto <leon@ixswap.io>
2024-11-25 08:57:40 +0700

    Fix error

--- src/state/dexV2/tokens/hooks/useTokenApprovalActions.ts
+++ src/state/dexV2/tokens/hooks/useTokenApprovalActions.ts
@@ -1,11 +1,23 @@
 import { MaxUint256 } from '@ethersproject/constants'
 import { parseUnits } from '@ethersproject/units'
 import { simulateContract, waitForTransactionReceipt, writeContract } from '@wagmi/core'
+import { flatten } from 'lodash';
+import { Address } from 'viem'
+
 import { wagmiConfig } from 'components/Web3Provider'
 import { useWeb3React } from 'hooks/useWeb3React'
 import { TokenInfo } from 'types/TokenList'
-import { Address } from 'viem'
 import { useTokens } from './useTokens'
+import { TransactionActionInfo } from 'pages/DexV2/types/transactions'
+
+interface Params {
+  amountsToApprove: AmountToApprove[];
+  spender: string;
+  actionType: ApprovalAction;
+  forceMax?: boolean;
+  skipAllowanceCheck?: boolean;
+}
+
 
 export type AmountToApprove = {
   address: string
@@ -31,7 +43,7 @@ interface ApproveTokenParams {
 
 export default function useTokenApprovalActions() {
   const { account } = useWeb3React()
-  const { injectSpenders, refetchAllowances } = useTokens()
+  const { injectSpenders, refetchAllowances, getToken } = useTokens()
 
   async function updateAllowancesFor(spender: string): Promise<void> {
     injectSpenders([spender])
@@ -85,6 +97,69 @@ export default function useTokenApprovalActions() {
 
     return transaction
   }
+
+    /**
+   * Returns a list of TransactionActions to approve tokens for a given spender.
+   * Typically used to inject into BalActionSteps.
+   *
+   * @param {AmountToApprove[]} amountsToApprove The list of tokens and amounts
+   * to approve.
+   * @param {string} spender The contract address to give the approval too,
+   * typically the vault.
+   * @param {ApprovalAction} actionType The action type that follows the
+   * approval, used for labeling.
+   * @param {boolean} forceMaxApprovals If true, the approval will be for the
+   * maximum possible amount.
+   * @returns {Promise<TransactionActionInfo[]>} The list of TransactionActions.
+   */
+    // async function getTokenApprovalActions({
+    //   amountsToApprove,
+    //   spender,
+    //   actionType,
+    //   forceMax = true,
+    //   skipAllowanceCheck = false,
+    // }: Params): Promise<TransactionActionInfo[]> {
+    //   const approvalsRequired = await getApprovalsRequired(
+    //     amountsToApprove,
+    //     spender,
+    //     skipAllowanceCheck
+    //   );
+
+    //   return flatten(
+    //     approvalsRequired.map(amountToApprove => {
+    //       const token = getToken(amountToApprove.address);
+    //       const actions: TransactionActionInfo[] = [];
+
+    //       /**
+    //        * Some tokens require setting approved amount to 0 before changing the
+    //        * approval amount. This injects another action to do that.
+    //        */
+    //       if (isDoubleApprovalRequired(token, spender)) {
+    //         actions.push(
+    //           createApprovalAction({
+    //             token,
+    //             normalizedAmount: '0',
+    //             spender,
+    //             actionType: ApprovalAction.Unapprove,
+    //             forceMax: false,
+    //           })
+    //         );
+    //       }
+
+    //       actions.push(
+    //         createApprovalAction({
+    //           token,
+    //           normalizedAmount: amountToApprove.amount,
+    //           spender,
+    //           actionType,
+    //           forceMax,
+    //         })
+    //       );
+
+    //       return actions;
+    //     })
+    //   );
+    // }
 
   return {
     approveToken,
