741ebb0ce08aa734be2b529dd2ed5b19ab697767
leonthuongto <leon@ixswap.io>
2024-11-26 22:08:42 +0700

    Add component step

--- src/pages/DexV2/Pool/components/HorizSteps.tsx
+++ src/pages/DexV2/Pool/components/HorizSteps.tsx
@@ -1,73 +1,89 @@
-import { Step, StepState } from 'pages/DexV2/types';
-import React from 'react';
-import styled from 'styled-components';
+import { Step, StepState } from 'pages/DexV2/types'
+import React from 'react'
+import styled from 'styled-components'
+
+import { MouseoverTooltip } from 'components/Tooltip'
 
 interface HorizStepsProps {
-  steps: Step[];
+  steps: Step[]
 }
 
 const HorizSteps: React.FC<HorizStepsProps> = ({ steps }) => {
+  console.log('HorizSteps', steps)
   return (
     <StepsContainer>
       {steps.map((step, i) => (
-        <StepStyled key={i} className="step">
-          <Tooltip>
-            {step.state === StepState.Completed ? (
-              <CheckIcon className="w-8 h-8" />
-            ) : step.state === StepState.Pending ? (
-              <>
-                <StepNumber className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                  {i + 1}
-                </StepNumber>
-                <SpinnerIcon className="w-8 h-8 animate-spin" />
-              </>
-            ) : (
-              <StepNumber>{i + 1}</StepNumber>
-            )}
-          </Tooltip>
-        </StepStyled>
+        <StepContent key={i}>
+          {i !== 0 ? <ConnectedLine /> : null}
+
+          <MouseoverTooltip text={step.tooltip} placement="top">
+            <StepStyled>
+              {step.state === StepState.Completed ? (
+                <CheckIcon className="w-8 h-8" />
+              ) : step.state === StepState.Pending ? (
+                <>
+                  <StepNumber className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                    {i + 1}
+                  </StepNumber>
+                  <SpinnerIcon className="w-8 h-8 animate-spin" />
+                </>
+              ) : (
+                <StepNumber>{i + 1}</StepNumber>
+              )}
+            </StepStyled>
+          </MouseoverTooltip>
+        </StepContent>
       ))}
     </StepsContainer>
-  );
-};
+  )
+}
 
 const StepsContainer = styled.div`
   display: flex;
-`;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+`
+
+const StepContent = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const ConnectedLine = styled.div`
+  width: 2.75rem;
+  height: 1px;
+  background-color: #e6e6ff;
+`
 
 const StepStyled = styled.div`
-  width: 2rem; /* w-8 */
-  height: 2rem; /* h-8 */
-  border-radius: 9999px; /* rounded-full */
-  border: 1px solid #d1d5db; /* border */
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05); /* shadow */
-  font-weight: 500; /* font-medium */
-  display: flex; /* flex */
-  align-items: center; /* items-center */
-  justify-content: center; /* justify-center */
-  position: relative; /* relative */
-`;
-
-const Tooltip = styled.div`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 9999px;
+  border: 1px solid #d1d5db;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: relative;
-`;
+`
 
 const StepNumber = styled.span`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-`;
+`
 
 const CheckIcon = styled.div`
-  width: 2rem; /* w-8 */
-  height: 2rem; /* h-8 */
-  /* Add your CheckIcon styles here */
-`;
+  width: 2rem;
+  height: 2rem;
+`
 
 const SpinnerIcon = styled.div`
-  width: 2rem; /* w-8 */
-  height: 2rem; /* h-8 */
+  width: 2rem;
+  height: 2rem;
   animation: spin 1s linear infinite; /* animate-spin */
   /* Add your SpinnerIcon styles here */
 
@@ -79,6 +95,6 @@ const SpinnerIcon = styled.div`
       transform: rotate(360deg);
     }
   }
-`;
+`
 
-export default HorizSteps;
+export default HorizSteps
