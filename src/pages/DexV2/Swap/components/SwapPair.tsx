d2cf4d301c9668f17eb7e913075a142108d557ee
leonthuongto <leon@ixswap.io>
2024-12-11 13:29:19 +0700

    Add UI page swap

--- src/pages/DexV2/Swap/components/SwapPair.tsx
+++ src/pages/DexV2/Swap/components/SwapPair.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { Box } from 'rebass'
+
 import TokenInput from './TokenInput'
 
 interface SwapPairProps {}
@@ -6,8 +8,13 @@ interface SwapPairProps {}
 const SwapPair: React.FC<SwapPairProps> = () => {
   return (
     <div>
-      <TokenInput />
-      <TokenInput />
+      <div>
+        <TokenInput />
+      </div>
+
+      <Box mt={24}>
+        <TokenInput />
+      </Box>
     </div>
   )
 }
