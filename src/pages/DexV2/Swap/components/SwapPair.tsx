107df88e2f545b398e3da246a256ca16f3fdc698
leonthuongto <leon@ixswap.io>
2024-12-24 13:40:20 +0700

    Update lib

--- src/pages/DexV2/Swap/components/SwapPair.tsx
+++ src/pages/DexV2/Swap/components/SwapPair.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Box } from 'rebass'
+import styled from 'styled-components'
 
 import TokenInput from './TokenInput'
 
@@ -11,7 +12,7 @@ const SwapPair: React.FC<SwapPairProps> = () => {
       <div>
         <TokenInput />
       </div>
-
+      <div>Icon Swap</div>
       <Box mt={24}>
         <TokenInput />
       </Box>
@@ -20,3 +21,7 @@ const SwapPair: React.FC<SwapPairProps> = () => {
 }
 
 export default SwapPair
+
+const Container = styled.div`
+  position: relative;
+`
