26ac15fdb31a4049f5edae0d7b322e9cf4554dbf
leonthuongto <leon@ixswap.io>
2024-12-25 10:13:03 +0700

    Add right sections

--- src/pages/DexV2/Pool/components/PoolSummary.tsx
+++ src/pages/DexV2/Pool/components/PoolSummary.tsx
@@ -1,14 +1,69 @@
 import React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'react-chartjs-2'
+import { Flex, Text } from 'rebass'
+import InfoIcon from 'assets/images/dex-v2/info.svg'
 
-interface PoolSummaryProps {
-  poolName: string
-  totalLiquidity: number
-  apr: number
-  volume24h: number
-}
+ChartJS.register(ArcElement, Tooltip, Legend)
 
-const PoolSummary: React.FC<PoolSummaryProps> = ({ poolName, totalLiquidity, apr, volume24h }) => {
-  return <div>Pool Summary</div>
+interface PoolSummaryProps {}
+
+const PoolSummary: React.FC<PoolSummaryProps> = () => {
+  let data = [
+    {
+      label: 'SFP',
+      value: 55,
+      color: '#6666FF',
+      cutout: '50%',
+    },
+    {
+      label: 'USDT',
+      value: 50,
+      color: '#11D8B3',
+      cutout: '50%',
+    },
+  ]
+
+  const options: any = {
+    plugins: {
+      responsive: true,
+    },
+    cutout: data.map((item) => item.cutout),
+  }
+
+  const finalData = {
+    labels: data.map((item) => item.label),
+    datasets: [
+      {
+        data: data.map((item) => Math.round(item.value)),
+        backgroundColor: data.map((item) => item.color),
+        borderColor: data.map((item) => item.color),
+        borderWidth: 1,
+        dataVisibility: new Array(data.length).fill(true),
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <div>
+        <Doughnut data={finalData} options={options} />
+
+        <Flex flexDirection="column" justifyContent="space-between" alignItems="center" width="100%" my={32}>
+          <Flex alignItems="center" style={{ gap: 6 }} mb="6px">
+            <Text fontSize={14} color="#B8B8D2">
+              In your wallet
+            </Text>
+            <img src={InfoIcon} alt="info" />
+          </Flex>
+
+          <Text fontSize={20} color="rgba(41, 41, 51, 0.90)" fontWeight={600}>
+            $38,600.90
+          </Text>
+        </Flex>
+      </div>
+    </div>
+  )
 }
 
 export default PoolSummary
--- src/pages/DexV2/Pool/components/TokenPrices.tsx
+++ src/pages/DexV2/Pool/components/TokenPrices.tsx
@@ -1,9 +1,75 @@
 import React from 'react'
+import { Flex, Text } from 'rebass'
+import styled from 'styled-components'
+
+import InfoIcon from 'assets/images/dex-v2/info.svg'
+import PenIcon from 'assets/images/dex-v2/pen.svg'
 
 interface TokenPricesProps {}
 
 const TokenPrices: React.FC<TokenPricesProps> = () => {
-  return <div>Token Prices</div>
+  return (
+    <Container>
+      <Flex alignItems="center" style={{ gap: 6 }} mb="6px">
+        <Text fontSize={14} color="#B8B8D2">
+          Token Prices
+        </Text>
+        <img src={InfoIcon} alt="info" />
+      </Flex>
+
+      <Flex
+        fontSize={14}
+        fontWeight={500}
+        justifyContent="space-between"
+        width="100%"
+        alignItems="center"
+        color="rgba(41, 41, 51, 0.90)"
+        py="12px"
+      >
+        <Flex alignItems="center" style={{ gap: 6 }}>
+          <img
+            src="https://assets.coingecko.com/coins/images/4713/standard/polygon.png?1698233745"
+            width={20}
+            height={20}
+          />
+          <Text>SFP</Text>
+        </Flex>
+        <Flex alignItems="center" style={{ gap: 6 }}>
+          <Text>$2.00</Text>
+          <img src={PenIcon} alt="edit" />
+        </Flex>
+      </Flex>
+
+      <Flex
+        fontSize={14}
+        fontWeight={500}
+        justifyContent="space-between"
+        width="100%"
+        alignItems="center"
+        color="rgba(41, 41, 51, 0.90)"
+        py="12px"
+      >
+        <Flex alignItems="center" style={{ gap: 6 }}>
+          <img
+            src="https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
+            width={20}
+            height={20}
+          />
+          <Text>USDT</Text>
+        </Flex>
+
+        <Flex alignItems="center" style={{ gap: 6 }}>
+          <Text>$2.00</Text>
+          <img src={PenIcon} alt="edit" />
+        </Flex>
+      </Flex>
+    </Container>
+  )
 }
 
 export default TokenPrices
+
+const Container = styled.div`
+  padding-top: 32px;
+  border-top: solid 1px #e6e6ff;
+`
