7b0f9a34391c08090ad28c700e86a9ba6e4fac2e
leonthuongto <leon@ixswap.io>
2024-12-25 08:56:16 +0700

    Add UI for Pool chart

---
+++ src/pages/DexV2/Pool/components/TokenPrices.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface TokenPricesProps {}
+
+const TokenPrices: React.FC<TokenPricesProps> = () => {
+  return <div>Token Prices</div>
+}
+
+export default TokenPrices
