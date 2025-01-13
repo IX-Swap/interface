6f6918b1dff65158f81293ff8cc99b78966164d1
Hung Le <hung@ixswap.io>
2025-01-13 09:51:03 +0700

    feat: [IXSPD1-2312] add v2 dashboard

---
+++ src/pages/DexV2/Dashboard/components/Card.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  cursor: pointer;
+  border-radius: 16px;
+  background: #fff;
+  padding: 48px;
+
+  &:hover {
+    background: #FFFFFF4D;
+  }
+`
