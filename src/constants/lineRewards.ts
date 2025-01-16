41346d31eb2aa51bd946c5cc93ca383469505331
Hung Le <hung@ixswap.io>
2025-01-16 10:42:49 +0700

    feat: [IXSPD1-2332] enable user claim reward for joining line campaign

--- src/constants/lineRewards.ts
+++ src/constants/lineRewards.ts
@@ -1,6 +1,8 @@
 
 export const DAILY_REWARD = 100
 
+export const JOIN_CAMPAIGN_REWARD = 100
+
 export const KYC_REWARD = 1_000
 
 export const INVEST_REWARD_PER_TOKEN = 1_000
@@ -9,6 +11,7 @@ export enum LineRewardAction {
   CONNECT_WALLET = 'connect_wallet',
   KYC = 'kyc',
   INVEST = 'invest',
+  JOIN_CAMPAIGN = 'join_campaign',
 }
 
 export const API_KEY = process.env.REACT_APP_LINE_REWARD_API_KEY
--- src/hooks/useLineReward.ts
+++ src/hooks/useLineReward.ts
@@ -1,9 +1,12 @@
-import { useMutation } from '@tanstack/react-query'
-import { useState } from 'react'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { linePoint } from 'services/apiUrls'
 import { Address } from 'viem'
-import { API_KEY, LineRewardAction } from 'constants/lineRewards'
+import { API_KEY, JOIN_CAMPAIGN_REWARD, LineRewardAction } from 'constants/lineRewards'
+import { useWeb3React } from './useWeb3React'
+import { isLineLiff } from 'utils'
+import { apiService as lineRewardApiService } from 'hooks/useLineReward'
 
 export const apiService = axios.create({
   baseURL: linePoint.baseUrl,
@@ -13,6 +16,7 @@ export const apiService = axios.create({
 })
 
 function useLineReward() {
+  const { account } = useWeb3React()
   const [openTaskSuccessModal, setOpenTaskSuccessModal] = useState(false)
   const [rewardsData, setRewardsData] = useState<{
     points: number
@@ -35,6 +39,36 @@ function useLineReward() {
       )
     },
   })
+
+  // Claim rewards for joining campaign
+  const validateClaimRewards = useQuery({
+    queryKey: ['validateJoinCampaignRewards', account],
+    enabled: isLineLiff,
+    queryFn: async () => {
+      const { data } = await lineRewardApiService.get(linePoint.checkClaimed, {
+        params: {
+          address: account,
+          action: LineRewardAction.JOIN_CAMPAIGN,
+        },
+      })
+      return data
+    },
+    staleTime: Infinity,
+  })
+  useEffect(() => {
+    if (
+      !account ||
+      !validateClaimRewards.isFetched ||
+      validateClaimRewards.data?.claimed
+    )
+      return
+
+    setOpenTaskSuccessModal(true)
+    setRewardsData({
+      action: LineRewardAction.JOIN_CAMPAIGN,
+      points: JOIN_CAMPAIGN_REWARD,
+    })
+  }, [account, validateClaimRewards.isFetched, validateClaimRewards.data?.claimed])
 
   return {
     mutatePoint,
