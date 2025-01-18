7711b9a509b7a9e12e0647f592d26ced73a5c2a8
thi-investax <thi@investax.io>
2025-01-18 11:59:55 +0700

    Disable liff provider temporarily

--- src/pages/LiffProvider.tsx
+++ src/pages/LiffProvider.tsx
@@ -16,24 +16,24 @@ interface LiffProviderProps {
 export const LiffProvider: React.FC<LiffProviderProps> = ({ children }) => {
   const [isLiffBrowser, setIsLiffBrowser] = useState(false)
 
-  useEffect(() => {
-    liff
-      .init({ liffId: '2006746651-DmRwZed0' })
-      .then(() => {
-        console.log('LIFF initialization is done')
-        // console.log(liff.getAppLanguage())
-        console.log(liff.getVersion())
-        console.log(liff.isInClient())
-        console.log(liff.isLoggedIn())
-        console.log(liff.getOS())
-        console.log(liff.getLineVersion())
+  // useEffect(() => {
+  //   liff
+  //     .init({ liffId: '2006746651-DmRwZed0' })
+  //     .then(() => {
+  //       console.log('LIFF initialization is done')
+  //       // console.log(liff.getAppLanguage())
+  //       console.log(liff.getVersion())
+  //       console.log(liff.isInClient())
+  //       console.log(liff.isLoggedIn())
+  //       console.log(liff.getOS())
+  //       console.log(liff.getLineVersion())
 
-        setIsLiffBrowser(liff.isInClient()) // Set the value of isLiffBrowser
-      })
-      .catch((error: any) => {
-        console.error(`LIFF initialization failed: ${error}`)
-      })
-  }, [])
+  //       setIsLiffBrowser(liff.isInClient()) // Set the value of isLiffBrowser
+  //     })
+  //     .catch((error: any) => {
+  //       console.error(`LIFF initialization failed: ${error}`)
+  //     })
+  // }, [])
 
   return <LiffContext.Provider value={{ isLiffBrowser }}>{children}</LiffContext.Provider>
 }
