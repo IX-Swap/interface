7d8f4c4c04e1be1a6c1bed3a35b26a35762a7247
leonthuongto <leon@ixswap.io>
2025-02-04 10:43:43 +0700

    remove security folders

--- e2e_tests/helpers/timeouts.ts
+++
@@ -1,9 +0,0 @@
-export const timeouts = {
-  globalTestsTimeout: 1500000,
-  testTimeout: 300000,
-  tinyTimeout: 2000,
-  timeoutForSignWindow: 5000,
-  shortTimeout: 25000,
-  mediumTimeout: 45000,
-  longTimeout: 65000
-};
