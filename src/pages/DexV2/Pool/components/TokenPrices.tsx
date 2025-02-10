a03e39630007bf2e6b0d1ea80fba168ef57e59ca
leonthuongto <leon@ixswap.io>
2025-02-10 19:52:46 +0700

    Add chart

--- src/pages/DexV2/Pool/components/TokenPrices.tsx
+++ src/pages/DexV2/Pool/components/TokenPrices.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Flex, Text } from 'rebass'
 import styled from 'styled-components'
 
@@ -6,18 +6,33 @@ import InfoIcon from 'assets/images/dex-v2/info.svg'
 import PenIcon from 'assets/images/dex-v2/pen.svg'
 import { usePoolCreation } from 'state/dexV2/poolCreation/hooks/usePoolCreation'
 import { useTokens } from 'state/dexV2/tokens/hooks/useTokens'
-import useNumbers from 'hooks/dex-v2/useNumbers'
+import useNumbers, { FNumFormats } from 'hooks/dex-v2/useNumbers'
+import { selectByAddress } from 'lib/utils'
 
 interface TokenPricesProps {
   toggleUnknownPriceModal?: () => void
 }
 
 const TokenPrices: React.FC<TokenPricesProps> = () => {
-  const { tokensList } = usePoolCreation();
-  const { getToken, priceFor, injectedPrices } = useTokens();
-const { fNum } = useNumbers();
+  const { tokensList } = usePoolCreation()
+  const { getToken, priceFor, injectedPrices } = useTokens()
+  const { fNum } = useNumbers()
 
-  console.log('tokensList', tokensList);
+  const validTokens = useMemo(() => tokensList.filter((t) => t !== ''), [JSON.stringify(tokensList)])
+  const knownTokens = useMemo(
+    () => validTokens.filter((token) => priceFor(token) !== 0 && !selectByAddress(injectedPrices, token)),
+    [JSON.stringify(validTokens), JSON.stringify(injectedPrices)]
+  )
+  const unknownTokens = useMemo(
+    () => validTokens.filter((token) => priceFor(token) === 0 || selectByAddress(injectedPrices, token)),
+    [JSON.stringify(validTokens), JSON.stringify(injectedPrices)]
+  )
+  const hasUnknownPrice = useMemo(
+    () => validTokens.some((token) => priceFor(token) === 0),
+    [JSON.stringify(validTokens)]
+  )
+
+  console.log('knownTokens', knownTokens)
 
   return (
     <Container>
@@ -28,30 +43,33 @@ const { fNum } = useNumbers();
         <img src={InfoIcon} alt="info" />
       </Flex>
 
-      <Flex
-        fontSize={14}
-        fontWeight={500}
-        justifyContent="space-between"
-        width="100%"
-        alignItems="center"
-        color="rgba(41, 41, 51, 0.90)"
-        py="12px"
-      >
-        <Flex alignItems="center" style={{ gap: 6 }}>
-          <img
-            src="https://assets.coingecko.com/coins/images/4713/standard/polygon.png?1698233745"
-            width={20}
-            height={20}
-          />
-          <Text>SFP</Text>
+      {knownTokens.map((token) => (
+        <Flex
+          fontSize={14}
+          fontWeight={500}
+          justifyContent="space-between"
+          width="100%"
+          alignItems="center"
+          color="rgba(41, 41, 51, 0.90)"
+          py="12px"
+          key={`tokenPrice-known-${token}`}
+        >
+          <Flex alignItems="center" style={{ gap: 6 }}>
+            <img
+              src={getToken(token)?.logoURI}
+              width={20}
+              height={20}
+            />
+            <Text>{getToken(token)?.symbol}</Text>
+          </Flex>
+          <Flex alignItems="center" style={{ gap: 6 }}>
+            <Text>{`${fNum(priceFor(token), FNumFormats.fiat)}`}</Text>
+            {/* <img src={PenIcon} alt="edit" /> */}
+          </Flex>
         </Flex>
-        <Flex alignItems="center" style={{ gap: 6 }}>
-          <Text>$2.00</Text>
-          <img src={PenIcon} alt="edit" />
-        </Flex>
-      </Flex>
+      ))}
 
-      <Flex
+      {/* <Flex
         fontSize={14}
         fontWeight={500}
         justifyContent="space-between"
@@ -73,7 +91,7 @@ const { fNum } = useNumbers();
           <Text>$2.00</Text>
           <img src={PenIcon} alt="edit" />
         </Flex>
-      </Flex>
+      </Flex> */}
     </Container>
   )
 }
