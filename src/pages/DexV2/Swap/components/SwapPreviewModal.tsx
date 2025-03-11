e0eb49391b2620d8c20d93a8afedb8d1559b5a89
leonthuongto <leon@ixswap.io>
2025-03-11 16:59:16 +0700

    Update logic plus 1

--- src/pages/DexV2/Swap/components/SwapPreviewModal.tsx
+++ src/pages/DexV2/Swap/components/SwapPreviewModal.tsx
@@ -244,8 +244,8 @@ const SwapPreviewModal: React.FC<SwapSettingsModalProps> = ({ swapping, error, w
     async function fetchTokenApprovalActions() {
       let amountToApprove = bnum(swapping.tokenInAmountInput).plus(1).toString()
       if (!swapping.exactIn) {
-        amountToApprove = bnum(swapping.tokenInAmountInput)
-          .times(bnum(1).plus(slippage + 1))
+        amountToApprove = bnum(swapping.tokenInAmountInput + 1)
+          .times(bnum(1).plus(slippage)).plus(1)
           .toString()
       }
 
