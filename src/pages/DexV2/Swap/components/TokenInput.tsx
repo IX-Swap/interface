fc4fff8ce768468b90272a79678e1f7d6c177f62
leonthuongto <leon@ixswap.io>
2025-03-19 13:55:10 +0700

    Fix width amount

--- src/pages/DexV2/Swap/components/TokenInput.tsx
+++ src/pages/DexV2/Swap/components/TokenInput.tsx
@@ -309,7 +309,7 @@ const StyledInput = styled.input`
   border: none;
   background: transparent;
   min-width: 50px;
-  max-width: 170px;
+  max-width: 234px;
 `
 
 const StyledNumber = styled.div`
