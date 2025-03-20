096f234e83ced15896a58bac81c14c19edb7cec6
leonthuongto <leon@ixswap.io>
2025-03-20 15:22:56 +0700

    jwt logout handling and cookie selection based on user address (#3131)

--- src/state/auth/reducer.ts
+++ src/state/auth/reducer.ts
@@ -3,14 +3,12 @@ import { logout, postLogin } from './actions'
 
 export interface AuthState {
   readonly token: Record<any, string | undefined>
-  readonly refreshToken: Record<any, string | undefined>
   loginLoading: boolean
   loginError: string | null
 }
 
 const initialState: AuthState = {
   token: {},
-  refreshToken: {},
   loginLoading: false,
   loginError: null,
 }
@@ -19,7 +17,6 @@ export default createReducer<AuthState>(initialState, (builder) =>
   builder
     .addCase(logout, (state, { payload: account }) => {
       state.token[account] = ''
-      state.refreshToken[account] = ''
     })
     .addCase(postLogin.pending, (state, { payload: account }) => {
       state.loginLoading = true
@@ -27,15 +24,11 @@ export default createReducer<AuthState>(initialState, (builder) =>
       if (!state.token) {
         state.token = { [account]: '' }
       }
-      if (!state.refreshToken) {
-        state.refreshToken = { [account]: '' }
-      }
     })
     .addCase(postLogin.fulfilled, (state, { payload: { auth, account } }) => {
       state.loginLoading = false
       state.loginError = null
       state.token[account] = auth.accessToken
-      state.refreshToken[account] = auth.refreshToken
       localStorage.setItem('redux_localstorage_simple_auth', JSON.stringify(state))
     })
     .addCase(postLogin.rejected, (state, { payload: { errorMessage, account } }) => {
