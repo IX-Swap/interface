50b2c1dba95c3616e1050f6e0c2647eb6874e8a3
Hung Le <hung@ixswap.io>
2025-04-09 14:11:49 +0700

    feat: [IXSPD1-2573] Update Lock sections

--- src/pages/DexV2/Dashboard/components/LockRewards.tsx
+++ src/pages/DexV2/Dashboard/components/LockRewards.tsx
@@ -9,17 +9,11 @@ import { Line } from 'components/Line'
 import CurrencyLogo from 'components/CurrencyLogo'
 import { NewApproveButton, PinnedContentButton } from 'components/Button'
 import { LockItem } from '../graphql/dashboard'
-import { formatDate } from 'utils/time'
 import useWeb3 from 'hooks/dex-v2/useWeb3'
 import useLocksQuery from 'hooks/dex-v2/queries/useLocksQuery'
 import { routes } from 'utils/routes'
 import { useHistory } from 'react-router-dom'
 import dayjs from 'dayjs'
-import duration from 'dayjs/plugin/duration'
-import relativeTime from 'dayjs/plugin/relativeTime'
-
-dayjs.extend(duration)
-dayjs.extend(relativeTime)
 
 const LockRewards: React.FC = () => {
   const { account } = useWeb3()
@@ -54,14 +48,11 @@ const TableHeader = ({ id }: { id: number }) => {
       <Grid item xs={3}>
         <TYPE.subHeader1 color="text6">Lock #{id}</TYPE.subHeader1>
       </Grid>
-      <Grid item xs={2}>
-        <TYPE.subHeader1 color="text6">Lock Date</TYPE.subHeader1>
-      </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <TYPE.subHeader1 color="text6">Amount</TYPE.subHeader1>
       </Grid>
-      <Grid item xs={2}>
-        <TYPE.subHeader1 color="text6">Lock Time</TYPE.subHeader1>
+      <Grid item xs={3}>
+        <TYPE.subHeader1 color="text6">Unlock At</TYPE.subHeader1>
       </Grid>
     </Grid>
   )
@@ -72,9 +63,14 @@ const TableBody = ({ data }: { data: LockItem }) => {
   const currency = useCurrency(data.token)
   const history = useHistory()
 
-  const durationLabel = useMemo(() => {
-    const durationMs = dayjs(+data.expiresAt * 1000).diff(dayjs())
-    return dayjs.duration(durationMs, 'milliseconds').humanize()
+  const isNotExpired = useMemo(() => {
+    const currentTime = Math.floor(Date.now() / 1000)
+    return Number(data.expiresAt) > currentTime
+  }, [data.expiresAt])
+
+  const unlockAtLabel = useMemo(() => {
+    const date = dayjs(Number(data.expiresAt) * 1000)
+    return date.format('ddd MMM DD YYYY HH:mm:ss')
   }, [data.expiresAt])
 
   const handleIncrease = () => {
@@ -93,6 +89,10 @@ const TableBody = ({ data }: { data: LockItem }) => {
     history.push(`${path}?${searchParams.toString()}`)
   }
 
+  const handleWithdraw = () => {
+    console.log('withdraw')
+  }
+
   return (
     <Grid container spacing={2} alignItems="center">
       <Grid item xs={3}>
@@ -101,40 +101,50 @@ const TableBody = ({ data }: { data: LockItem }) => {
           <TYPE.subHeader1 fontSize={16}>veIXS</TYPE.subHeader1>
         </Stack>
       </Grid>
-      <Grid item xs={2}>
-        <TYPE.subHeader1>{data.votedAt === '0' ? 'N/A' : formatDate(Number(data.votedAt))}</TYPE.subHeader1>
-      </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <TYPE.label fontSize={16}>{data.amount}</TYPE.label>
       </Grid>
-      <Grid item xs={2}>
-        <TYPE.label fontSize={16}>{durationLabel}</TYPE.label>
+      <Grid item xs={3}>
+        <TYPE.label fontSize={16}>{unlockAtLabel}</TYPE.label>
       </Grid>
       <Grid item xs={3}>
-        <Stack direction="row" gap={2}>
-          <NewApproveButton
-            style={{
-              color: theme.primary1,
-              border: `1px solid ${theme.bg24}`,
-              width: 'auto',
-              paddingTop: 12,
-              paddingBottom: 12,
-            }}
-            onClick={handleIncrease}
-          >
-            Increase
-          </NewApproveButton>
+        {isNotExpired ? (
+          <Stack direction="row" gap={2}>
+            <NewApproveButton
+              style={{
+                color: theme.primary1,
+                border: `1px solid ${theme.bg24}`,
+                width: 'auto',
+                paddingTop: 12,
+                paddingBottom: 12,
+              }}
+              onClick={handleIncrease}
+            >
+              Increase
+            </NewApproveButton>
+            <PinnedContentButton
+              style={{
+                width: 'auto',
+                paddingTop: 12,
+                paddingBottom: 12,
+              }}
+              onClick={handleExtend}
+            >
+              Extend
+            </PinnedContentButton>
+          </Stack>
+        ) : (
           <PinnedContentButton
             style={{
               width: 'auto',
               paddingTop: 12,
               paddingBottom: 12,
             }}
-            onClick={handleExtend}
+            onClick={handleWithdraw}
           >
-            Extend
+            Withdraw
           </PinnedContentButton>
-        </Stack>
+        )}
       </Grid>
     </Grid>
   )
